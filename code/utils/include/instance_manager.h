361488deba850d1417d8ecdde0132270199fede3
Peter Thoman <peter.thoman@uibk.ac.at>
2010-08-14 14:34:36 +0200

    Compiler warning fix

--- code/utils/include/instance_manager.h
+++ code/utils/include/instance_manager.h
@@ -81,13 +81,13 @@ struct target_hash: public std::unary_function<T, std::size_t> {
 	/**
 	 * This function is used to compute the hash of the actual target.
 	 */
-	const boost::hash<T> hasher;
+	boost::hash<T> hasher;
 
 	/**
 	 * Explicit Default constructor required by VC.
 	 */
 	target_hash() : hasher() {	}
-
+	
 	/**
 	 * Computes the hash value of the given pointer based on the target it is pointing to.
 	 */
