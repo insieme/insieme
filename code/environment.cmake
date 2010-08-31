47e568ce3bbbcfde89804a33c90f99bf344e8ed2
Peter Thoman <peter.thoman@uibk.ac.at>
2010-08-31 11:49:54 +0200

    Added more constructor expressions

--- code/environment.cmake
+++ code/environment.cmake
@@ -22,8 +22,13 @@ include_directories( ${Boost_INCLUDE_DIRS} )
 link_directories(${Boost_LIBRARY_DIRS})
 
 # glog
-include_directories( $ENV{GLOG_HOME}/include )
-find_library(glog_LIB NAMES glog PATHS $ENV{GLOG_HOME}/lib)
+if(MSVC)
+	include_directories( $ENV{GLOG_HOME}/src/windows )
+	set(glog_LIB $ENV{GLOG_HOME}/Release/libglog_static.lib)
+else()
+	include_directories( $ENV{GLOG_HOME}/include )
+	find_library(glog_LIB NAMES glog PATHS $ENV{GLOG_HOME}/lib)
+endif()
 
 # lookup perl
 find_package( Perl )
