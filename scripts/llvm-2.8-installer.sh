418a1a9ecb90d6bf6c3376ad5b3dc657a12039e8
motonacciu <motonacciu@moto-desk.(none)>
2010-09-19 20:18:08 +0200

    fixing installer

--- scripts/llvm-2.8-installer.sh
+++ scripts/llvm-2.8-installer.sh
@@ -3,7 +3,6 @@ PREFIX = /home/motonacciu/software/llvm28
 wget http://dps.uibk.ac.at/~spellegrini/llvm-2.8.tar.bz2
 tar -xf llvm-2.8.tar.bz2
 cd llvm-2.8/
-
 patch -p1  < ../insieme-2.8.patch
 
 ./configure --prefix=$PREFIX --enable-shared=yes --enable-assert=yes --enable-debug-runtime=yes --enable-debug-symbols=yes --enable-optimized=no
@@ -12,5 +11,5 @@ patch -p1  < ../insieme-2.8.patch
 make REQUIRES_RTTI=1 clang-only -j2
 make clang-only install
 
-python llvm_alignof.py $PREFIX/include
+python ../llvm_alignof.py $PREFIX/include
 
