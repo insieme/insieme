862771299cc1d67352bb86a0f015a14c7c5a16f0
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2010-09-28 17:02:44 +0200

    fixing compiler bugs

--- code/frontend/src/conversion.cpp
+++ code/frontend/src/conversion.cpp
@@ -586,21 +586,21 @@ public:
 				builder.tupleExpr(
 				{ 	// ref.assign(a int.add(a, 1))
 					builder.callExpr( core::lang::OP_REF_ASSIGN_PTR,
-						{
+						std::vector<core::ExpressionPtr>({
 							subExpr, // ref<a'> a
 							builder.callExpr(
 								( additive ? core::lang::OP_INT_ADD_PTR:core::lang::OP_INT_SUB_PTR ),
 									std::vector<core::ExpressionPtr>({ subExpr, core::lang::CONST_UINT_ONE_PTR })
 							) // a - 1
-						}
+						})
 					),
 					(post ? // if is post increment/decrement
 						builder.callExpr(
 							( additive ? core::lang::OP_INT_SUB_PTR:core::lang::OP_INT_ADD_PTR ),
-							{
+							std::vector<core::ExpressionPtr>({
 								builder.callExpr( core::lang::OP_REF_DEREF_PTR, {subExpr} ), // ref.deref(a)
 								core::lang::CONST_UINT_ONE_PTR // 1
-							}
+							})
 						)
 						: // else
 						builder.callExpr( core::lang::OP_REF_DEREF_PTR, {subExpr} )
