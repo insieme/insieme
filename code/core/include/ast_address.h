02d9b495c6afd301c185057cf10ac61eb480a8ee
Herbert Jordan <herbert.jordan@dps.uibk.ac.at>
2010-10-06 20:50:26 +0200

    Add AST addressing

---
+++ code/core/include/ast_address.h
@@ -0,0 +1,201 @@
+/**
+ * Copyright (c) 2002-2013 Distributed and Parallel Systems Group,
+ *                Institute of Computer Science,
+ *               University of Innsbruck, Austria
+ *
+ * This file is part of the INSIEME Compiler and Runtime System.
+ *
+ * We provide the software of this file (below described as "INSIEME")
+ * under GPL Version 3.0 on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ *
+ * If you require different license terms for your intended use of the
+ * software, e.g. for proprietary commercial or industrial use, please
+ * contact us at:
+ *                   insieme@dps.uibk.ac.at
+ *
+ * We kindly ask you to acknowledge the use of this software in any
+ * publication or other disclosure of results by referring to the
+ * following citation:
+ *
+ * H. Jordan, P. Thoman, J. Durillo, S. Pellegrini, P. Gschwandtner,
+ * T. Fahringer, H. Moritsch. A Multi-Objective Auto-Tuning Framework
+ * for Parallel Codes, in Proc. of the Intl. Conference for High
+ * Performance Computing, Networking, Storage and Analysis (SC 2012),
+ * IEEE Computer Society Press, Nov. 2012, Salt Lake City, USA.
+ *
+ * All copyright notices must be kept intact.
+ *
+ * INSIEME depends on several third party software packages. Please 
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
+ * regarding third party software licenses.
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "ast_node.h"
+#include "hash_utils.h"
+
+namespace insieme {
+namespace core {
+
+/**
+ * This immutable value class can be used to address nodes within the AST. Since nodes within the AST are shared,
+ * the same node may be reused at multiple locations within the AST. Hence, a simple pointer would be insufficient for
+ * uniquely identifying an element of the syntax tree. To address such an element, the entire path, starting from an
+ * arbitrary root node, has to be considered. Only this path allows to address particular sections within an AST.
+ *
+ * TODO: extend documentation with usage scenarios
+ */
+class NodeAddress : public utils::HashableImmutableData<NodeAddress> {
+
+	/**
+	 * The path used to identify the node referenced by this address.
+	 * The pointer stored within this vector are describing the sequence of edges to be followed
+	 * for reaching the corresponding node.
+	 */
+	std::vector<NodePtr> path;
+
+public:
+
+	/**
+	 * A constructor creating an address for the given root node.
+	 */
+	NodeAddress(const NodePtr& root);
+
+	/**
+	 * A constructor creating a node address based on a path through an AST.
+	 *
+	 * @param path the path to the node to be addressed.
+	 */
+	NodeAddress(const std::vector<NodePtr>& path = std::vector<NodePtr>());
+
+	/**
+	 * Obtains a pointer to the root node this address is starting from.
+	 *
+	 * NOTE: nodes are not addressed in a unique way. Since nodes are shared, a single node might be accessible
+	 * via multiple addresses.
+	 *
+	 * @return a pointer to the root node.
+	 */
+	NodePtr getRootNode() const;
+
+	/**
+	 * Obtains a pointer to the parent node of the given address (of an arbitrary higher level).
+	 * An assertion error will occur in case the requested level is large or equal the depth of this address.
+	 * (Hence, the corresponding parent node does not exist or is unknown).
+	 *
+	 * @param level the number of levels to go up (0=same node, 1=parent, 2=grandparents ...)
+	 * @return the requested address
+	 */
+	NodePtr getParentNode(unsigned level=1) const;
+
+	/**
+	 * Obtains a pointer to the node referenced by this address.
+	 *
+	 * NOTE: nodes are not addressed in a unique way. Since nodes are shared, a single node might be accessible
+	 * via multiple addresses.
+	 *
+	 * @return a pointer to the addressed node.
+	 */
+	NodePtr getAddressedNode() const;
+
+	/**
+	 * Obtains the address of the root node this address is starting from. Since it will only consist of a single
+	 * element its parents will be unknown.
+	 *
+	 * @return the (relative) address of the root node
+	 */
+	NodeAddress getRootNodeAddress() const;
+
+	/**
+	 * Obtains the address of the parent node of the given address (of an arbitrary higher level).
+	 * An assertion error will occur in case the requested level is large or equal the depth of this address.
+	 * (Hence, the corresponding parent node does not exist or is unknown).
+	 *
+	 * @param level the number of levels to go up (0=same node, 1=parent, 2=grandparents ...)
+	 * @return the requested address
+	 */
+	NodeAddress getParentNodeAddress(unsigned level=1) const;
+
+	/**
+	 * Obtains the address of a child node. It is extending the path maintained by this address by a single
+	 * step, namely the given node.
+	 *
+	 * @param child the node to be addressed
+	 * @return the address of the child node
+	 */
+	NodeAddress getChildAddress(const NodePtr& child) const;
+
+	/**
+	 * Checks whether this address is constituting a valid path within some AST. The method returns
+	 * true in case the represented path can be reconstructed within the AST, hence if the (i+1)-th element
+	 * within the path constituting this address is a pointer to a child node referenced by the i-th element.
+	 *
+	 * @return true if it is a valid path, false otherwise
+	 */
+	bool isValid() const;
+
+	/**
+	 * Obtains the depth / length of this address. The depth corresponds to the number of nodes passed
+	 * between the root node and the addressed node.
+	 *
+	 * @return the depth of the addressed nodes within the AST
+	 */
+	unsigned getDepth() const {
+		return path.size();
+	}
+
+	/**
+	 * Obtains the entire path constituting this node address.
+	 *
+	 * @return a constant reference to the internally maintained path.
+	 */
+	const std::vector<NodePtr>& getPath() const {
+		return path;
+	}
+
+
+	/**
+	 * An implicit conversion to boolean. It will be converted to true, if the address is valid.
+	 * Otherwise the result will be false.
+	 *
+	 * @return true if address is valid, false otherwise
+	 */
+	operator bool() const {
+		return isValid();
+	}
+
+	/**
+	 * Obtains a reference to the node addressed by this address (if valid). Otherwise an
+	 * assertion will be violated.
+	 *
+	 * @return a reference to the addressed node.
+	 */
+	const Node& operator*() const {
+		return *getAddressedNode();
+	}
+
+protected:
+
+	/**
+	 * Compares this instance with the given instance. Of both addresses represent
+	 * the same path within an expression, both addresses will be considered equivalent.
+	 *
+	 * @param other The node address to be compared to.
+	 * @return true if equivalent, false otherwise.
+	 */
+	virtual bool equals(const NodeAddress& other) const;
+
+};
+
+} // end namespace core
+} // end namespace insieme
+
+
+
---
+++ code/core/test/ast_address_test.cc
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2002-2013 Distributed and Parallel Systems Group,
+ *                Institute of Computer Science,
+ *               University of Innsbruck, Austria
+ *
+ * This file is part of the INSIEME Compiler and Runtime System.
+ *
+ * We provide the software of this file (below described as "INSIEME")
+ * under GPL Version 3.0 on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ *
+ * If you require different license terms for your intended use of the
+ * software, e.g. for proprietary commercial or industrial use, please
+ * contact us at:
+ *                   insieme@dps.uibk.ac.at
+ *
+ * We kindly ask you to acknowledge the use of this software in any
+ * publication or other disclosure of results by referring to the
+ * following citation:
+ *
+ * H. Jordan, P. Thoman, J. Durillo, S. Pellegrini, P. Gschwandtner,
+ * T. Fahringer, H. Moritsch. A Multi-Objective Auto-Tuning Framework
+ * for Parallel Codes, in Proc. of the Intl. Conference for High
+ * Performance Computing, Networking, Storage and Analysis (SC 2012),
+ * IEEE Computer Society Press, Nov. 2012, Salt Lake City, USA.
+ *
+ * All copyright notices must be kept intact.
+ *
+ * INSIEME depends on several third party software packages. Please 
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
+ * regarding third party software licenses.
+ */
+
+#include <gtest/gtest.h>
+
+#include "ast_address.h"
+#include "ast_builder.h"
+
+namespace insieme {
+namespace core {
+
+TEST(NodeAddressTest, Basic) {
+	ASTBuilder builder;
+
+	TypePtr typeA = builder.genericType("A",toVector<TypePtr>(builder.genericType("1"), builder.genericType("2")));
+	TypePtr typeB = builder.genericType("B",toVector<TypePtr>(builder.genericType("3")));
+	TypePtr typeC = builder.genericType("C",toVector<TypePtr>());
+
+	TypePtr root = builder.genericType("root", toVector(typeA, typeB, typeC));
+
+	EXPECT_EQ("root<A<1,2>,B<3>,C>", toString(*root));
+
+	// start with root node R
+	NodeAddress addrRoot(root);
+	EXPECT_TRUE(addrRoot);
+	EXPECT_TRUE(addrRoot.isValid());
+	EXPECT_EQ(1, addrRoot.getDepth());
+
+	EXPECT_EQ(root, addrRoot.getAddressedNode());
+	EXPECT_EQ(root, addrRoot.getRootNode());
+	EXPECT_EQ(addrRoot, addrRoot.getParentNodeAddress(0));
+
+	EXPECT_EQ(toString(toVector(root)), toString(addrRoot.getPath()));
+
+	// go to node R-A
+	NodeAddress addrA = addrRoot.getChildAddress(typeA);
+	EXPECT_TRUE(addrA);
+	EXPECT_TRUE(addrA.isValid());
+	EXPECT_EQ(2, addrA.getDepth());
+
+	EXPECT_EQ(typeA, addrA.getAddressedNode());
+	EXPECT_EQ(root, addrA.getRootNode());
+	EXPECT_EQ(addrA, addrA.getParentNodeAddress(0));
+	EXPECT_EQ(addrRoot, addrA.getParentNodeAddress(1));
+	EXPECT_EQ(typeA, addrA.getParentNode(0));
+	EXPECT_EQ(root,  addrA.getParentNode(1));
+
+	EXPECT_EQ(toString(toVector(root, typeA)), toString(addrA.getPath()));
+
+	// go to child R-A-2
+	TypePtr type2 = builder.genericType("2");
+	NodeAddress addrA2 = addrA.getChildAddress(type2);
+	EXPECT_TRUE(addrA2);
+	EXPECT_TRUE(addrA2.isValid());
+	EXPECT_EQ(3, addrA2.getDepth());
+
+	EXPECT_EQ(type2, addrA2.getAddressedNode());
+	EXPECT_EQ(root, addrA2.getRootNode());
+	EXPECT_EQ(addrA2, addrA2.getParentNodeAddress(0));
+	EXPECT_EQ(addrA, addrA2.getParentNodeAddress(1));
+	EXPECT_EQ(addrRoot, addrA2.getParentNodeAddress(2));
+	EXPECT_EQ(type2, addrA2.getParentNode(0));
+	EXPECT_EQ(typeA, addrA2.getParentNode(1));
+	EXPECT_EQ(root,  addrA2.getParentNode(2));
+
+	EXPECT_EQ(toString(toVector(root, typeA, type2)), toString(addrA2.getPath()));
+}
+
+
+TEST(NodeAddressTest, EqualityTest) {
+	ASTBuilder builder;
+
+	// test a diamond
+	TypePtr typeD = builder.genericType("D");
+	TypePtr typeB = builder.genericType("B",toVector<TypePtr>(typeD));
+	TypePtr typeC = builder.genericType("C",toVector<TypePtr>(typeD));
+	TypePtr typeA = builder.genericType("A", toVector(typeB, typeC));
+
+	EXPECT_EQ("A<B<D>,C<D>>", toString(*typeA));
+
+	// start with root node R
+	NodeAddress ABD1(toVector<NodePtr>(typeA, typeB, typeD));
+	NodeAddress ABD2(toVector<NodePtr>(typeA, typeB, typeD));
+	NodeAddress ACD(toVector<NodePtr>(typeA, typeC, typeD));
+
+	EXPECT_EQ(ABD1, ABD2);
+	EXPECT_NE(ABD1, ACD);
+	EXPECT_NE(ABD2, ACD);
+
+	EXPECT_EQ(ABD1.getAddressedNode(), ACD.getAddressedNode());
+	EXPECT_EQ(ABD2.getAddressedNode(), ACD.getAddressedNode());
+}
+
+} // end namespace core
+} // end namespace insieme
+
