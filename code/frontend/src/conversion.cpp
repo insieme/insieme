66c8d11c5c1b2de7f84ed09bb81c079522d65ffb
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2010-10-22 09:35:31 +0200

    fixed compiler error

--- code/frontend/src/conversion.cpp
+++ code/frontend/src/conversion.cpp
@@ -812,8 +812,8 @@ public:
         core::TypePtr&& exprTy = convFact.convertType( *GET_TYPE_PTR(vecElemExpr) );
         core::ExpressionPtr&& idx = convFact.builder.literal(pos, exprTy); // FIXME! are you sure the type is exprTy?
         // if the type of the vector is a refType, we deref it
-        if(core::RefTypePtr&& baseTy = core::dynamic_pointer_cast<const core::RefType>(base))
-        	base =  builder.callExpr( baseTy->getElementType(), core::lang::OP_REF_DEREF_PTR, toVector(base) );
+        if(core::RefTypePtr&& baseTy = core::dynamic_pointer_cast<const core::RefType>(base->getType()))
+        	base =  convFact.builder.callExpr( baseTy->getElementType(), core::lang::OP_REF_DEREF_PTR, toVector(base) );
 
         core::ExpressionPtr&& retExpr = convFact.builder.callExpr(convFact.builder.refType(exprTy), core::lang::OP_SUBSCRIPT_PTR, toVector( base, idx ));
         END_LOG_EXPR_CONVERSION(retExpr);
