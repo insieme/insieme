42aba069de18b010fbd9d3e735a743a12892953c
Heiko Studt <studt@dps.uibk.ac.at>
2010-10-23 20:34:26 +0200

    minor change in commmented part.

--- scripts/cmake_heiko.bat
+++ scripts/cmake_heiko.bat
@@ -21,7 +21,7 @@ REM --- Call cmake
 cmake .. -G "Visual Studio 10" -DGTEST_ROOT=%GTEST_ROOT% -Dpthread_LIB=%PTHREAD_ROOT%\lib\pthreadVC2.lib -DLLVM_HOME="C:/Program Files (x86)/LLVM" -DGLOG_HOME=%GLOG_ROOT%\ -DXERCES_INCLUDE_DIR=%XERCES_ROOT_DIR%\include -DXERCES_C_LIBRARY=%XERCES_ROOT_DIR%\lib\xerces-c_static_3.lib -DLINKING_TYPE=STATIC
 @echo off
 
-rem DEBUG: cmake .. -G "Visual Studio 10" -DGTEST_ROOT=%GTEST_ROOT% -Dpthread_LIB=%PTHREAD_ROOT%\lib\pthreadVC2.lib -DLLVM_HOME=%LLVM_2_8% -DGLOG_HOME=%GLOG_ROOT%\ -DXERCES_INCLUDE_DIR=%XERCES_ROOT_DIR%\include -DXERCES_C_LIBRARY=%XERCES_ROOT_DIR%\lib\xerces-c_static_3.lib -DLINKING_TYPE=STATIC
+rem DEBUG: cmake .. -G "Visual Studio 10" -DGTEST_ROOT=%GTEST_ROOT% -Dpthread_LIB=%PTHREAD_ROOT%\lib\pthreadVC2.lib -DLLVM_HOME=%LLVM_2_8% -DGLOG_HOME=%GLOG_ROOT%\ -DXERCES_INCLUDE_DIR=%XERCES_ROOT_DIR%\include -DXERCES_C_LIBRARY=%XERCES_ROOT_DIR%\lib\xerces-c_static_3d.lib -DLINKING_TYPE=STATIC
 
 
 REM --- Wait for user
