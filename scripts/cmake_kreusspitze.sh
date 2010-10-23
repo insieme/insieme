03933484453cbd23a5f59dc8827dbfa98129ee99
Heiko Studt <studt@dps.uibk.ac.at>
2010-10-23 22:10:07 +0200

    Working cmake shell script, using paths on kreusspitze. For make you need the including ld_library_path and path.

--- scripts/cmake_kreusspitze.sh
+++ scripts/cmake_kreusspitze.sh
@@ -1,3 +1,23 @@
+######
+# LIBRARY_PATH
+
+#GCC 4.5.0
+export LD_LIBRARY_PATH=/software/lib-gcc450/:/software/gcc450/lib64:/software/gcc450/lib:$LD_LIBRARY_PATH
+
+#LLVM 2.8
+export LD_LIBRARY_PATH=/insieme-libs/llvm-2.8/lib/:$LD_LIBRARY_PATH
+
+
+######
+#Path
+
+#Python
+export PATH=$PATH:/home/spellegrini/shared/python-2.6.2/bin/
+
+
+
+######
+#Out of old run_cmake.sh:
 export GLOG_HOME=/home/spellegrini/shared/glog031
 export BOOST_ROOT=/software/boost1430
 export CC=/software/gcc450/bin/gcc
