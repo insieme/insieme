93e7f01076609bf496efca13146913a0a5b1a48f
Heiko Studt <studt@dps.uibk.ac.at>
2010-10-23 20:33:11 +0200

    Peter's run_cmake.sh to compile things on kreusspitze (without distcc for the moment).

---
+++ scripts/cmake_kreusspitze.sh
@@ -0,0 +1,9 @@
+export GLOG_HOME=/home/spellegrini/shared/glog031
+export BOOST_ROOT=/software/boost1430
+export CC=/software/gcc450/bin/gcc
+export CTEST_OUTPUT_ON_FAILURE=ON
+export CXX=/software/gcc450/bin/g++
+export GTEST_ROOT=/home/csaf7445/share/gtest/current
+export LLVM_HOME=/home/spellegrini/shared/llvm28/
+export XERCES_ROOT_DIR=/home/spellegrini/shared/xerces-c-3.1.1/
+/usr/local/cmake-2.8/bin/cmake ..
