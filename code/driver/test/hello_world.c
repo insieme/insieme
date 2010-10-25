5cb63a4bab2e595a79493f2009eef28abf431b37
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2010-10-25 11:19:22 +0200

    Fixed pack operation

--- code/driver/test/hello_world.c
+++ code/driver/test/hello_world.c
@@ -34,7 +34,7 @@
  * regarding third party software licenses.
  */
 
-extern int printf(char *, ...);
+extern int printf(const char, ...);
 
 int main(int argc, char* argv[]) {
 	#pragma omp parallel for default(none)
