9c6ad2e295d30e9afecf19e4df7d7e77571652b5
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2010-11-02 11:24:41 +0100

    fixed errors with recoursive function calls

--- code/frontend/src/conversion.cpp
+++ code/frontend/src/conversion.cpp
@@ -2430,8 +2430,7 @@ core::ExpressionPtr ConversionFactory::convertFunctionDecl(const clang::Function
 	retLambdaExpr = builder.recLambdaExpr(recVarRef, definition);
 
 	// Adding the lambda function to the list of converted functions
-	// ctx->lambdaExprCache.insert( std::make_pair(funcDecl, retLambdaExpr) );
-
+	ctx->lambdaExprCache.insert( std::make_pair(funcDecl, retLambdaExpr) );
 	// we also need to cache all the other recursive definition, so when we will resolve
 	// another function in the recursion we will not repeat the process again
 	std::for_each(components.begin(), components.end(),
@@ -2444,6 +2443,7 @@ core::ExpressionPtr ConversionFactory::convertFunctionDecl(const clang::Function
 			this->attachFuncAnnotations(func, fd);
 		}
 	);
+	this->attachFuncAnnotations(retLambdaExpr, funcDecl);
 	return retLambdaExpr;
 }
 
