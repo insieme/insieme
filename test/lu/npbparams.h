82d605162cc4a1a1ff4bf07442c5f9aba60da3c2
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2010-12-02 13:29:51 +0100

    add LU test case

---
+++ test/lu/npbparams.h
@@ -0,0 +1,25 @@
+/* CLASS = W */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+
+/* full problem size */
+#define	ISIZ1	33
+#define	ISIZ2	33
+#define	ISIZ3	33
+/* number of iterations and how often to print the norm */
+#define	ITMAX_DEFAULT	300
+#define	INORM_DEFAULT	300
+#define	DT_DEFAULT	1.5e-3
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "02 Dec 2010"
+#define NPBVERSION "2.3"
+#define CS1 "cc"
+#define CS2 "cc"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-O3 "
+#define CS6 "-lm"
+#define CS7 "randdp"
