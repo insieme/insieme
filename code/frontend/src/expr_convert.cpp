1c683b89508a862cc08f499e9bb141d171324786
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2010-12-02 10:46:35 +0100

    revert changes

--- code/frontend/src/expr_convert.cpp
+++ code/frontend/src/expr_convert.cpp
@@ -1211,9 +1211,7 @@ core::NodePtr ConversionFactory::convertFunctionDecl(const clang::FunctionDecl* 
 		assert(ctx.globalVar && ctx.globalStruct.second);
 
 		std::vector<core::StatementPtr> stmts;
-		stmts.push_back( builder.declarationStmt(ctx.globalVar,
-			builder.callExpr(ctx.globalVar->getType(), mgr.basic.getRefNew(), ctx.globalStruct.second ))
-		);
+		stmts.push_back( builder.declarationStmt(ctx.globalVar, builder.refVar( ctx.globalStruct.second )) );
 		std::copy(compStmt->getStatements().begin(), compStmt->getStatements().end(), std::back_inserter(stmts));
 		body = builder.compoundStmt(stmts);
 	}
