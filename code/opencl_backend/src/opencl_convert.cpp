a9641ffc1b033a65656bbf5e773b37bcd5904abb
Ivan Grasso <grasso@dps.uibk.ac.at>
2010-12-03 16:24:14 +0100

    changed OpenCL Backend

--- code/opencl_backend/src/opencl_convert.cpp
+++ code/opencl_backend/src/opencl_convert.cpp
@@ -36,335 +36,35 @@
 
 #include "insieme/opencl_backend/opencl_convert.h"
 
-//#include <glog/logging.h>
-#include "insieme/utils/logging.h"
-
-#include "insieme/core/types.h"
-
 namespace insieme {
-namespace opencl_backend {
-	
+namespace backend {
+namespace ocl {
+
 using namespace core;
+using namespace insieme::simple_backend;
 
-CodePtr FunctionManager::getFunction(const LambdaExprPtr& lambda, const Identifier& ident) {
-	auto codeIt = functionMap.find(ident);
-	if(codeIt != functionMap.end()) {
-		return codeIt->second;
-	}
+OclConvertVisitor::OclConvertVisitor(OclConversionContext& conversionContext) : ConvertVisitor(conversionContext) { }
 
-	auto funType = dynamic_pointer_cast<const FunctionType>(lambda->getType());
-	auto body = lambda->getBody();
-	bool isCompoundBody = !!dynamic_pointer_cast<const CompoundStmt>(body);
-
-	// generate a new function from the lambda expression
-	CodePtr cptr = std::make_shared<CodeFragment>(string("fundef_codefragment_") + ident.getName());
-	CodeStream& cs = cptr->getCodeStream();
-	// write the function header
-	cs << cc.getTypeMan().getTypeName(funType->getReturnType()) << " " << ident.getName() << "(";
-	// handle arguments
-	cs << join(", ", lambda->getParameterList(), [this](std::ostream& os, const VariablePtr& param) -> std::ostream& {
-		return (os << this->cc.getTypeMan().getTypeName(param->getType()) << " " << cc.getNameGen().getVarName(param));
-	});
-	cs << ")";
-	if(!isCompoundBody) cs << " {" << CodeStream::indR << "\n";
-	// generate the function body
-	ConvertVisitor visitor(cc, cptr);
-	visitor.visit(lambda->getBody());
-	if(!isCompoundBody) cs << CodeStream::indL << "\n}\n";
-	cs << "\n";
-	// insert into function map and return
-	functionMap.insert(std::make_pair(ident, cptr));
-	return cptr;
-}
-
-//TODO OCL: Kernels should not be literally exist - can we be sure?
-CodePtr FunctionManager::getFunctionLiteral(const core::FunctionTypePtr& type, const string& name) {
-	// TODO refactor duplication w/ above
-	auto ident = Identifier(string("fundecl_codefragment_") + name);
-	auto codeIt = functionMap.find(ident);
-	if(codeIt != functionMap.end()) {
-		return codeIt->second;
-	}
-	CodePtr cptr = std::make_shared<CodeFragment>(ident.getName());
-	CodeStream& cs = cptr->getCodeStream();
-	cs << cc.getTypeMan().getTypeName(type->getReturnType()) << " " << name << "(";
-//	auto argType = type->getArgumentType();
-//	if(auto tupleArgType = dynamic_pointer_cast<const TupleType>(argType)) {
-//		cs << join(", ", tupleArgType->getElementTypes(), [this](std::ostream& o, const TypePtr& cur) -> std::ostream& {
-//			return (o << this->cc.getTypeMan().getTypeName(cur));
-//		});
-//	} // TODO handle other argument types
-	cs << ");\n";
-	// insert into function map and return
-	functionMap.insert(std::make_pair(ident, cptr));
-	return cptr;
-}
-
-void FunctionManager::writeFunctionCall(const Identifier& funId, const LambdaExprPtr& ptr) {
-	 // TODO
+void OclConvertVisitor::visitLiteral(const LiteralPtr& ptr) {
+	// just print strange literal
+	cStr << ptr->getValue() << "_OK";
 }
 
 
-ConvertedCode ConversionContext::convert(const core::ProgramPtr& prog) {
+OclConversionContext::OclConversionContext(const NodePtr& target) : ConversionContext(target){ }
+	
+ConvertedCode OclConversionContext::convert(const core::ProgramPtr& prog) {
 	ConvertedCode converted(prog);
 	for_each(prog->getEntryPoints(), [&converted, this](const ExpressionPtr& ep) {
-		ConvertVisitor convVisitor(*this);
-		convVisitor.visit(ep);
-		converted.insert(std::make_pair(ep, convVisitor.getCode()));
+		OclConvertVisitor oclConvVisitor(*this);
+		oclConvVisitor.visit(ep);
+		CodePtr ptr = oclConvVisitor.getCode();
+		ptr->setDummy(true);
+		converted.insert(std::make_pair(ep, ptr));
 	});
 	return converted;
 }
 
-
-void ConvertVisitor::visitLambdaExpr(const LambdaExprPtr& ptr) {
-	string cFunName = cc.getNameGen().getName(ptr);
-	if(auto cnameAnn = ptr->getAnnotation(c_info::CNameAnnotation::KEY)) { // originally a named C function
-		cFunName = cnameAnn->getName();
-	}
-	defCodePtr->addDependency(cc.getFuncMan().getFunction(ptr, cFunName));
-	cStr << cFunName;
-}
-
-void ConvertVisitor::visitCallExpr(const CallExprPtr& ptr) {
-	const std::vector<ExpressionPtr>& args = ptr->getArguments();
-	auto funExp = ptr->getFunctionExpr();
-	// generic built in C operator handling
-	if(auto cOpAnn = funExp->getAnnotation(c_info::COpAnnotation::KEY)) { 
-		string op = cOpAnn->getOperator();
-		cStr << "(";
-		visit(args.front());
-		cStr << " " << op << " ";
-		visit(args.back());
-		cStr << ")";
-		return;
-	}
-	// special built in function handling -- TODO make more generic
-	if(auto literalFun = dynamic_pointer_cast<const Literal>(funExp)) {
-		//LOG(INFO) << "+++++++ visitCallExpr dyncastLit\n";
-		auto funName = literalFun->getValue();
-		//LOG(INFO) << "+++++++ val: " << funName << "\n";
-		if(funName == "ref.deref") {
-			// TODO decide whether no-op or *
-			visit(ptr->getArguments().front());
-			return;
-		} else if(funName == "subscript") {
-			visit(ptr->getArguments().front());
-			cStr << "[";
-			visit(ptr->getArguments().back());
-			cStr << "]";
-			return;
-		}
-	}
-	// non built-in handling
-	visit(funExp);
-	cStr << "(";
-	if(args.size()>0) {
-		visit(args.front());
-		for_each(args.cbegin()+1, args.cend(), [&, this](const ExpressionPtr& curArg) {
-			this->cStr << ", ";
-			this->visit(curArg);
-		});
-	}
-	cStr << ")";
-}
-
-void ConvertVisitor::visitDeclarationStmt(const DeclarationStmtPtr& ptr) {
-	auto var = ptr->getVariable();
-	// handle fixed size vectors of simple types (C arrays)
-	vector<unsigned> vecLengths;
-	auto innerType = var->getType();
-	if(auto innerRefType = dynamic_pointer_cast<const RefType>(innerType)) {
-		innerType = innerRefType->getElementType();
-	}
-	while(auto innerVecType = dynamic_pointer_cast<const VectorType>(innerType)) {
-		//LOG(INFO) << "+++++++ innerVec\n";
-		assert(innerVecType->getSize().isConcrete() && "Vectors with non-concrete size not yet supported");
-		vecLengths.push_back(innerVecType->getSize().getValue());
-		innerType = innerVecType->getElementType();
-		if(auto innerRefType = dynamic_pointer_cast<const RefType>(innerType)) {
-			innerType = innerRefType->getElementType();
-		}
-	}
-	if(!vecLengths.empty()) { // TODO check that innerType is "simple" enough to be part of C array
-		//LOG(INFO) << "+++++++ innerType " << innerType << "\n";
-		cStr << printTypeName(innerType) << " " << nameGen.getVarName(var);
-		for_each(vecLengths, [this](unsigned vl) { this->cStr << "[" << vl << "]"; });
-		// TODO initialization
-		return;
-	}
-	// standard handling
-	cStr << printTypeName(var->getType()) << " " << nameGen.getVarName(var) << " = ";
-	visit(ptr->getInitialization());
-}
-
-void ConvertVisitor::visitLiteral(const LiteralPtr& ptr) {
-	auto typePtr = ptr->getType();
-	const string& val = ptr->getValue();
-	if (ptr->getNodeManager().basic.isString(typePtr)) {
-		// TODO change once the decision is made how string literals should be represented int the AST
-		if(val.empty() || val[0] != '"' || val[val.length()-1] != '"') {
-			cStr << "\"" << val << "\"";
-		} else {
-			cStr << val;
-		}
-	} 
-	else if(auto funType = dynamic_pointer_cast<const FunctionType>(typePtr)) {
-		auto funLiteralDeclCode = cc.getFuncMan().getFunctionLiteral(funType, val); 
-		defCodePtr->addDependency(funLiteralDeclCode);
-		cStr << val;
-	}
-	else {
-		cStr << val;
-	}
-}
-
-void ConvertVisitor::visitReturnStmt( const ReturnStmtPtr& ptr )
-{
-	cStr << "return ";
-	if (ptr->getNodeManager().basic.isUnit(ptr->getReturnExpr()->getType())) {
-		visit(ptr->getReturnExpr());
-	}
-	cStr << ";";
-}
-
-void ConvertVisitor::visitCompoundStmt( const CompoundStmtPtr& ptr ) {
-	cStr << "{" << CodeStream::indR << "\n";
-	for_each(ptr->getChildList(), [&](const NodePtr& cur) { 
-		this->visit(cur); 
-		cStr << ";";
-		if(cur != ptr->getChildList().back()) cStr << "\n";
-	});
-	cStr << CodeStream::indL << "\n}";
-}
-
-
-
-string TypeManager::getTypeName(const core::TypePtr type) {
-	SimpleTypeConverter conv(cc.getNameGen());
-	return conv.visit(type);
-	// TODO handle complex types
-}
-
-string TypeManager::getTypeDecl(const core::TypePtr type) {
-	//TODO
-	return string();
-}
-
-CodePtr TypeManager::getTypeDefinition(const core::TypePtr type) {
-	//TODO
-	return CodePtr();
-}
-
-
-//TODO OCL: vector<4> etc pp
-string SimpleTypeConverter::visitGenericType(const GenericTypePtr& ptr) {
-	firstRef = true;
-	const core::lang::BasicGenerator& basic = ptr->getNodeManager().basic;
-
-	if(basic.isUnit(ptr)) {
-		return "void";
-	} else
-	if(basic.isInt(ptr)) {
-		string qualifier = basic.isUnsignedInt(ptr) ? "unsigned " : "";
-		auto intParm = ptr->getIntTypeParameter().front();
-		if(intParm.isConcrete()) switch(intParm.getValue()) {
-			case 1: return qualifier + "char";
-			case 2: return qualifier + "short";
-			case 4: return qualifier + "int";
-			case 8: return qualifier + "long"; // long long ?
-			default: return ptr->getName();
-		}
-	} else
-	if(basic.isBool(ptr)) {
-		return "bool";
-	} else
-	if(basic.isReal(ptr)) {
-		auto intParm = ptr->getIntTypeParameter().front();
-		if(intParm.isConcrete()) switch(intParm.getValue()) {
-			case 4: return "float";
-			case 8: return "double";
-			default: return ptr->getName();
-		}
-	} else
-	if(basic.isString(ptr)) {
-		return "string";
-	}
-	if(basic.isChar(ptr)) {
-		return "char";
-	}
-	if(basic.isVarList(ptr)) {
-		return "...";
-	}
-	//assert(0 && "Unhandled generic type.");
-	return string("[[unhandled_simple_type: ") + ptr->getName() + "]]";
-}
-
-string SimpleTypeConverter::visitRefType(const RefTypePtr& ptr) {
-	if(firstRef) {
-		firstRef = false;
-		return visit(ptr->getElementType());
-	}
-	return visit(ptr->getElementType()) + "*";
-}
-
-string SimpleTypeConverter::visitStructType(const StructTypePtr& ptr) {
-	firstRef = true;
-	string structName;
-	if(auto annotation = ptr->getAnnotation(c_info::CNameAnnotation::KEY)) {
-		structName = annotation->getName();
-	} else {
-		structName = nameGen.getName(ptr, "unnamed_struct");
-	}
-	std::ostringstream ret; // TODO use code stream
-	ret << "struct " << structName << " {\n";
-	for_each(ptr->getEntries(), [&ret, this](const NamedCompositeType::Entry& entry) {
-		ret << this->visit(entry.second) << " " << entry.first.getName() << ";";
-	});
-	return ret.str();
-}
-
-
-const ProgramPtr& ConvertedCode::getProgram() const {
-	return fromProg;
-}
-
-
-string NameGenerator::getName( const NodePtr& ptr, const char* fragment /*= "unnamed"*/ ) {
-	auto it = nameMap.find(ptr);
-	if(it != nameMap.end()) return string("__insieme_") + fragment + "_" + it->second;
-	// generate a new name string
-	std::stringstream name;
-	switch(ptr->getNodeCategory()) {
-	case NC_Support:
-		name << "supp"; break;
-	case NC_Type:
-		name << "type"; break;
-	case NC_Expression:
-		name << "expr"; break;
-	case NC_Statement:
-		name << "stat"; break;
-	case NC_Program:
-		name << "prog"; break;
-	}
-	name << "_" << num++;
-	nameMap.insert(make_pair(ptr, name.str()));
-	return getName(ptr, fragment);
-}
-
-string NameGenerator::getVarName(const VariablePtr& var) {
-	if(auto annotation = var->getAnnotation(c_info::CNameAnnotation::KEY)) {
-		return annotation->getName();
-	} else {
-		return string("unnamed_var_") + toString(var->getId());
-	}
-}
-
-}
-}
-
-std::ostream& operator<<(std::ostream& out, const insieme::opencl_backend::ConvertedCode& code) {
-	for_each(code.getProgram()->getEntryPoints(), [&](const insieme::core::ExpressionPtr& ep) {
-		out << "---\n" << *code.find(ep);
-	});
-	return out;
-}
+} // namespace ocl
+} // namespace backend
+} // namespace insieme
