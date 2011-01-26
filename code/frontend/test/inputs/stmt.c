fb7d4d3298162ee417b36c9118673417fff87d2a
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2011-01-26 13:37:58 +0100

    test for lazy evaluation

--- code/frontend/test/inputs/stmt.c
+++ code/frontend/test/inputs/stmt.c
@@ -162,9 +162,9 @@ void if_stmt_test() {
 	"ite((( *v1)==0), [v1]fun[ref<int<4>> v4]()return (( *v4)+1), [v1]fun[ref<int<4>> v2]()return (( *v2)-1))"
 	a == 0 ? a+1 : a-1;
 
-	//#pragma test \
-	"if((CAST<bool>(v1)&&[v1]fun[ref<int<4>> v2]()int.ne(( *v2), 1))) { { };} else { }"
-	if(cond && cond != 1) {	; }
+	#pragma test \
+	"if(((( *v1)>0)&&[v1]fun[ref<int<4>> v2]()return int.ne(( *v2), 1))) { { };} else { }"
+	if(cond > 0 && cond != 1) {	; }
 }
 
 void for_stmt_test() {
