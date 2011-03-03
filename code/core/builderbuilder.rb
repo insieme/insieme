48a4b5fb7ad347adb9d81dfbee0c8e49a0da0849
Peter Thoman <peter.thoman@uibk.ac.at>
2011-03-03 10:33:46 +0100

    builderbuilder: now only rebuilds inc files on changes

--- code/core/builderbuilder.rb
+++ code/core/builderbuilder.rb
@@ -27,28 +27,40 @@ Dir["**/*.h"].each { |header|
 
 signatures = signatures.sort_by { |s| s[0] }
 
-File.open("include/insieme/core/ast_builder.inl", "w+") { |inlFile|
-File.open("src/ast_builder_impl.inl", "w+") { |inlFileImpl|
-File.open("include/insieme/core/ast_builder_decls.inl", "w+") { |inlDeclFile|
-	signatures.each { |sig|
-		typeName = sig[0][0..-4]
-		params = sig[2]
-		funDecl = "#{typeName.sub(/[A-Z]/) {|c| c.downcase}}(#{params})"
-		inlFile.puts("#{sig[0].ljust(30)} #{funDecl} const;")
-		if(params)
-			# remove default param assignments
-			params = params.split(",").map { |param| param.sub(/=.*/,"").strip }.join(", ")
+sigcache = ""
+if File.exists?("signatures.cache")
+	sigcache = IO.read("signatures.cache").strip
+end
+newsigs = signatures.to_s.strip
+
+if sigcache == newsigs
+	puts "Builderbuilder.rb : no change in signatures detected, skipping file generation."
+else 
+	File.open("signatures.cache", "w+") { |sigfile| sigfile.puts(newsigs) }
+
+	File.open("include/insieme/core/ast_builder.inl", "w+") { |inlFile|
+	File.open("src/ast_builder_impl.inl", "w+") { |inlFileImpl|
+	File.open("include/insieme/core/ast_builder_decls.inl", "w+") { |inlDeclFile|
+		signatures.each { |sig|
+			typeName = sig[0][0..-4]
+			params = sig[2]
 			funDecl = "#{typeName.sub(/[A-Z]/) {|c| c.downcase}}(#{params})"
-			paramNames = params.split(",").map { |param| param[/\w* *\z/].strip }.join(", ")
-			inlFileImpl.puts("#{sig[0].ljust(18)} ASTBuilder::#{funDecl} const { return #{typeName}::get(manager, #{paramNames}); }")
-		else
-			inlFileImpl.puts("#{sig[0].ljust(18)} ASTBuilder::#{funDecl} const { return #{typeName}::get(manager); }")
-		end
-		inlDeclFile.puts("class #{typeName}; typedef AnnotatedPtr<const #{typeName}> #{sig[0]};")
+			inlFile.puts("#{sig[0].ljust(30)} #{funDecl} const;")
+			if(params)
+				# remove default param assignments
+				params = params.split(",").map { |param| param.sub(/=.*/,"").strip }.join(", ")
+				funDecl = "#{typeName.sub(/[A-Z]/) {|c| c.downcase}}(#{params})"
+				paramNames = params.split(",").map { |param| param[/\w* *\z/].strip }.join(", ")
+				inlFileImpl.puts("#{sig[0].ljust(18)} ASTBuilder::#{funDecl} const { return #{typeName}::get(manager, #{paramNames}); }")
+			else
+				inlFileImpl.puts("#{sig[0].ljust(18)} ASTBuilder::#{funDecl} const { return #{typeName}::get(manager); }")
+			end
+			inlDeclFile.puts("class #{typeName}; typedef AnnotatedPtr<const #{typeName}> #{sig[0]};")
+		}
+	} 
+	} 
 	}
-} 
-} 
-}
+end
 
 # vector<std::pair<Identifier, TypePtr>> Entries
 # vector<TypePtr> ElementTypeList
