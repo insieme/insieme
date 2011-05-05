f6a2ef64e2fd0c4952e6f546d45ea826ad5c3c41
Herbert <herbert@dps.uibk.ac.at>
2011-05-05 13:28:20 +0200

    Added File IO test case

---
+++ test/file_io/file_io.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+void printFile(char* name) {
+	FILE *fp;
+	printf("Opening file %s ...\n", name);
+	fp=fopen(name, "r");
+	if (fp == 0) {
+		printf("File not found!\n");
+		return;
+	}
+
+	printf("Reading file ...\n");
+	int cur = fgetc(fp);
+	while(cur!=EOF) {
+		printf("%c", cur);
+		cur = fgetc(fp);
+	}
+	printf("Closing file ...\n");
+	fclose(fp);
+}
+
+int main() {
+
+	printFile("file.txt");
+	printFile("file2.txt");
+	printf("Done.\n");
+}
