760a915a6392b166e69c3579b71475acd0ef85b6
Herbert <herbert@dps.uibk.ac.at>
2011-05-13 10:58:55 +0200

    Fixed free error within stack example (preliminary) and made tests sensitive for return values of the test cases

--- test/multi_tu/b.c
+++ test/multi_tu/b.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
+
 #include "a.h"
 #include "b.h"
+
 
 int g(int a) {
 	return a-1;
 }
 
 int main(int argc, char** argv) {
-	return f(argc, argc);
+	printf("res=%d\n", f(argc, argc));
 }
