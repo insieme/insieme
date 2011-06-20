a6bd62be1fe83e7ec74090188c32472c7e237b55
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2011-06-20 19:19:57 +0000

    restoring main

--- code/driver/src/main.cpp
+++ code/driver/src/main.cpp
@@ -192,8 +192,8 @@ int main(int argc, char** argv) {
 				LOG(INFO) << timer;
 			}
 		
-			insieme::analysis::scop::ScopList sl = insieme::analysis::scop::mark(program);
-			std::cout << "SCOP Analysis: " << sl.size() << std::endl;
+			//insieme::analysis::scop::ScopList sl = insieme::analysis::scop::mark(program);
+			//std::cout << "SCOP Analysis: " << sl.size() << std::endl;
 
 			InverseStmtMap stmtMap;
 			if(CommandLineOptions::PrettyPrint || !CommandLineOptions::DumpIR.empty()) {
