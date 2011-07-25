e6c4a96710c2ee3317f1d24d6047afedf2e5ec79
Herbert <herbert@dps.uibk.ac.at>
2011-07-25 13:45:16 +0200

    Updated reference files for test cases

--- test/hello_world/hello_world.c
+++ test/hello_world/hello_world.c
@@ -7,8 +7,9 @@ void fun(void* x) {}
 int main(int argc, char* argv[]) {
 	#pragma omp parallel for default(none)
 	{
-	printf("Hallo Insieme, \n\t\"the number %d in compilers!\"", 1);
+	printf("Hallo Insieme, \n\t\"the number %d in compilers!\"\n", 1);
+	//printf("Name of executable: %s\n", argv[0]);
 	}
-	fun(NULL);
+	//fun(NULL);
 	return 0;
 }
