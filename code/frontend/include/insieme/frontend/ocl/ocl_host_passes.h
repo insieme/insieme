5bd2a9a33db897c223765a4c3f4b73e32008bded
klaus <klaus@dps.uibk.ac.at>
2011-08-12 11:00:37 +0200

    OpenCore example running through

--- code/frontend/include/insieme/frontend/ocl/ocl_host_passes.h
+++ code/frontend/include/insieme/frontend/ocl/ocl_host_passes.h
@@ -36,9 +36,8 @@
 
 #pragma once
 
-#include "insieme/core/transform/node_mapper_utils.h"
 #include "insieme/core/ast_builder.h"
-#include "insieme/core/parser/ir_parse.h"
+#include "insieme/core/transform/node_mapper_utils.h"
 
 #include "insieme/frontend/program.h"
 #include "insieme/utils/logging.h"
@@ -47,110 +46,6 @@ namespace insieme {
 namespace frontend {
 namespace ocl {
 
-
-// enums corresponding to the flags in clCreateBuffer
-enum CreateBufferFlags {
-		CL_MEM_READ_WRITE = 0,
-		CL_MEM_WRITE_ONLY,
-		CL_MEM_READ_ONLY,
-		CL_MEM_USE_HOST_PTR,
-		CL_MEM_ALLOC_HOST_PTR,
-		CL_MEM_COPY_HOST_PTR,
-		size
-};
-
-/**
- * Class to visit the AST and return the value of a certain variable, holding the path to a OpenCL kernel, if it exists at all
- */
-class KernelCodeRetriver: public core::ASTVisitor<bool> {
-	const core::VariablePtr& pathToKernelFile; // Variable to look for
-	const core::NodePtr& breakingStmt; // place where the path would be needed, can stop searching there
-	const core::ASTBuilder builder;
-	string path;
-
-	bool visitNode(const core::NodePtr& node);
-	bool visitCallExpr(const core::CallExprPtr& callExpr);
-	bool visitDeclarationStmt(const core::DeclarationStmtPtr& decl);
-
-public:
-	KernelCodeRetriver(const core::VariablePtr lookFor,
-			const core::NodePtr& stopAt, core::ASTBuilder build) :
-		ASTVisitor<bool> (false), pathToKernelFile(lookFor),
-				breakingStmt(stopAt), builder(build) {
-	}
-	string getKernelFilePath() {
-		return path;
-	}
-};
-
-/**
- * This struct holds inspire representations of OpenCL built-in host functions
- */
-struct Ocl2Inspire {
-private:
-	core::parse::IRParser parser;
-
-public:
-	Ocl2Inspire(core::NodeManager& mgr) :
-		parser(mgr) {
-	}
-
-	bool extractSizeFromSizeof(const core::ExpressionPtr& arg,
-			core::ExpressionPtr& size, core::TypePtr& type);
-
-	core::ExpressionPtr getClCreateBuffer(bool copyHostPtr);
-	core::ExpressionPtr getClWriteBuffer();
-	core::ExpressionPtr getClWriteBufferFallback();
-	core::ExpressionPtr getClReadBuffer();
-	core::ExpressionPtr getClReadBufferFallback();
-};
-
-/**
- * This class allows replaces a call to an OpenCL built-in function to an INSPIRE one
- */
-class Handler {
-protected:
-	core::ProgramPtr kernels;
-public:
-	Handler(core::ASTBuilder& build) {
-		kernels = core::Program::create(build.getNodeManager());
-	}
-
-	virtual core::NodePtr handleNode(core::CallExprPtr node) =0;
-
-	const vector<core::ExpressionPtr>& getKernels() {
-		return kernels->getEntryPoints();
-	}
-};
-
-/*
- * Provides templated child functions of the Handler class. The template argument determines which
- * OpenCL built-in funciton is handled
- */
-template<typename Lambda>
-class LambdaHandler: public Handler {
-	// flag indicating if the definition of the actual function has already been added to the program
-	static bool defAdded;
-	core::ASTBuilder& builder;
-
-	const char* fct;
-	Lambda body;
-
-public:
-	LambdaHandler(core::ASTBuilder& build, const char* fun, Lambda lambda) :
-		Handler(build), builder(build), fct(fun), body(lambda) {
-	}
-
-	// creating a shared pointer to a LambdaHandler
-
-	core::NodePtr handleNode(core::CallExprPtr node) {
-		LOG(DEBUG)
-			<< "Handling node " << node << std::endl;
-
-		return body(node, kernels);
-	}
-
-};
 
 /**
  * This specialized hasher hashes array accesses to the same variable to the same bin
@@ -229,140 +124,14 @@ std::cout << "yExpr: " << yCall << std::endl;
 		return false;
 	}
 };
-
-
-typedef std::shared_ptr<Handler> HandlerPtr;
-typedef boost::unordered_map<string, HandlerPtr, boost::hash<string> > HandlerTable;
 typedef insieme::utils::map::PointerMap<core::VariablePtr, core::VariablePtr> ClmemTable;
 typedef boost::unordered_map<core::ExpressionPtr, std::vector<core::ExpressionPtr>, hash_target_specialized, equal_variables> KernelArgs;
 //typedef std::map<core::ExpressionPtr, std::vector<core::ExpressionPtr>, equal_variables> KernelArgs;
 //typedef insieme::utils::map::PointerMap<core::ExpressionPtr, std::vector<core::ExpressionPtr> > KernelArgs;
 typedef boost::unordered_map<string, core::ExpressionPtr, boost::hash<string> > KernelNames;
-typedef insieme::utils::map::PointerMap<core::ExpressionPtr, core::LambdaExprPtr> KernelLambdas;
+typedef boost::unordered_map<core::ExpressionPtr, core::LambdaExprPtr, hash_target_specialized, equal_variables> KernelLambdas;
 typedef insieme::utils::map::PointerMap<core::ExpressionPtr, vector<core::DeclarationStmtPtr> > LocalMemDecls;
 
-template<typename Lambda>
-HandlerPtr make_handler(core::ASTBuilder& builder, const char* fct,
-		Lambda lambda) {
-	return std::make_shared<LambdaHandler<Lambda> >(builder, fct, lambda);
-}
-
-#define ADD_Handler(builder, fct, BODY) \
-    handles.insert(std::make_pair(fct, make_handler(builder, fct, [&](core::CallExprPtr node, core::ProgramPtr& kernels){ BODY }))).second;
-
-/*
- * First pass when translating a program OpenCL to IR
- * Responsible for:
- * - finding path to kernel file (in-code kernel code strings not supported yet)
- * - find cl_mem variable replacements and store them in the cl_mems map
- * - store names of called kernels in kernelNames
- * - store arguments of kernels in kernelArgs (the kernel name is the key)
- * - translate kernel code to IR
- * - store all entry points to kernels in kernelEntries
- * - call Ocl2Inspire's functions when encountering a function which needs IR replacement (e.g. clEnqueueWriteBuffer)
- * No out of order queue supported yet
- */
-class HostMapper: public core::transform::CachedNodeMapping {
-	core::ASTBuilder& builder;
-
-	HandlerTable handles;
-	ClmemTable cl_mems;
-	Ocl2Inspire o2i;
-	core::ProgramPtr& mProgram;
-	KernelArgs kernelArgs;
-	KernelNames kernelNames;
-	vector<core::ExpressionPtr> kernelEntries;
-	LocalMemDecls localMemDecls;
-	insieme::utils::map::PointerMap<core::NodePtr, core::NodePtr> replacements;
-
-	// check if the call is a call to ref.assign
-	core::CallExprPtr checkAssignment(const core::NodePtr& oldCall);
-
-	// needed to be able to work with handlers, identified by prefixes of function names
-	HandlerPtr& findHandler(const string& fctName);
-
-	// functions to get set of flags out of an Expression
-	// recursiveFlagCheck is designed to be called form getFlags only
-	template<typename Enum>
-	void recursiveFlagCheck(const core::ExpressionPtr& flagExpr, std::set<Enum>& flags);
-	template<typename Enum>
-	std::set<Enum> getFlags(const core::ExpressionPtr& flagExpr);
-
-	bool translateClCreateBuffer(const core::ExpressionPtr& var, const core::CallExprPtr& fun, const core::CallExprPtr& newRhs, core::NodePtr& ret);
-	bool handleClCreateKernel(const core::ExpressionPtr& expr, const core::ExpressionPtr& call, const core::ExpressionPtr& fieldName);
-	bool lookForKernelFilePragma(const core::TypePtr& type, const core::ExpressionPtr& createProgramWithSource);
-
-public:
-	HostMapper(core::ASTBuilder& build, core::ProgramPtr& program);
-
-	const core::NodePtr resolveElement(const core::NodePtr& element);
-
-	ClmemTable& getClMemMapping() { return cl_mems; }
-	const vector<core::ExpressionPtr>& getKernels() { return kernelEntries;	}
-	KernelArgs& getKernelArgs() { return kernelArgs; }
-	KernelNames& getKernelNames() { return kernelNames; }
-	LocalMemDecls& getLocalMemDecls() {	return localMemDecls; }
-	insieme::utils::map::PointerMap<core::NodePtr, core::NodePtr>& getReplacements() { return replacements; }
-};
-
-/*
- * Second pass when translating a program OpenCL to IR
- * Responsible for:
- * - connecting the names of kernel functions with the IR entry points (= LambdaExpr)
- */
-class Host2ndPass {
-	KernelNames& kernelNames;
-	KernelLambdas kernelLambdas;
-	ClmemTable& cl_mems;
-	const core::ASTBuilder& builder;
-
-public:
-	Host2ndPass(KernelNames& oclKernelNames, ClmemTable& clMemTable, core::ASTBuilder& build) :
-		kernelNames(oclKernelNames), cl_mems(clMemTable), builder(build) {
-	}
-	void mapNamesToLambdas(const vector<core::ExpressionPtr>& kernelEntries);
-
-	ClmemTable& getCleanedStructures();
-
-	KernelNames& getKernelNames() {
-		return kernelNames;
-	}
-	KernelLambdas& getKernelLambdas() {
-		return kernelLambdas;
-	}
-};
-
-/*
- * First pass when translating a program OpenCL to IR
- * Responsible for:
- * - replace the OpenCL cl_mem vars/structs containing cl_mem vars with IR variables (array<...>)
- * - remove instances of unused variables (cl_program, cl_kernel, ...)
- * - replace calls to cl_enqueueNDRangeKernls with function calls to the correct LambdaExpr with the appropriate arguments
- */
-class HostMapper3rdPass: public core::transform::CachedNodeMapping {
-	const core::ASTBuilder& builder;
-	ClmemTable& cl_mems;
-	KernelArgs& kernelArgs;
-	LocalMemDecls& localMemDecls;
-	KernelNames& kernelNames;
-	KernelLambdas& kernelLambdas;
-	insieme::utils::map::PointerMap<core::NodePtr, core::NodePtr> replacements;
-
-	// takes the expression size which describes the work size for the clEnqueueNDRange and embed it in an IR function which returns a
-	// vector<uint<4>, 3>, always awaited by the kernel function. The elements with index greater or equal to workDim will always be set
-	// to 1, regardless of the argument size
-	const core::ExpressionPtr anythingToVec3(core::ExpressionPtr workDim,
-			core::ExpressionPtr size);
-
-public:
-	HostMapper3rdPass(const core::ASTBuilder build, ClmemTable& clMemTable, KernelArgs& oclKernelArgs, LocalMemDecls& oclLocalMemDecls,
-			KernelNames& oclKernelNames, KernelLambdas& oclKernelLambdas, insieme::utils::map::PointerMap<core::NodePtr, core::NodePtr> oclReplacements) :
-		builder(build), cl_mems(clMemTable), kernelArgs(oclKernelArgs),	localMemDecls(oclLocalMemDecls), kernelNames(oclKernelNames),
-			kernelLambdas(oclKernelLambdas), replacements(oclReplacements) { }
-
-	const core::NodePtr resolveElement(const core::NodePtr& element);
-
-};
 
 } //namespace ocl
 } //namespace frontend
