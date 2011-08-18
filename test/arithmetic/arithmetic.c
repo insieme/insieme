3813966be82fe703c932894b1b8658bc694fa85d
Herbert <herbert@dps.uibk.ac.at>
2011-08-18 10:00:34 +0200

    Added arithmetic test to integration test suite

---
+++ test/arithmetic/arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	unsigned a = 10;
+	int b = 3;
+
+	printf("a=%d\n", a);
+	printf("b=%d\n", b);
+	printf("a+b=%d\n", a+b);
+	printf("a-b=%d\n", a-b);
+	printf("a*b=%d\n", a*b);
+	printf("a/b=%d\n", a/b);
+	printf("a%b=%d\n", a%b);
+
+	return 0;
+}
+
