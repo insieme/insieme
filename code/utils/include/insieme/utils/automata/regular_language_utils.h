35a5d39e71ef1d60b8ae86a9b5315839f841b33f
Herbert <herbert@dps.uibk.ac.at>
2011-09-30 16:12:57 +0200

    Added automata utils to support pattern matching

---
+++ code/utils/include/insieme/utils/automata/regular_language_utils.h
@@ -0,0 +1,256 @@
+/**
+ * Copyright (c) 2002-2013 Distributed and Parallel Systems Group,
+ *                Institute of Computer Science,
+ *               University of Innsbruck, Austria
+ *
+ * This file is part of the INSIEME Compiler and Runtime System.
+ *
+ * We provide the software of this file (below described as "INSIEME")
+ * under GPL Version 3.0 on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ *
+ * If you require different license terms for your intended use of the
+ * software, e.g. for proprietary commercial or industrial use, please
+ * contact us at:
+ *                   insieme@dps.uibk.ac.at
+ *
+ * We kindly ask you to acknowledge the use of this software in any
+ * publication or other disclosure of results by referring to the
+ * following citation:
+ *
+ * H. Jordan, P. Thoman, J. Durillo, S. Pellegrini, P. Gschwandtner,
+ * T. Fahringer, H. Moritsch. A Multi-Objective Auto-Tuning Framework
+ * for Parallel Codes, in Proc. of the Intl. Conference for High
+ * Performance Computing, Networking, Storage and Analysis (SC 2012),
+ * IEEE Computer Society Press, Nov. 2012, Salt Lake City, USA.
+ *
+ * All copyright notices must be kept intact.
+ *
+ * INSIEME depends on several third party software packages. Please 
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
+ * regarding third party software licenses.
+ */
+
+#pragma once
+
+#include <memory>
+
+#include "insieme/utils/automata/automata.h"
+
+
+namespace insieme {
+namespace utils {
+namespace automata {
+
+	/**
+	 * A manipulation wrapper for automates enabling the application
+	 * of regular expression like connectives.
+	 */
+	template<
+		typename Pattern=char,
+		typename Matcher = std::equal_to<Pattern>
+	>
+	class RegularLanguage {
+
+		/**
+		 * A typedef for the state type.
+		 */
+		typedef typename Automata<Pattern,Matcher>::state_type State;
+
+		/**
+		 * The automata used to recognize the language of this regex.
+		 * It is stored using a shared pointer since the automata is not copyable.
+		 */
+		std::shared_ptr<Automata<Pattern,Matcher>> automata;
+
+	public:
+
+		/**
+		 * Create a regular language consisting solely of the given pattern.
+		 *
+		 * @param pattern the only word within the language
+		 */
+		RegularLanguage(const Pattern& pattern) {
+
+			automata = std::make_shared<Automata<Pattern,Matcher>>();
+
+			// create an automata accepting a single pattern
+			State s1 = automata->getInitialState();
+			State s2 = automata->getNewState();
+
+			automata->addTransition(s1, pattern, s2);
+			automata->setFinalState(s2);
+		}
+
+		/**
+		 * Obtains access to the internally stored automata.
+		 */
+		Automata<Pattern,Matcher>& getAutomata() {
+			return *automata;
+		}
+
+		/**
+		 * Obtains access to the internally stored automata.
+		 */
+		const Automata<Pattern,Matcher>& getAutomata() const {
+			return *automata;
+		}
+
+	};
+
+	/**
+	 * Creates a new regular language consisting of solely the given word / pattern.
+	 *
+	 * @param pattern the pattern to be covered by the resulting language.
+	 */
+	template<typename Pattern, typename Matcher = std::equal_to<Pattern>>
+	RegularLanguage<Pattern> single(const Pattern& pattern) {
+		return RegularLanguage<Pattern, Matcher>(pattern);
+	}
+
+	/**
+	 * Connects the two given languages and creates a new language covering
+	 * all sequences a'b' where a' was part of the first language and b' part of
+	 * the second language.
+	 *
+	 * @param langA the first language, will be invalidated in the process
+	 * @param langB the second language, will also be invalidated in the process
+	 * @return a new language forming the concatenation of the given languages
+	 */
+	template<typename Pattern, typename Matcher = std::equal_to<Pattern>>
+	RegularLanguage<Pattern> sequence(RegularLanguage<Pattern> langA, RegularLanguage<Pattern> langB) {
+		typedef typename Automata<Pattern>::state_type State;
+
+		Automata<Pattern>& a1 = langA.getAutomata();
+		Automata<Pattern>& a2 = langB.getAutomata();
+
+		// extract start/stop states
+		State s1 = a1.getInitialState();
+		State s2 = a2.getInitialState();
+
+		State f1 = *a1.getFinalStates().begin();
+		State f2 = *a2.getFinalStates().begin();
+
+		// copy states
+		a1.import(a2);
+
+		// concatenate automata
+		a1.addEpsilonTransition(f1,s2);
+
+		// set up new start/end states
+		a1.setInitialState(s1);
+		a1.setFinalState(f2);
+
+		return langA;
+	}
+
+	/**
+	 * A connector supporting the concatenation of multiple languages using
+	 * consecutive applications of the sequence operator.
+	 *
+	 * @param langA the first language, will be invalidated in the process
+	 * @param langB the second language, will also be invalidated in the process
+	 * @param rest the remaining languages
+	 * @return a new language forming the concatenation of the given languages
+	 */
+	template<typename Pattern, typename Matcher = std::equal_to<Pattern>, typename ... L>
+	RegularLanguage<Pattern> sequence(RegularLanguage<Pattern> langA, RegularLanguage<Pattern> langB, L ... rest) {
+		return sequence(sequence(langA, langB), rest ...);
+	}
+
+
+	/**
+	 * A connector forming the union of the given languages.
+	 *
+	 * @param langA the first language
+	 * @param langB the second language
+	 * @return a language accepting every word of both languages.
+	 */
+	template<typename Pattern>
+	RegularLanguage<Pattern> alternativ(RegularLanguage<Pattern> langA, RegularLanguage<Pattern> langB) {
+		typedef typename Automata<Pattern>::state_type State;
+
+		Automata<Pattern>& a1 = langA.getAutomata();
+		Automata<Pattern>& a2 = langB.getAutomata();
+
+		// extract start/stop states
+		State s1 = a1.getInitialState();
+		State s2 = a2.getInitialState();
+
+		State f1 = *a1.getFinalStates().begin();
+		State f2 = *a2.getFinalStates().begin();
+
+		// copy states
+		a1.import(a2);
+
+		// create new start / final state
+		State s = a1.getNewState();
+		State f = a1.getNewState();
+
+		a1.setInitialState(s);
+		a1.setFinalState(f);
+
+		// link it
+		a1.addEpsilonTransition(s, s1);
+		a1.addEpsilonTransition(s, s2);
+		a1.addEpsilonTransition(f1, f);
+		a1.addEpsilonTransition(f2, f);
+
+		return langA;
+	}
+
+	/**
+	 * A connector supporting the combination of multiple languages using
+	 * consecutive applications of the alternative operator.
+	 *
+	 * @param langA the first language, will be invalidated in the process
+	 * @param langB the second language, will also be invalidated in the process
+	 * @param rest the remaining languages
+	 * @return a new language forming the union of the given languages
+	 */
+	template<typename Pattern, typename Matcher = std::equal_to<Pattern>, typename ... L>
+	RegularLanguage<Pattern> alternativ(RegularLanguage<Pattern> langA, RegularLanguage<Pattern> langB, L ... rest) {
+		return alternativ(alternativ(langA, langB), rest ...);
+	}
+
+
+	/**
+	 * A connector creating a language of all words representing repetitions
+	 * of the given language.
+	 *
+	 * @param lang the language to be repeated
+	 * @return the requested language.
+	 */
+	template<typename Pattern>
+	RegularLanguage<Pattern> repetition(RegularLanguage<Pattern> lang) {
+		typedef typename Automata<Pattern>::state_type State;
+
+		Automata<Pattern>& a = lang.getAutomata();
+
+		// extract start/stop states
+		State s = a.getInitialState();
+		State f = *a.getFinalStates().begin();
+
+		// create new start and end state
+		State ns = a.getNewState();
+		State nf = a.getNewState();
+
+		a.setInitialState(ns);
+		a.setFinalState(nf);
+
+		// add new transitions
+		a.addEpsilonTransition(ns,s);
+		a.addEpsilonTransition(f,nf);
+		a.addEpsilonTransition(f,s);
+		a.addEpsilonTransition(ns,nf);
+
+		return lang;
+	}
+
+
+} // end namespace automata
+} // end namespace utils
+} // end namespace insieme
---
+++ code/utils/test/automata/automata_test.cc
@@ -0,0 +1,323 @@
+/**
+ * Copyright (c) 2002-2013 Distributed and Parallel Systems Group,
+ *                Institute of Computer Science,
+ *               University of Innsbruck, Austria
+ *
+ * This file is part of the INSIEME Compiler and Runtime System.
+ *
+ * We provide the software of this file (below described as "INSIEME")
+ * under GPL Version 3.0 on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ *
+ * If you require different license terms for your intended use of the
+ * software, e.g. for proprietary commercial or industrial use, please
+ * contact us at:
+ *                   insieme@dps.uibk.ac.at
+ *
+ * We kindly ask you to acknowledge the use of this software in any
+ * publication or other disclosure of results by referring to the
+ * following citation:
+ *
+ * H. Jordan, P. Thoman, J. Durillo, S. Pellegrini, P. Gschwandtner,
+ * T. Fahringer, H. Moritsch. A Multi-Objective Auto-Tuning Framework
+ * for Parallel Codes, in Proc. of the Intl. Conference for High
+ * Performance Computing, Networking, Storage and Analysis (SC 2012),
+ * IEEE Computer Society Press, Nov. 2012, Salt Lake City, USA.
+ *
+ * All copyright notices must be kept intact.
+ *
+ * INSIEME depends on several third party software packages. Please 
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
+ * regarding third party software licenses.
+ */
+
+#include <gtest/gtest.h>
+
+#include "insieme/utils/automata/automata.h"
+#include "insieme/utils/test/test_utils.h"
+
+namespace insieme {
+namespace utils {
+namespace automata {
+
+using namespace set;
+
+typedef Automata<>::state_type State;
+
+TEST(Automata, Basic) {
+
+	Automata<> a;
+
+	EXPECT_EQ(1, a.getNumStates());
+	EXPECT_TRUE(a.containsState(a.getInitialState()));
+
+	State s = a.getNewState();
+	a.setInitialState(s);
+
+	EXPECT_EQ(s, a.getInitialState());
+
+	EXPECT_FALSE(a.isFinalState(s));
+
+	a.setFinalStates(s);
+	EXPECT_TRUE(a.isFinalState(s));
+
+}
+
+TEST(Automata, Transitions) {
+
+	// create a new automata accepting a single X
+	Automata<> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+
+	a.setInitialState(s1);
+	a.setFinalState(s2);
+
+	a.addTransition(s1, 'X', s2);
+
+	string print = toDotGraph(a);
+
+	EXPECT_PRED2(containsSubString, print, "digraph");
+	EXPECT_PRED2(containsSubString, print, "label=\"X\"");
+
+	a.addEpsilonTransition(s2,s1);
+	print = toDotGraph(a);
+
+	EXPECT_PRED2(containsSubString, print, "digraph");
+	EXPECT_PRED2(containsSubString, print, "label=\"X\"");
+	EXPECT_PRED2(containsSubString, print, "label=\"<eps>\"");
+}
+
+TEST(Automata, EpsilonClosure) {
+
+	// create a new automata accepting a single X
+	Automata<> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+	State s3 = a.getNewState();
+
+	a.setInitialState(s1);
+	a.setFinalState(s2);
+
+	a.addTransition(s1, 'X', s2);
+
+	// get epsilon closure
+	EXPECT_EQ(toSet<std::set<State>>(s1), a.getEpsilonClosure(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2), a.getEpsilonClosure(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getEpsilonClosure(s3));
+
+
+	// add an edge 1 -> 2
+	a.addEpsilonTransition(s1, s2);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2), a.getEpsilonClosure(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2), a.getEpsilonClosure(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getEpsilonClosure(s3));
+
+
+	// add an edge 2 -> 3
+	a.addEpsilonTransition(s2, s3);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getEpsilonClosure(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2,s3), a.getEpsilonClosure(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getEpsilonClosure(s3));
+
+	// add an edge 3 -> 3 (should not change anything
+	a.addEpsilonTransition(s3, s3);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getEpsilonClosure(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2,s3), a.getEpsilonClosure(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getEpsilonClosure(s3));
+
+	// create a cycle 3 -> 2
+	a.addEpsilonTransition(s3, s2);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getEpsilonClosure(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2,s3), a.getEpsilonClosure(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s2,s3), a.getEpsilonClosure(s3));
+
+	// create a cycle 2 -> 1
+	a.addEpsilonTransition(s2, s1);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getEpsilonClosure(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getEpsilonClosure(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getEpsilonClosure(s3));
+
+}
+
+TEST(Automata, ReachableStates) {
+
+	// create a new automata accepting a single X
+	Automata<> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+	State s3 = a.getNewState();
+
+	a.setInitialState(s1);
+	a.setFinalState(s2);
+
+	a.addTransition(s1, 'X', s2);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2), a.getReachableStates(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2), a.getReachableStates(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getReachableStates(s3));
+
+	a.addEpsilonTransition(s2,s3);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getReachableStates(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s2,s3), a.getReachableStates(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getReachableStates(s3));
+
+	a.addEpsilonTransition(s2,s1);
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getReachableStates(s1));
+	EXPECT_EQ(toSet<std::set<State>>(s1,s2,s3), a.getReachableStates(s2));
+	EXPECT_EQ(toSet<std::set<State>>(s3), a.getReachableStates(s3));
+
+}
+
+TEST(Automata, NFAConversion) {
+
+	// create a new automata accepting a single X
+	Automata<> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+
+	a.setInitialState(s1);
+	a.setFinalState(s2);
+
+	a.addTransition(s1, 'X', s2);
+	a.addEpsilonTransition(s2,s1);
+
+	auto res = toNFA(a);
+
+	string eNFA = toDotGraph(a);
+	string NFA = toDotGraph(res);
+
+	EXPECT_PRED2(containsSubString, eNFA, "0 -> 1 [label=\"X\"]");
+	EXPECT_PRED2(containsSubString, eNFA, "1 -> 0 [label=\"<eps>\"]");
+
+	EXPECT_PRED2(containsSubString, NFA, "0 -> 1 [label=\"X\"]");
+	EXPECT_PRED2(containsSubString, NFA, "1 -> 1 [label=\"X\"]");
+
+
+	// another example
+	a.setInitialState(s2);
+	res = toNFA(a);
+
+	eNFA = toDotGraph(a);
+	NFA = toDotGraph(res);
+
+	EXPECT_PRED2(containsSubString, eNFA, "0 -> 1 [label=\"<eps>\"]");
+	EXPECT_PRED2(containsSubString, eNFA, "1 -> 0 [label=\"X\"]");
+
+	EXPECT_PRED2(containsSubString, NFA, "0 -> 0 [label=\"X\"]");
+	EXPECT_PRED2(notContainsSubString, NFA, "1");
+
+}
+
+TEST(Automata, Accept) {
+
+	// create a new automata accepting a single X
+	eNFA<> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+
+	a.setInitialState(s1);
+	a.setFinalState(s2);
+
+	a.addTransition(s1, 'X', s2);
+
+	auto n = toNFA(a);
+	EXPECT_FALSE(accepts(n,string("")));
+	EXPECT_TRUE(accepts(n,string("X")));
+	EXPECT_FALSE(accepts(n,string("XX")));
+
+	EXPECT_EQ(accepts(n,string("")), accepts(a,string("")));
+	EXPECT_EQ(accepts(n,string("X")), accepts(a,string("X")));
+	EXPECT_EQ(accepts(n,string("XX")), accepts(a,string("XX")));
+}
+
+TEST(Automata, Accept2) {
+
+	// a larger example
+	Automata<> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+	State s3 = a.getNewState();
+
+	a.setInitialState(s1);
+	a.setFinalState(s3);
+
+	a.addTransition(s1, 'a', s2);
+	a.addTransition(s2, 'b', s2);
+	a.addTransition(s2, 'c', s2);
+	a.addTransition(s2, 'd', s3);
+
+	// should work for eNFA
+	EXPECT_FALSE(accepts(a,string("")));
+	EXPECT_FALSE(accepts(a,string("X")));
+	EXPECT_FALSE(accepts(a,string("XX")));
+
+	EXPECT_TRUE(accepts(a,string("ad")));
+	EXPECT_TRUE(accepts(a,string("abbbd")));
+	EXPECT_TRUE(accepts(a,string("abcbd")));
+	EXPECT_TRUE(accepts(a,string("acbcd")));
+
+	EXPECT_TRUE(accepts(a,string("abd")));
+	EXPECT_TRUE(accepts(a,string("abcd")));
+
+	EXPECT_FALSE(accepts(a,string("abda")));
+	EXPECT_FALSE(accepts(a,string("d")));
+
+	// and for NFA
+	auto n = toNFA(a);
+
+	EXPECT_FALSE(accepts(n,string("")));
+	EXPECT_FALSE(accepts(n,string("X")));
+	EXPECT_FALSE(accepts(n,string("XX")));
+
+	EXPECT_TRUE(accepts(n,string("ad")));
+	EXPECT_TRUE(accepts(n,string("abbbd")));
+	EXPECT_TRUE(accepts(n,string("abcbd")));
+	EXPECT_TRUE(accepts(n,string("acbcd")));
+
+	EXPECT_TRUE(accepts(n,string("abd")));
+	EXPECT_TRUE(accepts(n,string("abcd")));
+
+	EXPECT_FALSE(accepts(n,string("abda")));
+	EXPECT_FALSE(accepts(n,string("d")));
+
+}
+
+TEST(Automata, IntegerTest) {
+	typedef eNFA<int>::state_type State;
+
+	eNFA<int> a;
+
+	State s1 = a.getNewState();
+	State s2 = a.getNewState();
+	State s3 = a.getNewState();
+
+	a.addTransition(s1, 1, s2);
+	a.addTransition(s2, 2, s2);
+	a.addTransition(s2, 3, s3);
+
+	a.setInitialState(s1);
+	a.setFinalState(s3);
+
+	EXPECT_TRUE(accepts(a, toVector(1,2,3)));
+	EXPECT_TRUE(accepts(a, toVector(1,3)));
+	EXPECT_TRUE(accepts(a, toVector(1,2,2,3)));
+
+	EXPECT_FALSE(accepts(a, toVector(1,2)));
+
+}
+
+
+
+} // end namespace automata
+} // end namespace core
+} // end namespace insieme
+
