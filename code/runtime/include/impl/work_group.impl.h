5b40228665a6aa998ce59ee58d3b568f264f9e38
Peter Thoman <peter.thoman@uibk.ac.at>
2011-10-14 15:24:41 +0200

    runtime: Fixed multiple recycling

--- code/runtime/include/impl/work_group.impl.h
+++ code/runtime/include/impl/work_group.impl.h
@@ -125,7 +125,9 @@ void irt_wg_joining_barrier(irt_work_group* wg) {
 	// check if outstanding work group pfor joins required
 	uint32 pfor_c = wg->pfor_count, joined_pfor_c =  wg->joined_pfor_count;
 	while(joined_pfor_c < pfor_c) {
+		IRT_DEBUG("% 4d: joined_pfor_c(%d) < pfor_c(%d)\n", irt_wi_get_wg_membership(irt_wi_get_current(), 0).num, joined_pfor_c, pfor_c);
 		if(irt_atomic_bool_compare_and_swap(&wg->joined_pfor_count, joined_pfor_c, joined_pfor_c+1)) {
+			IRT_DEBUG("% 4d: JOINED\n", irt_wi_get_wg_membership(irt_wi_get_current(), 0).num);
 			// join the outstanding pfor work item
 			IRT_ASSERT(pfor_c - (joined_pfor_c+1) < IRT_WG_RING_BUFFER_SIZE, IRT_ERR_OVERFLOW, "Work group ring buffer overflow (due to outstanding pfor joins)");
 			irt_wi_join(wg->pfor_wi_list[(joined_pfor_c+1) % IRT_WG_RING_BUFFER_SIZE]);
@@ -133,7 +135,9 @@ void irt_wg_joining_barrier(irt_work_group* wg) {
 		pfor_c = wg->pfor_count;
 		joined_pfor_c = wg->joined_pfor_count;
 	}
+	IRT_DEBUG("% 4d: PRE barrier\n", irt_wi_get_wg_membership(irt_wi_get_current(), 0).num);
 	irt_wg_barrier(wg);
+	IRT_DEBUG("% 4d: POST barrier\n", irt_wi_get_wg_membership(irt_wi_get_current(), 0).num);
 }
 
 void irt_wg_barrier(irt_work_group* wg) {
