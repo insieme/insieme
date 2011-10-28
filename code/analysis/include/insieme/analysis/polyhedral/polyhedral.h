c9089d9efd26b41d2a1524c24a6463007c186764
spellegrini <spellegrini@dps.uibk.ac.at>
2011-10-28 17:29:16 +0200

    preparing polytope representation for transformations

--- code/analysis/include/insieme/analysis/polyhedral/polyhedral.h
+++ code/analysis/include/insieme/analysis/polyhedral/polyhedral.h
@@ -45,6 +45,9 @@
 #include "insieme/analysis/polyhedral/iter_vec.h"
 #include "insieme/analysis/polyhedral/affine_func.h"
 #include "insieme/analysis/polyhedral/constraint.h"
+
+#include "insieme/analysis/defuse_collect.h"
+
 #include "insieme/core/ast_node.h"
 #include "insieme/utils/printable.h"
 
@@ -56,6 +59,8 @@ namespace insieme {
 namespace analysis {
 namespace poly {
 
+typedef ConstraintCombinerPtr<AffineFunction> AffineConstraintPtr;
+
 /**************************************************************************************************
  * IterationDomain: the iteration domain represent the domain on which a statement is valid.
  * Therefore it is a represented by a set of constraints (ConstraintCombiner). However, the
@@ -65,22 +70,31 @@ namespace poly {
 class IterationDomain : public utils::Printable {
 
 	const IterationVector& iterVec;
-	ConstraintCombinerPtr<AffineFunction>  constraint;
+	AffineConstraintPtr  constraint;
 	bool empty;
 
 public:
 	IterationDomain( const IterationVector& iterVec, bool empty=false) : 
 		iterVec(iterVec), empty(empty) { }
 
-	explicit IterationDomain( const ConstraintCombinerPtr<AffineFunction>& constraint ) : 
-		iterVec( extractIterationVector(constraint) ), constraint(constraint), empty(false) { }
+	explicit IterationDomain( const AffineConstraintPtr& constraint ) : 
+		iterVec( extractIterationVector(constraint) ), 
+		constraint(constraint), 
+		empty(false) { }
 
 	IterationDomain( const IterationVector& iv, const IterationDomain& otherDom) : 
-		iterVec(iv), constraint( poly::cloneConstraint(iv, otherDom.constraint ) ), empty(false) { }
+		iterVec(iv), 
+		// update the iteration vector of the important constraint to iv
+		constraint( poly::cloneConstraint(iv, otherDom.constraint ) ), 
+		empty(false) { }
 	
-	inline const IterationVector& getIterationVector() const { return iterVec; }
+	inline const IterationVector& getIterationVector() const { 
+		return iterVec; 
+	}
 
-	inline const ConstraintCombinerPtr<AffineFunction>& getConstraint() const { return constraint; }
+	inline const AffineConstraintPtr& getConstraint() const { 
+		return constraint;
+	}
 
 	inline bool isUniverse() const { return !empty && !static_cast<bool>(constraint); }
 
@@ -109,33 +123,152 @@ struct AffineSystem : public utils::Printable, boost::noncopyable {
 	
 	typedef std::list<AffineFunction> AffineList;
 
-	AffineSystem(const IterationVector& iterVec) : iterVec(iterVec) { }	
-	AffineSystem(const AffineSystem& other) : iterVec(other.iterVec) { cloneRows(other.funcs); }
+	AffineSystem(const IterationVector& iterVec) : 	
+		iterVec(iterVec) { }	
 
-	inline const IterationVector& getIterationVector() const { return iterVec; }
+	AffineSystem(const AffineSystem& other) :
+		iterVec(other.iterVec), funcs(other.funcs) {  }
 
-	void append(const AffineFunction& af) { return insert(end(), af); }
+	inline const IterationVector& getIterationVector() const { 
+		return iterVec;
+	}
+
+	inline void append(const AffineFunction& af) { 
+		assert( &iterVec == &af.getIterationVector() && 
+			"AffineFunction has to have the same iteration vector" );
+
+		return insert(end(), af);
+	}
 
 	void insert(const AffineList::iterator& pos, const AffineFunction& af);
 
 	inline size_t size() const { return funcs.size(); }
 
-	std::ostream& printTo(std::ostream& out) const;
-
-	AffineList::iterator begin() { return funcs.begin(); }
-	AffineList::iterator end() { return funcs.end(); }
+	inline AffineList::iterator begin() { return funcs.begin(); }
+	inline AffineList::iterator end() { return funcs.end(); }
 
 	inline AffineList::const_iterator begin() const { return funcs.cbegin(); }
 	inline AffineList::const_iterator end() const { return funcs.cend(); }
 
-private:	
-	void cloneRows(const AffineList&);
+	std::ostream& printTo(std::ostream& out) const;
 
+private:	
 	const IterationVector& iterVec; 
 	AffineList funcs;
 };
 
 typedef std::shared_ptr<AffineSystem> AffineSystemPtr;
+
+/********************************************************************************************** 
+ * AccessInfo is a tuple which gives the list of information associated to a ref access: i.e.
+ * the pointer to a RefPtr instance (containing the ref to the variable being accessed and the
+ * type of access (DEF or USE). The iteration domain which defines the domain on which this
+ * access is defined and the access functions for each dimensions.
+ *********************************************************************************************/
+class AccessInfo : public utils::Printable {
+
+	core::ExpressionAddress	expr;
+	Ref::RefType			type;
+	Ref::UseType			usage; 
+	poly::AffineSystemPtr	access;
+
+public:
+	AccessInfo(
+		const core::ExpressionAddress&	expr, 
+		const Ref::RefType& 			type, 
+		const Ref::UseType& 			usage, 
+		const poly::AffineSystemPtr&	access 
+	) 
+	: expr(expr), type(type), usage(usage), access(access) { }
+	
+	inline const core::ExpressionAddress& getExpr() const { return expr; }
+	inline const Ref::RefType& getRefType() const { return type; }
+	inline const Ref::UseType& getUsage() const { return usage; }
+
+	inline AffineSystemPtr& getAccess() { return access; }
+	inline const AffineSystemPtr& getAccess() const { return access; }
+
+	std::ostream& printTo(std::ostream& out) const;
+};
+
+typedef std::vector<AccessInfo> AccessList;
+
+/*********************************************************************************************
+ * This class contains the scattering information of a statement contained in this SCoP from a
+ * point of view of iteration vector of this entry point. For each statement we keep:
+ * @addr: address (relative to this root node), 
+ *
+ * @iterDom:    iteration domain which contains the domain information for which the statment is 
+ * 			    defined
+ * @scattering: Which is the scattering infromation with the relative ordering of the statement 
+ * 				within this region
+ * @accessList: The list of ref accesses within the statement rewritten to the iteration vector
+ * 				of this entry point (iterVec).
+ *********************************************************************************************/
+
+/**************************************************************************************************
+ * PolyStmt: this class assembles together the informations which are utilized to represent a
+ * statement in the polyhedral mdoel. 
+ *
+ * A statement is represented by the following infomrations:
+ * 	- An *iteration domain* (with associated an iteration vector)
+ * 	- A *scheduling* (or scattering) matrix (which defines the logical dates when the statment is
+ * 	  executed)
+ *  - a set of *access functions* which identifies the read/writes happening inside the statement
+ *************************************************************************************************/
+class Stmt : public utils::Printable {
+
+	size_t	 				id;
+	core::StatementAddress 	addr;
+	IterationDomain 		dom;
+	AffineSystem 			schedule;
+	AccessList				access;
+
+	typedef AccessList::iterator AccessIterator;
+	typedef AccessList::const_iterator ConstAccessIterator;
+public:
+
+	Stmt(
+		  size_t 						id, 
+		  const core::StatementAddress& addr,
+		  const IterationDomain& 		dom, 
+		  const AffineSystem& 			schedule, 
+		  const AccessList& 			access 
+		) 
+	: id(id), addr(addr), dom(dom), schedule(schedule), access(access) { }
+
+	// Getter for the ID
+	inline size_t getId() const { return id; }
+
+	// Getter for StmtAddress
+	inline const core::StatementAddress& getAddr() const { return addr; }
+
+	// Getters/Setters for the iteration domain
+	inline const IterationDomain& getDomain() const { return dom; }
+	inline IterationDomain& getDomain() { return dom; }
+
+	// Getters/Setters for scheduling / scattering 
+	inline AffineSystem& getSchedule() { return schedule; }
+	inline const AffineSystem& getSchedule() const { return schedule; }
+
+	// Accessories for iterating through accesses of this statement (read/write)
+	inline AccessIterator access_begin() { return access.begin(); }
+	inline AccessIterator access_end() { return access.end(); }
+
+	// Accessories for iterating through accesses of this statement (write only)
+	inline ConstAccessIterator access_begin() const { return access.cbegin(); }
+	inline ConstAccessIterator access_end() const { return access.cend(); }
+
+	std::ostream& printTo(std::ostream& out) const;
+};
+
+struct Scop : public std::list<Stmt> {
+
+	Scop(const IterationVector& iterVec) : iterVec(iterVec) { }
+
+private:
+	IterationVector 	iterVec;
+};
 
 } // end poly namespace
 } // end analysis namespace
