91eee1302c19200e415ef55868d26d16044001e2
Peter Thoman <peter.thoman@uibk.ac.at>
2011-12-15 11:23:38 +0100

    Added lots of omp test cases

---
+++ test/omp/reduction/reduction.c
@@ -0,0 +1,23 @@
+#include <omp.h>
+
+#define ITERATIONS 100000
+
+int main() {
+	double a = 0, b = 0;
+	#pragma omp parallel
+	{
+		double c = 2;
+		double d = 4;
+		#pragma omp for reduction(+:a,b)
+		for(int i=0; i<ITERATIONS; ++i) {
+			a+=c;
+			b+=d;
+		}
+		#pragma omp master
+		{
+			if(a != c * ITERATIONS) printf("FAIL a\n");
+			else if(b != d * ITERATIONS) printf("FAIL b\n");
+			else printf("SUCCESS\n");
+		}
+	}
+}
