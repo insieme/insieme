8f856029782c99a737377dd40461886e4532cfd5
spellegrini <spellegrini@dps.uibk.ac.at>
2012-01-17 14:54:12 +0100

    merge

--- code/core/src/arithmetic/arithmetic_utils.cpp
+++ code/core/src/arithmetic/arithmetic_utils.cpp
@@ -433,13 +433,64 @@ Formula replace(core::NodeManager& mgr, const Formula& src, const ValueReplaceme
 		return toFormula(expr);
 
 	} catch (NotAFormulaException&& e) { 
-		assert(false);
+		assert(false && "After a replacement a Formula must be reparsable as a Formula again");
 	}
 }
 
 Constraint replace(core::NodeManager& mgr, const Constraint& src, const ValueReplacementMap& replacements) {
 	return Constraint(replace(mgr, src.getFunction(), replacements), src.getType());
 }
+
+namespace {
+
+struct ConstraintSimplifier : public utils::RecConstraintVisitor<Formula> {
+
+	core::NodeManager& mgr;
+	const ValueReplacementMap& repMap;
+	ConstraintPtr curr;
+
+	ConstraintSimplifier(
+			core::NodeManager& mgr, 
+			const ValueReplacementMap& repMap
+		) : mgr(mgr), repMap(repMap) { }
+
+	void visit(const utils::RawConstraintCombiner<Formula>& rcc) { 
+		curr = makeCombiner( replace(mgr, rcc.getConstraint(), repMap) );
+	}
+
+	void visit(const utils::NegatedConstraintCombiner<Formula>& ucc) { 
+		ucc.getSubConstraint()->accept(*this);
+		assert(curr);
+		
+		// if the constraint we are nagating is a raw constraint and the result is statically
+		// determined, then we rewrite the negation as a new constraint which evaluates to
+		// true/false
+
+		//if (utils::RawConstraintCombinerPtr<Formula> rc = 
+		//		std::dynamic_pointer_cast<utils::RawConstraintCombiner<Formula>>(ucc->getSubConstraint())) 
+		//{
+			
+		//}
+		curr = not_( curr );
+	}
+
+	void visit(const utils::BinaryConstraintCombiner<Formula>& bcc) {
+		//bcc.getLHS()->accept(*this);
+		//assert(curr);
+		//ConstraintCombinerPtr<TrgTy> lhs = curr;
+		//bcc.getRHS()->accept(*this);
+		//assert(curr);
+		//ConstraintCombinerPtr<TrgTy> rhs = curr; 
+
+		//curr = bcc.getType() == BinaryConstraintCombiner<SrcTy>::OR ? 
+			//lhs or rhs : lhs and rhs;
+	}
+
+};
+
+
+
+} // end anonymous namespace 
 
 //ConstraintPtr replace(core::NodeManager& mgr, const ConstraintPtr& src, const ValueReplacementMap& replacements) {
 //
