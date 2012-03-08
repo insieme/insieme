b120c2a0048be8d0e82de39d6c82f8fd306f1c4c
Ivan Grasso <c7031008@login.intern.leo3.uibk.ac.at>
2012-03-08 18:42:52 +0100

    added gcc_install script

---
+++ scripts/single_script/gcc_install.sh
@@ -0,0 +1,34 @@
+# setup environment variables
+. ../environment.setup
+
+VERSION=4.6.3
+########################################################################
+##                                                              GCC
+########################################################################
+rm -Rf $PREFIX/gcc-$VERSION
+echo "#### Downloading GCC ####"
+wget http://gcc.igor.onlinedirect.bg/releases/gcc-$VERSION/gcc-$VERSION.tar.gz
+wget http://gcc.igor.onlinedirect.bg/releases/gcc-$VERSION/gcc-g++-$VERSION.tar.gz
+
+tar -xzf gcc-$VERSION.tar.gz
+tar -xzf gcc-g++-$VERSION.tar.gz
+
+echo "#### Building GCC ####"
+mkdir gcc-build
+cd gcc-build
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/gmp-latest/lib:$PREFIX/mpfr-latest/lib:$PREFIX/mpc-latest/lib 
+export LD_LIBRARY_PATH
+
+../gcc-$VERSION/configure --prefix=$PREFIX/gcc-$VERSION --enable-languages=c,c++ --with-gmp=$PREFIX/gmp-latest --with-mpfr=$PREFIX/mpfr-latest --with-mpc=$PREFIX/mpc-latest
+make make -j $SLOTS
+
+echo "#### Installing GCC ####"
+make install $PREFIX/gcc-$VERSION
+
+rm $PREFIX/gcc-latest
+ln -s $PREFIX/gcc-$VERSION $PREFIX/gcc-latest
+
+echo "#### Cleaning up environment ####"
+cd ..
+rm -R gcc-$VERSION*
