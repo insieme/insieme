137e63e33fbf1d0632aaa1c36f31b62f76b3b9c1
Ivan Grasso <grasso@dps.uibk.ac.at>
2012-03-26 18:02:21 +0200

    added change to the installation script

--- scripts/single_script/cloog_install.sh
+++ scripts/single_script/cloog_install.sh
@@ -15,7 +15,7 @@ cd cloog-$VERSION
 export LD_LIBRARY_PATH=$PREFIX/gcc-latest/lib64:$PREFIX/gmp-latest/lib:$PREFIX/mpc-latest/lib:$PREFIX/mpfr-latest/lib:$PREFIX/cloog-gcc-latest/lib:$PREFIX/ppl-latest/lib:$LD_LIBRARY_PATH 
 
 echo "#### Building Cloog library ####"
-CC=$CC CXX=$CXX CFLAGS="-mtune=native -fgraphite-identity -O3" LDFLAGS="-mtune=native -O3" ./configure --prefix=$PREFIX/cloog-$VERSION --with-gmp=system --with-gmp-prefix=$PREFIX/gmp-latest --with-isl=system --with-isl-prefix=$PREFIX/isl-latest
+CC=$CC CXX=$CXX CFLAGS="-mtune=native -O3" LDFLAGS="-mtune=native -O3" ./configure --prefix=$PREFIX/cloog-$VERSION --with-gmp=system --with-gmp-prefix=$PREFIX/gmp-latest --with-isl=system --with-isl-prefix=$PREFIX/isl-latest
 make -j $SLOTS
 
 echo "#### Installing Cloog library ####"
