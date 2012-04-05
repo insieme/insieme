ba01e376491807891504ff27affb7aa579dc2922
Klaus Kofler <klaus@dps.uibk.ac.at>
2012-04-05 19:33:07 +0200

    refactoring of train_ffnet

--- code/machine_learning/src/cmd_line_utils.cpp
+++ code/machine_learning/src/cmd_line_utils.cpp
@@ -49,13 +49,13 @@ namespace po = boost::program_options;
 // current version number of the compiler: todo put into a file or something else
 #define VERSION_NUMBER "0.1"
 
-// initialize the static references of the CommandLineOptions args
+// initialize the static references of the TrainCmdOptions args
 #define FLAG(opt_name, opt_id, var_name, def_value, var_help) \
-	bool CommandLineOptions::var_name;
+	bool TrainCmdOptions::var_name;
 #define OPTION(opt_name, opt_id, var_name, var_type, var_help) \
-	var_type CommandLineOptions::var_name;
+	var_type TrainCmdOptions::var_name;
 #define INT_OPTION(opt_name, opt_id, var_name, def_val, var_help) \
-	int CommandLineOptions::var_name;
+	int TrainCmdOptions::var_name;
 #include "../include/insieme/machine_learning/options.def"
 #undef FLAG
 #undef OPTION
@@ -68,11 +68,11 @@ ostream& operator<<(ostream& out, const vector<string>& argList) {
 }
 
 void HandleImmediateArgs(po::options_description const& desc) {
-	if( CommandLineOptions::Help ) {
+	if( TrainCmdOptions::Help ) {
 		cout << desc << endl;
 		// we exit from the compiler
 		exit(1);
-	} if( CommandLineOptions::Version ) {
+	} if( TrainCmdOptions::Version ) {
 		cout << "This is the Insieme (tm) machine learning part: " << VERSION_NUMBER << endl <<
 				"Realized by the Distributed and Parallel Systems (DPS) group, copyright 2011-2012, " <<
 				"University of Innsbruck\n";
@@ -81,7 +81,7 @@ void HandleImmediateArgs(po::options_description const& desc) {
 }
 }
 
-void CommandLineOptions::Parse(int argc, char** argv, bool debug) {
+void TrainCmdOptions::Parse(int argc, char** argv, bool debug) {
 	po::options_description cmdLineOpts("Insieme (tm) compiler:\nOptions");
 
 	po::positional_options_description posDesc;
@@ -90,7 +90,7 @@ void CommandLineOptions::Parse(int argc, char** argv, bool debug) {
 	#define OPTION(opt_name, opt_id, var_name, var_type, var_help) \
 		(opt_name, po::value< var_type >(), var_help)
 	#define INT_OPTION(opt_name, opt_id, var_name, def_val, var_help) \
-		(opt_name, po::value< int >(&CommandLineOptions::var_name)->default_value(def_val), var_help)
+		(opt_name, po::value< int >(&TrainCmdOptions::var_name)->default_value(def_val), var_help)
 	#define FLAG(opt_name, opt_id, var_name, def_val, var_help) \
 		(opt_name, var_help)
 	// Declare a group of options that will be allowed on the command line
@@ -125,9 +125,9 @@ void CommandLineOptions::Parse(int argc, char** argv, bool debug) {
 
 		// assign the value to the class fields
 		#define FLAG(opt_name, opt_id, var_name, def_value, var_help) \
-			CommandLineOptions::var_name = (varsMap.count(opt_id) ?  true : def_value);
+			TrainCmdOptions::var_name = (varsMap.count(opt_id) ?  true : def_value);
 		#define OPTION(opt_name, opt_id, var_name, var_type, var_help) \
-			if(varsMap.count(opt_id)) CommandLineOptions::var_name = varsMap[opt_id].as< var_type >();
+			if(varsMap.count(opt_id)) TrainCmdOptions::var_name = varsMap[opt_id].as< var_type >();
 		#define INT_OPTION(opt_name, opt_id, var_name, var_type, var_help)
 		#include "../include/insieme/machine_learning/options.def"
 		#undef OPTION
