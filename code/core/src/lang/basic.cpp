4dc7847bfbea53f621393975a5b70e82c96067e4
Klaus Kofler <klaus@dps.uibk.ac.at>
2012-04-13 16:13:58 +0200

    extended definitions to support raytraceing

--- code/core/src/lang/basic.cpp
+++ code/core/src/lang/basic.cpp
@@ -257,7 +257,6 @@ ExpressionPtr BasicGenerator::getOperator(const TypePtr& type, const BasicGenera
 //	    return (*this).getLiteral(string("vector.pointwise"));
 	}
 
-std::cout << type << " " << op << std::endl;
 	assert(false && "Required combination of operator and type not declared");
 	return 0;
 }
