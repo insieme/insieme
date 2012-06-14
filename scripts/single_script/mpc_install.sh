9be6f7c0909418c5ee466ffca939c530c3ebef66
spellegrini <spellegrini@dps.uibk.ac.at>
2012-06-14 10:31:06 +0200

    changing installation scripts

--- scripts/single_script/mpc_install.sh
+++ scripts/single_script/mpc_install.sh
@@ -1,19 +1,28 @@
 # setup environment variables
 . ../environment.setup
 
-VERSION=0.9
-
 ########################################################################
 ##								MPC
 ########################################################################
-rm -Rf $PREFIX/mpc-$VERSION
+
+VERSION=0.9
+PACKAGE=mpc-$VERSION
+FILE=$PACKAGE.tar.gz
+
 echo "#### Downloading MPC library ####"
-wget http://www.multiprecision.org/mpc/download/mpc-$VERSION.tar.gz
-tar -xzf mpc-$VERSION.tar.gz
-cd mpc-$VERSION
+wget -nc http://www.multiprecision.org/mpc/download/$FILE
+
+# Remove any previous installation dir
+rm -Rf $PREFIX/$PACKAGE
+
+tar -xzf $FILE
+
+cd $PACKAGE
+
+export LD_LIBRARY_PATH=$PREFIX/gmp-latest/lib:$PREFIX/mpfr-latest/lib:$LD_LIBRARY_PATH
 
 echo "#### Building MPC library ####"
-CFLAGS="-mtune=native -O3" LDFLAGS="-mtune=native -O3" CXXFLAGS="-mtune=native -O3" ./configure --prefix=$PREFIX/mpc-$VERSION --with-gmp=$PREFIX/gmp-latest --with-mpfr=$PREFIX/mpfr-latest
+CFLAGS="-mtune=native -O3" ./configure --prefix=$PREFIX/mpc-$VERSION --with-gmp=$PREFIX/gmp-latest --with-mpfr=$PREFIX/mpfr-latest
 make -j $SLOTS
 make check
 
@@ -27,10 +36,10 @@ echo "#### Installing MPC library ####"
 make install
 
 rm $PREFIX/mpc-latest
-ln -s $PREFIX/mpc-$VERSION $PREFIX/mpc-latest
+ln -s $PREFIX/$PACKAGE $PREFIX/mpc-latest
 
 echo "#### Cleaning up environment ####"
 cd ..
-rm -R mpc-$VERSION*
+rm -R $PACKAGE*
 
 exit 0
