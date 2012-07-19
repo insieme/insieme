2c11d1216d247707dc48cd66b8329b79e1d20dce
Ivan Grasso <grasso@dps.uibk.ac.at>
2012-07-19 18:32:17 +0200

    added changes to flowmap

--- test/ocl/flowmap/flowmap.c
+++ test/ocl/flowmap/flowmap.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 
-static inline float icl_random01_float(){ return (float) rand()/(RAND_MAX); }
+static inline float random01_float(){ return (float) rand()/(RAND_MAX); }
 
 void icl_fillrandom_float(float* arrayPtr, int width, int height, float rangeMin, float rangeMax){
     if(!arrayPtr) {
@@ -16,45 +16,40 @@ void icl_fillrandom_float(float* arrayPtr, int width, int height, float rangeMin
     for(int i = 0; i < height; i++)
         for(int j = 0; j < width; j++) {
             int index = i*width + j;
-            arrayPtr[index] = rangeMin + (float)(range*icl_random01_float()); 	
+            arrayPtr[index] = rangeMin + (float)(range*random01_float()); 	
         }    
 }	
 
-int main(int argc, const char* argv[]) 
-{
+int main(int argc, const char* argv[]) {
 	int numTimesteps = 16;
 	cl_float2 origin   = { 0.f,  0.f  };
 	cl_float2 cellSize = { 0.1f, 0.1f };	
 	float startTime = 1.0f;
 	float advectionTime = 0.5f;	
 
-	
-	icl_args* args = icl_init_args();
+        icl_args* args = icl_init_args();
+        icl_parse_args(argc, argv, args);
 
-	icl_parse_args(argc, argv, args);		
-
-	unsigned int width = (unsigned int)floor(sqrt(args->size));
-	args->size = width * width;
+        int width = (int)floor(sqrt(args->size));
+        args->size = width * width;
         int size = args->size;
         icl_print_args(args);
-
+	
 
 	// prepare inputs
-	float* data      = (float*) malloc(sizeof(float) * size * numTimesteps * 2);
+	float* data      = (float*) malloc(sizeof(cl_float2) * size * numTimesteps);
 	float* timesteps = (float*) malloc(sizeof(float) * numTimesteps);
-	float* flowMap   = (float*) malloc(sizeof(float) * size * 2);
-	float* output    = (float*) malloc(sizeof(float) * size * 2);
+	float* flowMap   = (float*) malloc(sizeof(cl_float2) * size);
+	float* output    = (float*) malloc(sizeof(cl_float2) * size);
 	
 	icl_fillrandom_float(data, size*2, 1, -1.0f, 1.0f);
 	for(int i=0; i<numTimesteps; i++)
 		timesteps[i] = 0.05f * i;	
 	icl_fillrandom_float(flowMap, size, 1, -1.0f, 1.0f); // filled in case we only run the 2nd kernel
 	
-
-	icl_init_devices(ICL_CPU);
-
-	if (icl_get_num_devices() != 0) {
-		icl_device* dev = icl_get_device(0);
+        icl_init_devices(args->device_type);
+        if (icl_get_num_devices() != 0) {
+                icl_device* dev = icl_get_device(args->device_id);
 
 		icl_print_device_short_info(dev);
 
@@ -74,23 +69,17 @@ int main(int argc, const char* argv[])
 			multiplier += 1;
 		size_t szGlobalWorkSize = (int)multiplier * localWorkSize;
 
-		icl_run_kernel(kernel, 1, &szGlobalWorkSize, &localWorkSize, NULL, NULL, 9,
+		icl_run_kernel(kernel, 1, &szGlobalWorkSize, &localWorkSize, NULL, NULL, 10,
 			(size_t)0, (void *)buf_data,
-			sizeof(cl_uint), (void *)&width,
-//			sizeof(cl_uint), (void *)&height,
+			sizeof(cl_int), (void *)&width,
 			sizeof(cl_float2), (void *)&origin,
 			sizeof(cl_float2), (void *)&cellSize,
 			(size_t)0, (void *)buf_timesteps,
-			sizeof(cl_uint), (void *)&numTimesteps,
+			sizeof(cl_int), (void *)&numTimesteps,
 			sizeof(cl_float), (void *)&startTime,
 			sizeof(cl_float), (void *)&advectionTime,
-			(size_t)0, (void *)buf_flowMap
-
-//			sizeof(cl_uint), (void *)&width,
-//			sizeof(cl_uint), (void *)&height,
-//			sizeof(cl_float2), (void *)&origin,
-//			sizeof(cl_float2), (void *)&cellSize
-		);
+			(size_t)0, (void *)buf_flowMap,
+			sizeof(cl_int), (void *)&size);
 		icl_read_buffer(buf_flowMap, CL_TRUE, sizeof(cl_float2) * size, &flowMap[0], NULL, NULL);
 		
 
@@ -98,21 +87,21 @@ int main(int argc, const char* argv[])
 		icl_release_kernel(kernel);		
 	}
 
-		
-	if(args->check_result) printf("Checking results - not implemented.\n");
-	
-			
-	icl_release_devices();
+	if (args->check_result) {
+		printf("======================\n");
+		printf("Check Not Implemented\n");
+		printf("Result check: OK\n");
+	} else {
+		printf("Result check: OK\n");
+	}
 
-	
+        icl_release_args(args);
+        icl_release_devices();
+
 	free(timesteps);
 	free(flowMap);
 	free(data);
 	free(output);
-
-	#ifdef _MSC_VER
-	icl_prompt();
-	#endif
-
+	
 	return 0;
 }
