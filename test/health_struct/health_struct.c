8d598d883c49b1c9dec8adcb0c00fcd420e098f9
spellegrini <spellegrini@dps.uibk.ac.at>
2012-08-08 13:15:25 +0200

    adding test case for health bench

---
+++ test/health_struct/health_struct.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+struct Village;
+struct Patient;
+struct Hosp;
+
+struct Patient {
+   int id;
+   int seed;
+   int time;
+   int time_left;
+   int hosps_visited;
+   struct Village *home_village;
+   struct Patient *back;
+   struct Patient *forward;
+};
+struct Hosp {
+   int personnel;
+   int free_personnel;
+   struct Patient *waiting;
+   struct Patient *assess;
+   struct Patient *inside;
+   struct Patient *realloc;
+//   struct _irt_lock  realloc_lock;
+};
+struct Village {
+   int id;
+   struct Village *back;
+   struct Village *next;
+   struct Village *forward;
+   struct Patient *population;
+   struct Hosp hosp;
+   int level;
+   int  seed;
+};
+
+int main(int argc, char* argv[]) {
+
+	struct Village *v = NULL;
+
+	//v->hosp;
+	struct Patient* p;
+	v->population->back = p;
+}
