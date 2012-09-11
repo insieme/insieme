0c9125dd665287f54643f75b8415210a967aaee9
spellegrini <spellegrini@dps.uibk.ac.at>
2012-09-11 15:05:53 +0200

    adding new cases

--- test/vector_partial_init/vector_partial_init.c
+++ test/vector_partial_init/vector_partial_init.c
@@ -2,28 +2,45 @@
 
 int main() {
 
-	// partially initialized integer array
-	int a[20] = { 1, 2 };
-	printf("a[0]=%d\n", a[0]);
-	printf("a[1]=%d\n", a[1]);
-
-	// fully initialized integer array
-	int b[3] = { 1, 2, 3 };
-	printf("b[0]=%d\n", b[0]);
-	printf("b[1]=%d\n", b[1]);
-	printf("b[2]=%d\n", b[2]);
-
-	// array with undefined size
-	int c[] = { 1, 2, 3, 4 };
-	printf("c[0]=%d\n", c[0]);
-	printf("c[1]=%d\n", c[1]);
-	printf("c[2]=%d\n", c[2]);
-	printf("c[3]=%d\n", c[3]);
-
-	// string literals
-	char s[20] = "a";
-	printf("s[0]=%c\n", s[0]);
-
+	{ 
+		int a[20] = { 1 };
+		printf("a[0]=%d\n", a[0]);
+	}
+	{
+		// partially initialized integer array
+		int a[20] = { 1, 2 };
+		printf("a[0]=%d\n", a[0]);
+		printf("a[1]=%d\n", a[1]);
+	}
+	{
+		// fully initialized integer array
+		int a[3] = { 1, 2, 3 };
+		printf("a[0]=%d\n", a[0]);
+		printf("a[1]=%d\n", a[1]);
+		printf("a[2]=%d\n", a[2]);
+	}
+	{
+		// array with undefined size
+		int a[] = { 1, 2, 3, 4 };
+		printf("a[0]=%d\n", a[0]);
+		printf("a[1]=%d\n", a[1]);
+		printf("a[2]=%d\n", a[2]);
+		printf("a[3]=%d\n", a[3]);
+	}
+	{
+		// string literals
+		char s[20] = "a";
+		printf("s[0]=%c\n", s[0]);
+		printf("s[1]=%c\n", s[1]);
+	}
+	{
+		// string literals
+		char s[20] = "abc";
+		printf("s[0]=%c\n", s[0]);
+		printf("s[1]=%c\n", s[1]);
+		printf("s[2]=%c\n", s[2]);
+		printf("s[3]=%c\n", s[3]);
+	}
 	return 0;
 }
 
