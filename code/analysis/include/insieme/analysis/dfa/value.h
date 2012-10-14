2e53bddbd357958fa87810d19ebc81b379dfe36f
spellegrini <spellegrini@dps.uibk.ac.at>
2012-10-14 16:22:11 +0200

    fix weird namespace resolution error

--- code/analysis/include/insieme/analysis/dfa/value.h
+++ code/analysis/include/insieme/analysis/dfa/value.h
@@ -200,8 +200,8 @@ namespace std {
 	struct hash<insieme::analysis::dfa::Value<T>> {
 		
 		size_t operator()(const insieme::analysis::dfa::Value<T>& val) const {
-			if (val.isTop()) 	{ return reinterpret_cast<size_t>(&top); }
-			if (val.isBottom()) { return reinterpret_cast<size_t>(&bottom); }
+			if (val.isTop()) 	{ return reinterpret_cast<size_t>(&insieme::analysis::dfa::top); }
+			if (val.isBottom()) { return reinterpret_cast<size_t>(&insieme::analysis::dfa::bottom); }
 			return std::hash<T>()(val.value());
 		}
 
