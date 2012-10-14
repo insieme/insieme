5a4632dde68873d9fde557a27ffc447d9a9d76c2
spellegrini <spellegrini@dps.uibk.ac.at>
2012-10-14 16:19:23 +0200

    fix build

--- code/analysis/include/insieme/analysis/dfa/value.h
+++ code/analysis/include/insieme/analysis/dfa/value.h
@@ -196,12 +196,10 @@ inline Value<T> value(const T& e) { return Value<T>(e); }
 
 namespace std {
 
-	using namespace insieme::analysis::dfa;
-
 	template <typename T>
-	struct hash<Value<T>> {
+	struct hash<insieme::analysis::dfa::Value<T>> {
 		
-		size_t operator()(const Value<T>& val) const {
+		size_t operator()(const insieme::analysis::dfa::Value<T>& val) const {
 			if (val.isTop()) 	{ return reinterpret_cast<size_t>(&top); }
 			if (val.isBottom()) { return reinterpret_cast<size_t>(&bottom); }
 			return std::hash<T>()(val.value());
