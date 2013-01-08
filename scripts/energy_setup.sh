5e421fab7c917090b201933f2a303352dd2ba9f4
Philipp Gschwandtner <philipp@dps.uibk.ac.at>
2013-01-08 17:53:10 +0100

    extended energy setup script

--- scripts/energy_setup.sh
+++ scripts/energy_setup.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
 if [ `whoami` != root ]; then
-	echo Please run this script as root or using sudo
+	echo "Please run this script as root or using sudo"
 	exit
 fi
 
+echo -n "Loading msr module..."
 modprobe msr
+if [ $? -ne 0 ]; then
+	echo "failed!"
+	exit
+fi
+echo "done!"
+
+echo -n "Changing permission of msr files..."
 chmod 666 /dev/cpu/*/msr
+if [ $? -ne 0 ]; then
+	echo "failed!"
+	exit
+fi
+echo "done!"
+
+echo -n "Changing permissions of cpufreq files..."
 chmod 666 /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+if [ $? -ne 0 ]; then
+	echo "failed!"
+	exit
+fi
+echo "done!"
