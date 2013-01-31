890a397eb0cdd8fcdbb56bd6223e8121f394caa4
Peter Thoman <peter.thoman@uibk.ac.at>
2013-01-31 14:37:33 +0100

    Added Flood simulator test case

---
+++ test/omp/flood_simulator/ImgConv.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace ImgConv
+{
+    class ImgConv
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter outfile = new StreamWriter(args[0] + ".map"))
+            {
+                Image img = Image.FromFile(args[0]);
+                Bitmap bmp = new Bitmap(img);
+                int size = img.Height < img.Width ? img.Height : img.Width;
+                outfile.WriteLine(size);
+                for (int y = 0; y < size; ++y)
+                {
+                    for (int x = 0; x < size; ++x)
+                    {
+                        outfile.Write((int)(bmp.GetPixel(x, y).GetBrightness() * 10));
+                        outfile.Write(" ");
+                    }
+                    outfile.WriteLine();
+                }
+            }
+        }
+    }
+}
