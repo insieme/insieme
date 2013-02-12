da25c92fec26c6588444cf1aa02cefb9c4798ef9
Peter Thoman <peter.thoman@uibk.ac.at>
2013-02-12 12:12:21 +0100

    Added more CWB unit tests

--- code/runtime/test/cwb_test_par.cc
+++ code/runtime/test/cwb_test_par.cc
@@ -48,7 +48,7 @@
 
 #define NUM_THREADS 12
 
-#define NUM_MULTI_WIS 12
+#define NUM_MULTI_WIS 10
 
 TEST(circular_work_buffers, token_passing_single) {
 	for(int j=0; j<PARALLEL_ITERATIONS; ++j) {
@@ -62,15 +62,47 @@ TEST(circular_work_buffers, token_passing_single) {
 		wi.id.node = 0;
 		irt_cwb_push_front(&cwb, &wi);
 
-		#pragma omp parallel
+		#pragma omp parallel num_threads(NUM_THREADS)
 		{
 			while(num < TEST_ITERATIONS) {
 				if(irt_work_item* swi = irt_cwb_pop_back(&cwb)) {
-					num++;
 					irt_cwb_push_front(&cwb, swi);
+					irt_atomic_inc(&num);
 				} 
 			}
 		}
+	}
+}
+
+TEST(circular_work_buffers, token_passing_single_dual) {
+	for(int j=0; j<PARALLEL_ITERATIONS; ++j) {
+		irt_circular_work_buffer cwb;
+		irt_cwb_init(&cwb);
+		uint32 num = 0;
+
+		irt_work_item wi;
+		wi.id.index = 5;
+		wi.id.thread = 1;
+		wi.id.node = 0;
+		irt_cwb_push_front(&cwb, &wi);
+
+		irt_work_item wi2;
+		wi2.id.index = 9;
+		wi2.id.thread = 1;
+		wi2.id.node = 0;
+		irt_cwb_push_front(&cwb, &wi2);
+
+		#pragma omp parallel num_threads(NUM_THREADS)
+		{
+			while(num < TEST_ITERATIONS) {
+				if(irt_work_item* swi = irt_cwb_pop_back(&cwb)) {
+					irt_cwb_push_front(&cwb, swi);
+					irt_atomic_inc(&num);
+				} 
+			}
+		}
+
+		EXPECT_EQ(2, irt_cwb_size(&cwb));
 	}
 }
 
@@ -88,11 +120,12 @@ TEST(circular_work_buffers, token_passing_multi_self) {
 
 		#pragma omp parallel num_threads(NUM_THREADS)
 		{
+			uint32 rand_seed = 123;
 			while(num < TEST_ITERATIONS) {
-				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand()%NUM_THREADS])) {
-					num++;
+				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand_r(&rand_seed)%NUM_THREADS])) {
 					irt_cwb_push_front(&cwb[omp_get_thread_num()], swi);
-				} 
+					irt_atomic_inc(&num);
+				}
 			}
 		}
 	}
@@ -112,10 +145,11 @@ TEST(circular_work_buffers, token_passing_multi_rand) {
 
 		#pragma omp parallel num_threads(NUM_THREADS)
 		{
+			uint32 rand_seed = 123;
 			while(num < TEST_ITERATIONS) {
-				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand()%NUM_THREADS])) {
-					num++;
-					irt_cwb_push_front(&cwb[rand()%NUM_THREADS], swi);
+				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand_r(&rand_seed)%NUM_THREADS])) {
+					irt_cwb_push_front(&cwb[rand_r(&rand_seed)%NUM_THREADS], swi);
+					irt_atomic_inc(&num);
 				} 
 			}
 		}
@@ -142,10 +176,11 @@ TEST(circular_work_buffers, token_passing_multi_dual_rand) {
 
 		#pragma omp parallel num_threads(NUM_THREADS)
 		{
+			uint32 rand_seed = 123;
 			while(num < TEST_ITERATIONS) {
-				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand()%NUM_THREADS])) {
-					irt_atomic_inc(&num);
-					irt_cwb_push_front(&cwb[rand()%NUM_THREADS], swi);
+				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand_r(&rand_seed)%NUM_THREADS])) {
+					irt_cwb_push_front(&cwb[rand_r(&rand_seed)%NUM_THREADS], swi);
+					irt_atomic_inc(&num); 
 				} 
 			}
 		}
@@ -174,10 +209,11 @@ TEST(circular_work_buffers, token_passing_multi_multi_rand) {
 
 		#pragma omp parallel num_threads(NUM_THREADS)
 		{
+			uint32 rand_seed = 123;
 			while(num < TEST_ITERATIONS) {
 				if(irt_work_item* swi = irt_cwb_pop_back(&cwb[rand()%NUM_THREADS])) {
+					irt_cwb_push_front(&cwb[rand_r(&rand_seed)%NUM_THREADS], swi);
 					irt_atomic_inc(&num);
-					irt_cwb_push_front(&cwb[rand()%NUM_THREADS], swi);
 				} 
 			}
 		}
