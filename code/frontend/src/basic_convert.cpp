9e54f2335b16190a63a1e4ab943208af8beaf972
Bernhard Hoeckner <bernhard.hoeckner@student.uibk.ac.at>
2013-03-25 15:35:15 +0100

    adding recursive typevisitor to interceptor to resolve templated stuff - not fully working

--- code/frontend/src/basic_convert.cpp
+++ code/frontend/src/basic_convert.cpp
@@ -185,9 +185,9 @@ void ConversionFactory::buildGlobalStruct(analysis::GlobalVarCollector& globColl
 void ConversionFactory::buildInterceptedCaches(utils::Interceptor& interceptor) {
 	//copy interceptor exprcache into lambdaexpr cache
 	ctx.typeCache = interceptor.buildInterceptedTypeCache(*this);
+	VLOG(2) << "typeCache " << ctx.typeCache;
 	ctx.lambdaExprCache = interceptor.buildInterceptedExprCache(*this);
 	VLOG(2) << "lambdaExprCache: " << ctx.lambdaExprCache;
-	VLOG(2) << "typeCache " << ctx.typeCache;
 }
 
 
