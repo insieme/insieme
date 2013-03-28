9aa5602f3a8a3a7222b611e25a4e632beb14286a
Philipp Gschwandtner <philipp@dps.uibk.ac.at>
2013-03-28 13:35:19 +0100

    added new load balance metric

--- code/driver/src/measure/measure.cpp
+++ code/driver/src/measure/measure.cpp
@@ -321,6 +321,82 @@ namespace measure {
 		template<typename T> first_impl<T> first(const T& list) { return first_impl<T>(list); }
 
 		/**
+		 * Computes the min of quantities extracted as a list.
+		 *
+		 * @param T the functor used to extract the list.
+		 */
+		template<typename T>
+		struct min_impl {
+			T list_extractor;
+			min_impl(T list_extractor) : list_extractor(list_extractor) {}
+			Quantity operator()(const Measurements& data, MetricPtr metric, region_id region) const {
+				// check whether there is something
+				vector<Quantity> list = list_extractor(data, metric, region);
+				if (list.empty()) {
+					return Quantity::invalid(metric->getUnit());
+				}
+
+				// compute min
+				Quantity res = list[0];
+				for_each(list, [&](const Quantity& cur) {
+					if(cur < res)
+						res = cur;
+				});
+				return res;
+			}
+			std::set<MetricPtr> getDependencies() const { return list_extractor.getDependencies(); };
+		};
+
+		// a specialization for metric pointer
+		template<> struct min_impl<MetricPtr> : public min_impl<list> {
+			min_impl(MetricPtr m) : min_impl<list>(list(m)) {}
+		};
+
+		/**
+		 * Since template-structs cannot be constructed nicely without specifying the template
+		 * parameters, this function is introducing the necessary automated type deduction.
+		 */
+		template<typename T> min_impl<T> min(const T& list) { return min_impl<T>(list); }
+
+		/**
+		 * Computes the max of quantities extracted as a list.
+		 *
+		 * @param T the functor used to extract the list.
+		 */
+		template<typename T>
+		struct max_impl {
+			T list_extractor;
+			max_impl(T list_extractor) : list_extractor(list_extractor) {}
+			Quantity operator()(const Measurements& data, MetricPtr metric, region_id region) const {
+				// check whether there is something
+				vector<Quantity> list = list_extractor(data, metric, region);
+				if (list.empty()) {
+					return Quantity::invalid(metric->getUnit());
+				}
+
+				// compute max
+				Quantity res = list[0];
+				for_each(list, [&](const Quantity& cur) {
+					if(cur > res)
+						res = cur;
+				});
+				return res;
+			}
+			std::set<MetricPtr> getDependencies() const { return list_extractor.getDependencies(); };
+		};
+
+		// a specialization for metric pointer
+		template<> struct max_impl<MetricPtr> : public max_impl<list> {
+			max_impl(MetricPtr m) : max_impl<list>(list(m)) {}
+		};
+
+		/**
+		 * Since template-structs cannot be constructed nicely without specifying the template
+		 * parameters, this function is introducing the necessary automated type deduction.
+		 */
+		template<typename T> max_impl<T> max(const T& list) { return max_impl<T>(list); }
+
+		/**
 		 * Computes the sum of quantities extracted as a list.
 		 *
 		 * @param T the functor used to extract the list.
