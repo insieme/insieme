2e9e20144565bef697d4c1e83ef3a47c26c2b56d
Herbert <herbert@dps.uibk.ac.at>
2013-04-24 09:50:34 +0200

    Added script file for updating / fixing license headers.

---
+++ scripts/license/update_all_headers.sh
@@ -0,0 +1,23 @@
+#
+# This script can be used to update all license headers in case some
+# files are missing those or the license header text has changed.
+#
+
+ROOT_DIR=../..
+
+echo "Updating license headers ..."
+find $ROOT_DIR/ \( \
+	-ipath "$ROOT_DIR/code*.h" -or \
+	-ipath "$ROOT_DIR/code*.hpp" -or \
+	-ipath "$ROOT_DIR/code*.c" -or \
+	-ipath "$ROOT_DIR/code*.cpp" -or \
+	-ipath "$ROOT_DIR/code*.cxx" -or \
+	-ipath "$ROOT_DIR/code*.cc" -or \
+	-ipath "$ROOT_DIR/code*.cl" -or \
+	-ipath "$ROOT_DIR/code*.inc" -or \
+	-ipath "$ROOT_DIR/code*.def" -or \
+	-ipath "$ROOT_DIR/code*.in" \
+    \) \
+    | xargs -r -n 1 -P 8 ruby ./licensor.rb ./license_header.txt
+
+
