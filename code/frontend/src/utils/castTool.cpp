bce62f23a02b4dd07d98647b88ad2a5b3333c085
Simone Pellegrini <spellegrini@dps.uibk.ac.at>
2013-04-25 14:29:52 +0200

    fixing function missing return value

--- code/frontend/src/utils/castTool.cpp
+++ code/frontend/src/utils/castTool.cpp
@@ -581,6 +581,8 @@ core::ExpressionPtr performClangCastOnIR (const insieme::core::IRBuilder& builde
 		default:
 			assert(false && "not all options listed, is this clang 3.2? maybe should upgrade Clang support");
 	}
+	
+	assert(false && "control reached an invalid point!");
 
 }
 
