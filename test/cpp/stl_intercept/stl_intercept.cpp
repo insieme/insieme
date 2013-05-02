6d412112691deaa28bbd0db242858bccf599826a
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-05-02 09:42:19 +0200

    extending stl intercept tests

--- test/cpp/stl_intercept/stl_intercept.cpp
+++ test/cpp/stl_intercept/stl_intercept.cpp
@@ -3,7 +3,6 @@
 #include <list>
 #include <iostream>
 #include <cstdio>
-//#include <stdio.h>
 
 using namespace std;
 
@@ -13,27 +12,30 @@ using namespace std;
 int main() {
 	//wprintf("%lc",L'c');
 
-	std::cout << "asd";
+	//std::cout << "STL Intercept Test -- std::cout" << endl;
+	std::cout << "STL Intercept Test -- std::cout";
+
 	//globalInt = 0;
 	//globalList.push_back(10);
-	int a;
 
 	vector<int> v;
 	v.push_back(12);
 	v.push_back(14);
 
-	a= 1;
-
 	vector<int>::value_type x = v.at(0);
+	cout << x;
 
 	printf("[%d,%d]\n", v[0], v[1]);
 
 	vector<char> v1;
-	//v1.push_back(L'c');
 	v1.push_back('a');
 	v1.push_back('b');
-
 	printf("[%d,%d]\n", v1[0], v1[1]);
+
+	//vector<wchar> v2;
+	//v2.push_back(L'a');
+	//v2.push_back(L'b');
+	//printf("[%d,%d]\n", v2[0], v2[1]);
 
 	set<bool> universe;
 	universe.insert(true); 
@@ -47,7 +49,9 @@ int main() {
 
 	list<int>::iterator it = l.begin();
 	list<int>::iterator e = l.end();
-	for(; it!=e; it++ ) { }
+	for(; it!=e; it++ ) {
+		cout << "List: " << *it;
+	}
 
 	return 0; 
 }
