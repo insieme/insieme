6f5a1bb9c79af225d8b0261fbd834e26814745fd
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-05-22 10:52:22 +0200

    extending cpp/inherit_single testcase

--- test/cpp/inherit_single/inherit_single.cpp
+++ test/cpp/inherit_single/inherit_single.cpp
@@ -3,39 +3,51 @@
 class Shape {
 	public:
 		void fill() const { printf("Shape::fill()\n"); }
-    	void draw() const { printf("Shape::draw()\n"); }
+    	void draw() const { printf("Shape::draw() const\n"); }
+    	void draw() { printf("Shape::draw()\n"); }
     	~Shape() { printf("Shape::~Shape()\n"); }
 };
 
 class Circle: public Shape {
 	public:
 		void fill() const { printf("Circle::fill()\n"); }
-		void draw() const { printf("Circle::draw()\n"); }
+		void draw() const { printf("Circle::draw() const\n"); }
+		void draw() { printf("Circle::draw()\n"); }
 		~Circle() { printf("Circle::~Circle()\n"); }
 };
 
 class Circle2: public Shape {
 	public:
 		void fill() const { printf("Circle2::fill()\n"); }
-		void draw() const { printf("Circle2::draw()\n"); }
+		void draw() const { printf("Circle2::draw() const\n"); }
 		~Circle2() { printf("Circle2::~Circle2()\n"); }
 };
 
 
 int main() {
 
-	Shape s;
-	Circle c1;
-	//Circle2 c2;
 
-	s.draw();		// Shape::draw()
-	c1.draw();		// Circle::draw()
-	//c2.draw();		// Circle2::draw()
+	Shape s;
+	s.draw();			// Shape::draw()
+
+	Circle c1;
+	c1.draw();			// Circle::draw()
 
 	Shape *p1 = &c1;
-	p1->draw();   	// Circle::draw()
-	//p1 = &c1;
-	//p1->draw();   	// Circle::draw()
+	p1->draw();			// Shape::draw()
+
+	/**
+	 * WARNING CONST is lost in INSPIRE!!!
+	 * const Shape *cp1 = &c1;
+	 * cp1->draw();			// Shape::draw() const
+	 * */
+
+
+	Shape& rs = c1;
+	rs.draw();			// Shape::draw()
+
+	const Shape& crs = c1;
+	crs.draw();			// Shape::draw() const
 
 	return 0;
 }
