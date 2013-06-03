9781cc37ff7e3a51fe2792689412fa9b57403a16
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-06-03 13:34:15 +0200

    adding fix (q'n'd) for functiontemplates

--- code/frontend/src/utils/indexer.cpp
+++ code/frontend/src/utils/indexer.cpp
@@ -107,6 +107,7 @@ class IndexerVisitor{
 				indexDeclaration(f->getFriendDecl());	
 			} else {
 				//friendType --> getFriendType()
+				//assert(false && "indexer -- friendType not implemented");
 			}
 		} else if (const clang::NamedDecl *named = llvm::dyn_cast<clang::NamedDecl>(decl)) {
 			assert (named && "no name Decl, can not be indexed and we dont know what it is");
@@ -120,11 +121,11 @@ class IndexerVisitor{
 					// best way to find it, is to keep a simple record to address it
 					if(named->getNameAsString() == "main")
 						mIndex["main"] = elem; 
-				} 
+				}
 			}
-			else if (const clang::CXXRecordDecl *recDecl = llvm::dyn_cast<clang::CXXRecordDecl>(decl)){
-				if (recDecl->hasDefinition()){
-					Indexer::TranslationUnitPair elem =  std::make_pair(llvm::cast<clang::Decl>(recDecl->getDefinition()),mTu); 
+			else if (const clang::CXXRecordDecl *cxxRecDecl = llvm::dyn_cast<clang::CXXRecordDecl>(decl)) {
+				if (cxxRecDecl->hasDefinition()){
+					Indexer::TranslationUnitPair elem =  std::make_pair(llvm::cast<clang::Decl>(cxxRecDecl->getDefinition()),mTu); 
 					mIndex[buildNameTypeChain(decl)] = elem;
 				}
 
@@ -228,6 +229,8 @@ Indexer::TranslationUnitPair Indexer::getDefAndTUforDefinition (const clang::Dec
 	if(const clang::FunctionDecl* fd = llvm::dyn_cast<clang::FunctionDecl>(decl)) {
 		switch( fd->getTemplatedKind() ) {
 			case clang::FunctionDecl::TemplatedKind::TK_NonTemplate:
+				VLOG(2) << "TK_NonTemplate";
+				VLOG(2) << buildNameTypeChain(fd);
 				return getDefAndTUforDefinition(buildNameTypeChain(fd));
 				break;
 			case clang::FunctionDecl::TemplatedKind::TK_FunctionTemplate:
@@ -241,6 +244,13 @@ Indexer::TranslationUnitPair Indexer::getDefAndTUforDefinition (const clang::Dec
 				//return getDefAndTUforDefinition(buildNameTypeChain(fd->getMemberSpecializationInfo()->getInstantiatedFrom()));
 				break;
 			case clang::FunctionDecl::TemplatedKind::TK_FunctionTemplateSpecialization:
+				VLOG(2) << "TK_FunctionTemplateSpecialization";
+				if(const clang::FunctionTemplateSpecializationInfo* ti = fd->getTemplateSpecializationInfo() ) {
+					VLOG(2) << ti->getTemplate()->getTemplatedDecl();
+					VLOG(2) << "fd: " << buildNameTypeChain(fd);
+					VLOG(2) << "templateDecl: " << buildNameTypeChain(ti->getTemplate()->getTemplatedDecl());
+					return TranslationUnitPair( { const_cast<clang::FunctionDecl*>(fd), getDefAndTUforDefinition(buildNameTypeChain(ti->getTemplate()->getTemplatedDecl())).second});
+				}
 				break;
 			case clang::FunctionDecl::TemplatedKind::TK_DependentFunctionTemplateSpecialization:
 				break;
