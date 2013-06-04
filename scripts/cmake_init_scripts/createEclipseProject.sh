2bc0b01e52f458cceb0412d4e7f7c452102c08bc
Herbert <herbert@dps.uibk.ac.at>
2013-06-04 18:57:45 +0200

    Added additional cmake init scripts.

---
+++ scripts/cmake_init_scripts/createEclipseProject.sh
@@ -0,0 +1,13 @@
+# The directory used by cmake to locate third party libraries
+INSIEME_LIBS_HOME=~/libs 
+
+# optional: set up the CC and CXX compiler to be used
+#CXX=g++
+#CC=gcc
+
+# to include the std-lib header files within you eclipse project you have to specify the path to them
+#STD_LIB_DIR=/usr/include/c++/4.6.3
+
+# run cmake to create the project
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DGCC_INCLUDE_DIR=$STD_LIB_DIR ../code
+
