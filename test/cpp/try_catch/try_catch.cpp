634909797ca19e3683782e2df35d5e57874644e0
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-06-05 08:14:13 +0200

    extending trycatch testcase

--- test/cpp/try_catch/try_catch.cpp
+++ test/cpp/try_catch/try_catch.cpp
@@ -4,8 +4,11 @@ int main() {
 
 	try {
 		throw 10;
+		int x = 10;
+		x < 10 ? throw 10 : 1;
+		x < 10 ? 1 : throw 10;
 	} catch(int e) {
-		std::cout << "exception caught";
+		std::cout << "exception caught " << e;
 	}
 
 	return 0;
