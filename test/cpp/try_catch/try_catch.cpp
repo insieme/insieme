d936b8fb52f740597d8e932987d8e55f8bff906a
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-06-06 10:05:23 +0200

    extending try-catch test

--- test/cpp/try_catch/try_catch.cpp
+++ test/cpp/try_catch/try_catch.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 
+void throwFun() throw(int) {
+	throw 1;
+}
+
+int throwFun1() throw(int) {
+	throw 1;
+	return 1;
+}
+
 int main() {
 
-	try {
-		throw 10;
-		int x = 10;
-		x < 10 ? throw 10 : 1;
-		x < 10 ? 1 : throw 10;
-	} catch(int e) {
-		std::cout << "exception caught " << e;
-	}
+	try { throw 10; } catch(int e) { std::cout << "exception caught " << e << std::endl; }
 
+	try {
+		int x = 10;
+		//x < 10 ? throw 10 : 1;
+	} catch(int e) { std::cout << "exception caught " << e << std::endl; }
+
+	try {
+		int x = 10;
+		//x < 10 ? 1 : throw 10;
+	} catch(int e) { std::cout << "exception caught " << e << std::endl; }
+
+	int x = 10;
+	try{ throwFun(); } catch(int e) { std::cout << "exception caught " << e << std::endl; }
+	try { x = throwFun1(); } catch(int e) { std::cout << "exception caught " << e << std::endl; }
+	try { x < 10 ? throwFun1() : 1; } catch(int e) { std::cout << "exception caught " << e << std::endl; }
+	try { x < 10 ? 1 : throwFun1(); } catch(int e) { std::cout << "exception caught " << e << std::endl; }
+	
+	try{ throwFun(); } catch(...) { std::cout << "exception caught " << std::endl; }
+	try{ x = throwFun1(); } catch(...) { std::cout << "exception caught " << std::endl; }
+	try{ x < 10 ? throwFun1() : 1; } catch(...) { std::cout << "exception caught " << std::endl; }
+	try{ x < 10 ? 1 : throwFun1(); } catch(...) { std::cout << "exception caught " << std::endl; }
 	return 0;
 }
+
