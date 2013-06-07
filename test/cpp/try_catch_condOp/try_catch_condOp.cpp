6c78d86fc3e00e2aa09260d452f147f2747db27c
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-06-07 11:42:01 +0200

    adding testcase for throw-expr in conditional-operator

---
+++ test/cpp/try_catch_condOp/try_catch_condOp.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void throwFun() throw(int) {
+	throw 1;
+}
+
+int throwFun1() throw(int) {
+	throw 1;
+	return 1;
+}
+
+int main() {
+
+	try {
+		int x = 10;
+		x < 10 ? throw 10 : 1;
+	} catch(int e) { std::cout << "exception caught " << e << std::endl; }
+
+	try {
+		int x = 10;
+		x < 10 ? 1 : throw 10;
+	} catch(int e) { std::cout << "exception caught " << e << std::endl; }
+
+	try {
+		int x = 10;
+		x < 10 ? throw x : 1;
+	} catch(int e) { std::cout << "exception caught " << e << std::endl; }
+
+	try {
+		int x = 10;
+		x < 10 ? 1 : throw x;
+	} catch(int e) { std::cout << "exception caught " << e << std::endl; }
+
+
+	return 0;
+}
+
