5ddb778f418834f74089832e86c3fcc9656711af
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-06-20 11:35:33 +0200

    extending POD struct test

--- test/cpp/pod_struct/pod_struct.cpp
+++ test/cpp/pod_struct/pod_struct.cpp
@@ -1,10 +1,57 @@
-struct Test {
+#include <iostream>
+
+struct POD {
 	int a;
 };
 
 int main() {
-	struct Test test;
+	{
+		POD t;
+		std::cout << t.a << std::endl;
+	}
 	
-	test.a;
+	/*
+	{
+		//TODO currently zero-initialized 
+		POD p = {};
+		std::cout << p.a << std::endl;
+	}
+	*/
+
+	{
+		POD p = {1};
+		std::cout << p.a << std::endl;
+	}
+
+	{
+		//TODO currently zero-initialized 
+		//default-init
+		POD *f = new POD;
+		delete f;
+		std::cout << f->a << std::endl;
+	}
+
+	/*
+	{
+		//zero-init
+		POD *f = new POD();
+		std::cout << f->a << std::endl;
+		delete f;
+	}
+	*/
+
+
+	{
+		POD x;
+		POD y(x);
+	}
+
+
+	{
+		int i = ((POD){10}).a;
+		std::cout << i << std::endl;
+		std::cout << ((POD){10}).a << std::endl;
+		std::cout << ((POD){10}).a << std::endl;
+	}
 	return 0;
 }
