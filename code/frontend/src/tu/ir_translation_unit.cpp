56c47d6df3de19e0de4eb6bcb75cbdccc1134fb4
Herbert <herbert@dps.uibk.ac.at>
2013-08-06 09:37:11 +0200

    Minor fixes - removing an invalid comment and unused include.

--- code/frontend/src/tu/ir_translation_unit.cpp
+++ code/frontend/src/tu/ir_translation_unit.cpp
@@ -29,8 +29,8 @@
  *
  * All copyright notices must be kept intact.
  *
- * INSIEME depends on several third party software packages. Please
- * refer to http://www.dps.uibk.ac.at/insieme/license.html for details
+ * INSIEME depends on several third party software packages. Please 
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
  * regarding third party software licenses.
  */
 
@@ -55,7 +55,6 @@
 #include "insieme/core/transform/manipulation_utils.h"
 
 #include "insieme/annotations/c/extern.h"
-#include "insieme/annotations/c/include.h"
 
 namespace insieme {
 namespace frontend {
