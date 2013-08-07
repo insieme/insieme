77129348325d0eee7287347837d7deb96db9fd64
stefanm <stefan.moosbrugger@student.uibk.ac.at>
2013-08-07 17:32:28 +0200

    typeid expression implementation and integration test for type_id structs

---
+++ test/cpp/typeinfo_struct/typeinfo_struct.cpp
@@ -0,0 +1,36 @@
+// comparing type_info objects
+#include <iostream>   // std::cout
+#include <typeinfo>   // operator typeid
+
+struct Base {};
+struct Derived : Base {};
+struct Poly_Base {virtual void Member(){}};
+struct Poly_Derived: Poly_Base {};
+
+typedef int my_int_type;
+
+int main() {
+  std::cout << std::boolalpha;
+
+  // fundamental types:
+  std::cout << "int vs my_int_type: ";
+  std::cout << ( typeid(int) == typeid(my_int_type) ) << '\n';
+
+  // class types:
+  std::cout << "Base vs Derived: ";
+  std::cout << ( typeid(Base)==typeid(Derived) ) << '\n';
+
+  // non-polymorphic object:
+  Base* pbase = new Derived;
+
+  std::cout << "Base vs *pbase: ";
+  std::cout << ( typeid(Base)==typeid(*pbase) ) << '\n';
+
+  // polymorphic object:
+  Poly_Base* ppolybase = new Poly_Derived;
+
+  std::cout << "Poly_Base vs *ppolybase: ";
+  std::cout << ( typeid(Poly_Base)==typeid(*ppolybase) ) << '\n';
+
+  return 0;
+}
