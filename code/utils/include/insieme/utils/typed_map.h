0a5ed6acd948fa6bd1e2c61e9e66844f3ed57393
Herbert <herbert@dps.uibk.ac.at>
2013-08-23 19:05:20 +0200

    Added a heterogenious map type using a generic type as an index.

---
+++ code/utils/include/insieme/utils/typed_map.h
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2002-2013 Distributed and Parallel Systems Group,
+ *                Institute of Computer Science,
+ *               University of Innsbruck, Austria
+ *
+ * This file is part of the INSIEME Compiler and Runtime System.
+ *
+ * We provide the software of this file (below described as "INSIEME")
+ * under GPL Version 3.0 on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ *
+ * If you require different license terms for your intended use of the
+ * software, e.g. for proprietary commercial or industrial use, please
+ * contact us at:
+ *                   insieme@dps.uibk.ac.at
+ *
+ * We kindly ask you to acknowledge the use of this software in any
+ * publication or other disclosure of results by referring to the
+ * following citation:
+ *
+ * H. Jordan, P. Thoman, J. Durillo, S. Pellegrini, P. Gschwandtner,
+ * T. Fahringer, H. Moritsch. A Multi-Objective Auto-Tuning Framework
+ * for Parallel Codes, in Proc. of the Intl. Conference for High
+ * Performance Computing, Networking, Storage and Analysis (SC 2012),
+ * IEEE Computer Society Press, Nov. 2012, Salt Lake City, USA.
+ *
+ * All copyright notices must be kept intact.
+ *
+ * INSIEME depends on several third party software packages. Please 
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
+ * regarding third party software licenses.
+ */
+
+#pragma once
+
+#include <map>
+#include <ostream>
+#include <typeindex>
+#include <type_traits>
+
+namespace insieme {
+namespace utils {
+
+	template<
+		template<typename T> class ValueType,
+		typename BaseType,
+		typename ExampleElementType = int,
+		typename std::enable_if<std::is_base_of<BaseType, ValueType<ExampleElementType>>::value, int>::type = 0
+	>
+	class TypedMap {
+
+	public:
+
+		typedef std::map<std::type_index, BaseType*> map_type;
+		typedef typename map_type::const_iterator const_iterator;
+
+	private:
+
+		map_type data;
+
+	public:
+
+		TypedMap() {}
+
+		TypedMap(const TypedMap& other) {
+			for(auto cur : other.data) {
+				data[cur.first] = cur.second->copy();
+			}
+		}
+
+		TypedMap(TypedMap&& other) = default;
+
+		~TypedMap() {
+			for(auto cur : data) {
+				delete cur.second;
+			}
+		}
+
+		template<typename T>
+		ValueType<T>& get() {
+			auto& key = typeid(T);
+			auto pos = data.find(key);
+			if (pos != data.end()) {
+				return static_cast<ValueType<T>&>(*pos->second);
+			}
+
+			// create, register and return a new instance
+			ValueType<T>* element = new ValueType<T>();
+			data[key] = element;
+			return *element;
+		}
+
+		template<typename T>
+		const ValueType<T>& get() const {
+			static const ValueType<T> empty;
+
+			auto& key = typeid(T);
+			auto pos = data.find(key);
+			if (pos != data.end()) {
+				return static_cast<ValueType<T>&>(*pos->second);
+			}
+
+			return empty;
+		}
+
+		const_iterator begin() const { return data.begin(); }
+		const_iterator end() const { return data.end(); }
+
+		bool operator==(const TypedMap& other) const {
+			return data == other.data;
+		}
+
+		bool operator!=(const TypedMap& other) const {
+			return !(*this == other);
+		}
+
+	};
+
+} // end namespace utils
+} // end namespace insieme
+
+namespace std {
+
+	template<template<typename X> class A, typename B, typename C>
+	std::ostream& operator<<(std::ostream& out, const insieme::utils::TypedMap<A,B,C>& map) {
+		return out << "{" << join(",", map, [](std::ostream& out, const typename insieme::utils::TypedMap<A,B,C>::map_type::value_type& cur) {
+			out << *cur.second;
+		}) << "}";
+	}
+
+} // end namespace std
