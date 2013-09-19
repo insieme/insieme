48fe5fddf5823771f6fb3aaed94fbd8163b581b3
Herbert <herbert@dps.uibk.ac.at>
2013-09-19 13:44:56 +0200

    Improved assertion checking for proper concatentation of addresses.

--- code/core/include/insieme/core/ir_address.h
+++ code/core/include/insieme/core/ir_address.h
@@ -810,8 +810,16 @@ struct DynamicAddressCast {
 
 template <class A, class B>
 Address<const B> concat(const Address<const A>& head, const Address<const B>& tail) {
-	assert(head.getAddressedNode() == tail.getRootNode() && "Impossible to merge addresses");
+	assert_eq(head.getAddressedNode(), tail.getRootNode())
+				<< "Cannot concatenate paths with un-matching ends: "
+				<< head->getNodeType() << "@" << &*head.getAddressedNode() << " vs. "
+				<< tail.getRootNode()->getNodeType() << "@" << &*tail.getRootNode();
 	return Address<const B>(Path::concat(head.getPath(), tail.getPath()));
+}
+
+template<class A, class B>
+Address<const B> operator>>(const Address<const A>& head, const Address<const B>& tail) {
+	return concat(head, tail);
 }
 
 /**
