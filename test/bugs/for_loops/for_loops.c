376f958e43e0dabfc4607dec012cce6409d17130
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-09-27 12:07:29 +0200

    extending forLoopsTest

--- test/bugs/for_loops/for_loops.c
+++ test/bugs/for_loops/for_loops.c
@@ -7,9 +7,11 @@ int main(int argc, char** argv) {
 	int v[15] = {0};
 	int vec[15] = {0};
 
+	/*
 	for(unsigned char i = 8; i>0; --i) {
 		iterations++;
 	}
+	*/
 
 	printf("Iterations: %d\n", iterations);
 	
@@ -17,7 +19,7 @@ int main(int argc, char** argv) {
 	for(j=0; j<vec[10]; j++)
 	{
 		cnt ++;
-		//if(j == 10)
+		if(j == 10)
 			vec[10] ++;
 	}
 
