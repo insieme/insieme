33aaa69615a466a2b6ca6f7f52372d14d91892da
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2013-10-01 10:58:10 +0200

    fixing bug with escaped chars in strings
    extending test case for escaped chars

--- test/bugs/string_escape_chars/string_escape_chars.c
+++ test/bugs/string_escape_chars/string_escape_chars.c
@@ -1,12 +1,17 @@
  #include <stdio.h>
  #include <string.h>
  
- static char str[128] = "12345\t";
+static char str[128] = "12345\t";
  
  int main(int argc, char** argv)
  {
-         if(strlen(str) != 6)
-                 return 1;
-         else
-                 return 0;
- }
+	char s[128] = "1\t2\t3\t4\t5\t";
+	if(strlen(s) != 10)
+			return 1;
+	else
+			return 0;
+	if(strlen(str) != 6)
+		return 1;
+	else
+			return 0;
+}
