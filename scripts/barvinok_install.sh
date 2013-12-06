b7e0d40764c40a06e99faee60569a7e4de6d57bd
Luis Ayuso <luis@dps.uibk.ac.at>
2013-12-06 16:17:20 +0100

    some little changes in the configuration script, new sources for barvinok and isl, removed mandatory openmpi, is this a problem?

--- scripts/barvinok_install.sh
+++ scripts/barvinok_install.sh
@@ -11,17 +11,21 @@ OPT_FLAGS="-mtune=native -O3"
 
 rm -Rf $PREFIX/barvinok-$VERSION
 echo "#### Downloading Barvinok library ####"
-wget -nc ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/barvinok/barvinok-$VERSION.tar.bz2
+#wget -nc ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/barvinok/barvinok-$VERSION.tar.bz2
+#wget -nc http://repo.or.cz/w/barvinok.git/snapshot/c8c898a86498fffe11947de849af0bfa1fb42a51.tar.gz
+#mv c8c898a86498fffe11947de849af0bfa1fb42a51.tar.gz barvinok-$VERSION.tar.gz
+wget -nc http://www.cs.ucla.edu/~pouchet/software/pocc/download/modules/barvinok-0.35.tar.gz
 
 RET=$?
 if [ $RET -ne 0 ]; then
 	exit $RET
 fi
 
-tar -xf barvinok-$VERSION.tar.bz2
+tar -xf barvinok-$VERSION.tar.gz
 cd barvinok-$VERSION
 
-export LD_LIBRARY_PATH=$PREFIX/gcc-latest/lib64:$PREFIX/gmp-latest/lib:$PREFIX/mpfr-latest/lib:$PREFIX/cloog-gcc-latest/lib:$PREFIX/ppl-latest/lib:$PREFIX/ntl-latest/lib:$PREFIX/mpc-latest/lib:$LD_LIBRARY_PATH 
+#export LD_LIBRARY_PATH=$PREFIX/gcc-latest/lib64:$PREFIX/gmp-latest/lib:$PREFIX/mpfr-latest/lib:$PREFIX/cloog-gcc-latest/lib:$PREFIX/ppl-latest/lib:$PREFIX/ntl-latest/lib:$PREFIX/mpc-latest/lib:$LD_LIBRARY_PATH 
+export LD_LIBRARY_PATH=$PREFIX/ntl-latest/lib:$LD_LIBRARY_PATH 
 
 echo "#### Building Barvinok library ####"
 CC=$CC CXX=$CXX CFLAGS=$OPT_FLAGS CXXFLAGS=$OPT_FLAGS \
