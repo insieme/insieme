bc11595719f4979b98ae28506bb16c6bdddb9b35
Herbert <herbert@dps.uibk.ac.at>
2013-12-12 21:05:02 +0100

    Added C++ test case actually containing a recursive class with constructor - we did not had something like that before ...

---
+++ test/cpp/rec_class/rec_class.cpp
@@ -0,0 +1,21 @@
+
+/**
+ * This test case ensures that recursive types with initializer
+ * lists are working (bug found within some project code).
+ */
+
+struct A {
+
+	A* next;
+	int i;
+
+	A(int i=0) : i(i), next(0) {}
+};
+
+int main() {
+	
+	A a;
+	A b(1);
+
+	return 0;
+}
