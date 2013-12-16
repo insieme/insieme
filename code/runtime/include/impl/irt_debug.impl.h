d2ec53bc8211fda8fcd26db0387160564548c4cf
Peter Thoman <peter.thoman@uibk.ac.at>
2013-12-16 15:13:25 +0100

    RS: added irt_dbg fucntionality for tracking WIs

--- code/runtime/include/impl/irt_debug.impl.h
+++ code/runtime/include/impl/irt_debug.impl.h
@@ -42,6 +42,9 @@ void irt_dbg_print_worker_events(int32 wid, int32 num) {
 	for(int32 i = s; i>=0 && i>s-num; --i) {
 		irt_inst_event_data_output_single(irt_g_workers[wid]->instrumentation_event_data->data[i], stdout, true);
 	}
+	if(s<0) {
+		printf("\n");
+	} 
 }
 #else
 void irt_dbg_print_worker_events(int32 wid, int32 num) {
@@ -82,4 +85,117 @@ void irt_dbg_print_worker_overview() {
 	for(uint32 w=0; w<irt_g_worker_count; ++w) {
 		irt_dbg_print_worker_state(w);
 	}
+}
+
+#define _IRT_DBG_MAX_EVENTS (1024*32)
+typedef struct __irt_dbg_wi_list {
+	uint32 wi_index;
+	irt_instrumentation_event_data* events[_IRT_DBG_MAX_EVENTS];
+	uint32 num_events;
+	struct __irt_dbg_wi_list *next;
+} _irt_dbg_wi_list;
+void _irt_dbg_wi_list_add(_irt_dbg_wi_list **l, uint32 index) {
+	_irt_dbg_wi_list *elem = (_irt_dbg_wi_list*)malloc(sizeof(_irt_dbg_wi_list));
+	elem->wi_index = index;
+	elem->next = *l;
+	*l = elem;
+}
+void _irt_dbg_wi_list_remove(_irt_dbg_wi_list **l, uint32 index) {
+	// first
+	if(*l && (*l)->wi_index == index) {
+		_irt_dbg_wi_list* temp = (*l)->next;
+		free(*l);
+		*l = temp;
+		return;
+	}
+	// others
+	_irt_dbg_wi_list *li = *l;
+	while(li) {
+		if(li->next && li->next->wi_index == index) {
+			_irt_dbg_wi_list* temp = li->next->next;
+			free(li->next);
+			li->next = temp;
+			return;
+		}
+		li = li->next;
+	}
+	printf("DBG analysis error: irt_dbg_wi_list tried to remove non-existent element\n");
+}
+_irt_dbg_wi_list* _irt_dbg_wi_list_get(_irt_dbg_wi_list *l, uint32 index) {
+	while(l) {
+		if(l->wi_index == index) return l;
+		l = l->next;
+	}
+	return NULL;
+}
+void _irt_dbg_wi_list_clear(_irt_dbg_wi_list **l) {
+	while(*l) {
+		_irt_dbg_wi_list* temp = (*l)->next;
+		free(*l);
+		*l = temp;
+	}
+}
+void _irt_dbg_wi_list_print(_irt_dbg_wi_list *l) {
+	printf("(");
+	while(l) {
+		printf("%d,", l->wi_index);
+		l = l->next;
+	}
+	printf(")\n");
+}
+
+void irt_dbg_print_active_wis() {
+	// store current index on each worker's event list
+	uint64 *worker_ev_indices = (uint64*)calloc(irt_g_worker_count, sizeof(uint64));
+	irt_instrumentation_event_data* next_ev = NULL;
+	_irt_dbg_wi_list *active_wis = NULL;
+	do {
+		// find oldest event
+		uint64 min_timestamp = UINT64_MAX;
+		uint32 min_worker = 0;
+		next_ev = NULL;
+		for(uint32 w=0; w<irt_g_worker_count; ++w) {
+			uint32 s = irt_g_workers[w]->instrumentation_event_data->number_of_elements;
+			if(worker_ev_indices[w] < s) {		
+				irt_instrumentation_event_data* ev = &irt_g_workers[w]->instrumentation_event_data->data[worker_ev_indices[w]];
+				if(ev->timestamp < min_timestamp) {
+					min_worker = w;
+					min_timestamp = ev->timestamp;
+					next_ev = ev;
+				}
+			}
+		}
+		// process event
+		if(next_ev) {
+			worker_ev_indices[min_worker]++;
+			// add/remove to/from active list
+			switch(next_ev->event_id) {
+			case IRT_INST_WORK_ITEM_CREATED: {
+					//printf("created %d!\n", next_ev->index);
+					_irt_dbg_wi_list_add(&active_wis, next_ev->index);
+				} break;
+			case IRT_INST_WORK_ITEM_FINALIZED: {
+					//printf("finalized %d!\n", next_ev->index);
+					_irt_dbg_wi_list_remove(&active_wis, next_ev->index);
+				} break;
+			}
+			// store events for active
+			_irt_dbg_wi_list *entry = _irt_dbg_wi_list_get(active_wis, next_ev->index);
+			if(entry && entry->num_events<_IRT_DBG_MAX_EVENTS) {
+				entry->events[entry->num_events++] = next_ev;
+			}
+		}
+	} while(next_ev != NULL);
+	// print active wi indices
+	_irt_dbg_wi_list *l = active_wis;
+	while(l) {
+		printf("--------\nActive wi index: %d\n", active_wis->wi_index);
+		for(uint32 i=0; i<active_wis->num_events; ++i) {
+			irt_inst_event_data_output_single(*active_wis->events[i], stdout, true);
+		}
+		l = l->next;
+	}
+	// cleanup
+	free(worker_ev_indices);
+	_irt_dbg_wi_list_clear(&active_wis);
 }
