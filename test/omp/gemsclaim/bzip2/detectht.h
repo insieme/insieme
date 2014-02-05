daa14b09e69f68afdff3afa84d8e03f9efa9d8b6
Ferdinando Alessi <alessi@dps.uibk.ac.at>
2014-02-05 13:58:34 +0100

    Bzip2 test duplicate files removed

--- test/omp/gemsclaim/bzip2/detectht.h
+++
@@ -1,5 +0,0 @@
-#ifdef __linux__
-#define HT_DETECTION
-#endif
-
-extern int isHtPresent( void );
--- test/omp/gemsclaim/bzip2/note.h
+++
@@ -1,5 +0,0 @@
-/* The level is 0 initially */
-extern int verbosityLevel;
-
-/* Messages with level > verbosityLevel are not displayed */
-extern void note( int level, const char * format, ... );
