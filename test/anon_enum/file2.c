ee77c1cc424c376d53bf422cfc0ac7d41a94ae31
Luis Ayuso <luis@dps.uibk.ac.at>
2014-02-13 17:53:36 +0100

    more annonymous tests

---
+++ test/anon_enum/file2.c
@@ -0,0 +1,12 @@
+
+
+#include "header.h"
+
+
+
+
+void f(){
+	var1 =WHITE;
+	var2 =BLACK;
+	var3 =ONE;
+}
---
+++ test/anon_enum/header.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+typedef enum {BLACK = 15, WHITE = 16} color_t;
+extern color_t var1;
+extern color_t var2;
+
+typedef enum {ONE = 1, TWO = 2} number_t;
+extern number_t var3;
+
+
+void f();
+
+
