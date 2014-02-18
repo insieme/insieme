51c09ede891148ff83fc65e635fb1ecf54e502fd
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2014-02-18 14:52:50 +0100

    adding gettimeofday test to c integratin tests

---
+++ test/gettimeoftheday/second_cpu.c
@@ -0,0 +1,19 @@
+//#include <sys/param.h>
+//#include <sys/types.h>
+//#include <time.h>
+//#include <sys/times.h>
+
+#include "second_cpu.h"
+
+#define CLOCKS_PER_SEC 5
+
+double mysecond()
+{
+    long sec;
+    double secx;
+    struct tms realbuf;
+
+    times(&realbuf);
+    secx = ( realbuf.tms_stime + realbuf.tms_utime ) / (float) CLOCKS_PER_SEC;
+    return ((double) secx);
+}
