18f59d5148b804be701c87ac5a54d3557d7202f2
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2014-02-18 16:51:26 +0100

    adding a integration test using boost_mutex (currently without insiemecc-pass)

---
+++ test/cpp/bugs/boost_mutex/main.cpp
@@ -0,0 +1,20 @@
+#include "header.h"
+
+int main() {
+	S x;
+	x.useMutex();
+
+	std::cout << x.tryLockMutex() << std::endl;
+	x.unlockMutex();
+	
+	{
+		boost::lock_guard<MutexType> lock(x.mutexInst);
+		std::cout << x.tryLockMutex() << std::endl;
+	}
+
+	std::cout << x.tryLockMutex() << std::endl;
+	x.unlockMutex();
+
+
+	return 0;
+}
