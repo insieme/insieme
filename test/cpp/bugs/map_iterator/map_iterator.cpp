5bb998713ef1f2211b21cde04c2794f4d46c1958
stefanm <stefan.moosbrugger@student.uibk.ac.at>
2014-02-24 13:05:17 +0100

    const template argument bug test case

---
+++ test/cpp/bugs/map_iterator/map_iterator.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <functional>
+#include <string>
+#include <iostream>
+
+struct A {
+};
+
+enum X {
+    A,B,C
+} xX;
+
+int main() {
+	//std::map<std::string, std::pair<A, int> > values;
+    std::map<X,std::string> enumVal;
+
+	//std::map<const std::string, std::pair<A, int> >::iterator x = values.find("test");
+   // std::map<X,std::string>::iterator y = enumVal.find(xX);
+    //if(x==values.end())
+	//	std::cout << "not found";
+	//else
+	//	std::cout << "found";
+	return 0;
+}
