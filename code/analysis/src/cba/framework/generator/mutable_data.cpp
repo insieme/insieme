541eee9b0467165b2a0b38eb282fd6351e7beeaa
Herbert <herbert@dps.uibk.ac.at>
2014-03-10 17:25:11 +0100

    Unified constraint pruning filters based on static identification of code regions without effects.

--- code/analysis/src/cba/framework/generator/mutable_data.cpp
+++ code/analysis/src/cba/framework/generator/mutable_data.cpp
@@ -35,6 +35,7 @@
  */
 
 #include "insieme/analysis/cba/framework/generator/killed_definitions.h"
+#include "insieme/analysis/cba/utils/static_execution_check.h"
 
 namespace insieme {
 namespace analysis {
@@ -42,118 +43,11 @@ namespace cba {
 
 	namespace detail {
 
-		struct AssignmentFreeTag {
-			bool value;
-			AssignmentFreeTag(bool value) : value(value) {}
-			bool operator==(const AssignmentFreeTag& other) const {
-				return value == other.value;
-			}
-		};
-
-		class AssignmentCheck : public IRVisitor<bool> {
-
-		public:
-
-			virtual bool visitVariable(const VariablePtr& var) {
-				return true;		// no sync points here
-			}
-
-			virtual bool visitLiteral(const LiteralPtr& var) {
-				return true;		// the literal itself is never a sync point, only the call to a synchronizing function
-			}
-
-			virtual bool visitCallExpr(const CallExprPtr& call) {
-				auto fun = call->getFunctionExpr();
-				const auto& base = fun->getNodeManager().getLangBasic();
-
-				// if this is a call to a spawn => not free of spawn points
-				if (base.isRefAssign(fun)) return false;
-				if (base.isMerge(fun)) return false;
-
-				// if one of the arguments contains a sync point we are done
-				if (!all(call, isAssignmentFree)) {
-					return false;		// not sync point free
-				}
-
-				// if any of the arguments is a job or closure => return false (conservative)
-				for(const auto& cur : call) {
-					if (cur->getType().isa<FunctionTypePtr>()) return false;
-					if (cur.isa<JobExprPtr>()) return false;
-				}
-
-				// if it is a call to a literal
-				if (fun.isa<LiteralPtr>()) {
-					// this is fine
-					return true;
-				}
-
-				// if the target is a variable => fail (conservative)
-				if (fun.isa<VariablePtr>()) return false;
-
-				// if the target is anything else => check whether synchronizing expressions are included
-				return !visitDepthFirstOnceInterruptible(fun, [&](const LiteralPtr& lit){
-					return base.isRefAssign(lit) || base.isMerge(lit);
-				});
-			}
-
-			virtual bool visitLambdaExpr(const LambdaExprPtr& expr) {
-				return true;		// the evaluation of the lambda expression is not causing sync points
-			}
-
-			virtual bool visitJobExpr(const JobExprPtr& job) {
-				return isAssignmentFree(job->getThreadNumRange());
-			}
-
-			virtual bool visitTupleExpr(const TupleExprPtr& tuple) {
-				for(const auto& cur : tuple->getExpressions()) {
-					if (!isAssignmentFree(cur)) return false;
-				}
-				return true;
-			}
-
-			virtual bool visitVectorExpr(const VectorExprPtr& vec) {
-				for(const auto& cur : vec->getExpressions()) {
-					if (!isAssignmentFree(cur)) return false;
-				}
-				return true;
-			}
-
-			virtual bool visitStructExpr(const StructExprPtr& structExpr) {
-				for(const auto& cur : structExpr->getMembers()) {
-					if (!isAssignmentFree(cur->getValue())) return false;
-				}
-				return true;
-			}
-
-			virtual bool visitUnionExpr(const UnionExprPtr& unionExpr) {
-				return isAssignmentFree(unionExpr->getMember());
-			}
-
-			virtual bool visitNode(const NodePtr& node) {
-				assert_fail() << "Unsupported Node Type encountered: " << node->getNodeType();
-				return false;
-			}
-
-		};
-
-
 		bool isAssignmentFree(const NodePtr& node) {
-
-			static AssignmentCheck isAssignmentFreeInternal;
-
-			// check whether there is an attached sync-free tag
-			if (node->hasAttachedValue<AssignmentFreeTag>()) {
-				return node->getAttachedValue<AssignmentFreeTag>().value;
-			}
-
-			// compute state
-			bool res = isAssignmentFreeInternal(node);
-
-			// attach resulting annotation
-			node->attachValue<AssignmentFreeTag>(res);
-
-			// done
-			return res;
+			const auto& base = node->getNodeManager().getLangBasic();
+			return !mayReachCallTo(node, [&](const NodePtr& cur) {
+				return base.isRefAssign(cur) || base.isMerge(cur);
+			});
 		}
 
 	}
