3debf6e1c66697e940bf22ae477345d351782681
stefanm <stefan.moosbrugger@student.uibk.ac.at>
2014-03-20 10:55:26 +0100

    return type fix, fixes semantic bugs

--- code/frontend/src/utils/clang_utils.cpp
+++ code/frontend/src/utils/clang_utils.cpp
@@ -80,17 +80,41 @@ std::string removeSymbols(std::string& s) {
 /* we build a complete name for the class,
  * qualified name does not have the specific types of the spetialization
  */
-std::string getNameForRecord(const clang::NamedDecl* decl, const clang::QualType& type){
+std::string getNameForRecord(const clang::NamedDecl* decl, const clang::QualType& type, bool isDefinedInSystemHeader=false){
 
 	// beware of aliasing types, if we find a typedef, better to use the inner type
 	if (llvm::dyn_cast<clang::TypedefNameDecl>(decl)){
 		if (const clang::RecordType* recTy = llvm::dyn_cast<clang::RecordType>(type->getCanonicalTypeInternal().getTypePtr())){
 			// unleast is anonymous.. then there is no way to use anywhere else without the typedef name
-			if (!recTy->getDecl()->getNameAsString().empty() ){
-				return getNameForRecord(recTy->getDecl(), type->getCanonicalTypeInternal());
+			if (!recTy->getDecl()->getNameAsString().empty()){
+				return getNameForRecord(recTy->getDecl(), type->getCanonicalTypeInternal(), isDefinedInSystemHeader);
 			}
 		}
 	}
+
+    // hold on. if it is a cxxrecorddecl, the decl has no default name
+    // and we know the type, we return the name of the qualtype
+    if(const clang::RecordDecl* rec = llvm::dyn_cast<clang::RecordDecl>(decl)) {
+        if(decl->getNameAsString().empty() && !rec->isAnonymousStructOrUnion())
+        if(!isDefinedInSystemHeader) {
+            //sometimes we have a cpp lambda, don't touch this things.
+            //If it is no cpp lambda or not even a cxxrec use the normal behaviour
+            if(const clang::CXXRecordDecl* cxxrec = llvm::dyn_cast<clang::CXXRecordDecl>(decl)) {
+                if(!cxxrec->isLambda()) {
+                    //filter out some special cases
+                    //FIXME: find a smarter way to check for non
+                    //anonymous structs or union that have an anonymous type
+                    if( type.getUnqualifiedType().getAsString().find("<anonymous at") == std::string::npos )
+                        return type.getUnqualifiedType().getAsString();
+                }
+            } else {
+                    //FIXME: find a smarter way to check for non
+                    //anonymous structs or union that have an anonymous type
+                    if( type.getUnqualifiedType().getAsString().find("<anonymous at") == std::string::npos )
+                        return type.getUnqualifiedType().getAsString();
+            }
+        }
+    }
 
 	if(decl->getNameAsString().empty()){
 		// empty name, build an annonymous name for this fella
@@ -247,8 +271,8 @@ std::string buildNameForGlobal (const clang::VarDecl* varDecl, const clang::Sour
 
 	std::string name =  ss.str();
 	REMOVE_SYMBOLS(name);
-	boost::replace_all(name, ".", "_");  // names have full path, remove symbols 
-	boost::replace_all(name, "/", "_"); 
+	boost::replace_all(name, ".", "_");  // names have full path, remove symbols
+	boost::replace_all(name, "/", "_");
     boost::replace_all(name, "-", "_");
 
 	return name;
