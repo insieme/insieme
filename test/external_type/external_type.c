5f8b080eb8a4f9892b3f896ceda7be368f235410
Bernhard Höckner <bernhard.hoeckner@student.uibk.ac.at>
2014-06-10 12:29:58 +0200

    extending external_type test, and adding it to test.cfg (currently disabled)

--- test/external_type/external_type.c
+++ test/external_type/external_type.c
@@ -1,11 +1,16 @@
+#include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/XShm.h>
 
 int main() {
     Display *display = NULL;
 
-    if (XShmQueryExtension(display))
-        return 1;
+    display = XOpenDisplay(NULL);
+    if(!display) 
+    	return 1;
 
-    return 0;
+    if (XShmQueryExtension(display))
+        return 0;
+
+    return 1;
 }
