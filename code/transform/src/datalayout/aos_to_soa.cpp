fcd72742ac8f610f4e6297baeb40c33e4e78f604
Klaus <klaus@dps.uibk.ac.at>
2014-09-22 15:41:17 +0200

    fixed bug with nested data types

--- code/transform/src/datalayout/aos_to_soa.cpp
+++ code/transform/src/datalayout/aos_to_soa.cpp
@@ -377,6 +377,7 @@ AosToSoa::AosToSoa(core::NodePtr& toTransform) : mgr(toTransform->getNodeManager
 
 		// fixpoint iteration to capture all variables that new a new versions with new type
 		size_t curNumOfVars = 0;
+std::cout << "curNumOfVars: " << curNumOfVars << " != " << toReplaceList.size() << std::endl;
 		while(curNumOfVars != toReplaceList.size()) {
 			curNumOfVars = toReplaceList.size();
 			collectVariables(candidate, toReplaceList, tta, scopes);
@@ -505,42 +506,40 @@ void AosToSoa::collectVariables(const std::pair<ExpressionPtr, RefTypePtr>& tran
 			return;
 
 		// check if it is an access to the transformRoot's memory
-		if(*expr->getType() == *transformRoot.second) {
-			if(*transformRoot.first == *extractVariable(expr)) {
-//std::cout << "\ttesting: " << *expr << " to "<< *extractVariable(expr) << std::endl;
+		if(*transformRoot.first == *extractVariable(expr)) {
+			// if so, add all variable which alias it to the toReplaceList
+			visitDepthFirst(toTransform, [&](const ExpressionAddress& potentialAlias) {
+//std::cout << "\ttesting: " << *expr << " to "<< *potentialAlias << std::endl;
+				if(potentialAlias->getType()->getNodeType() != NT_RefType)
+					return;
 
-				// if so, add all variable which alias it to the toReplaceList
-				visitDepthFirst(toTransform, [&](const ExpressionAddress& potentialAlias) {
-					if(potentialAlias->getType()->getNodeType() != NT_RefType)
-						return;
-
-					if(*potentialAlias->getType() == *transformRoot.second) {
-//dumpPretty(potentialAlias);
-						if(ExpressionPtr varToAdd = extractVariable(potentialAlias)) {
+				if(*potentialAlias->getType() == *transformRoot.second)
+				{
+dumpPretty(potentialAlias);
+					if(ExpressionPtr varToAdd = extractVariable(potentialAlias)) {
 //dumpPretty(expr );
-							// no need to add the same variable again
-							if(*varToAdd == *transformRoot.first)
+						// no need to add the same variable again
+						if(*varToAdd == *transformRoot.first)
+							return;
+
+						if(expr->getNodeType() == NT_Variable && potentialAlias->getNodeType() == NT_Variable)
+							if(scopes[expr.as<VariableAddress>()] != scopes[potentialAlias.as<VariableAddress>()])
 								return;
 
-							if(expr->getNodeType() == NT_Variable && potentialAlias->getNodeType() == NT_Variable)
-								if(scopes[expr.as<VariableAddress>()] != scopes[potentialAlias.as<VariableAddress>()])
-									return;
 
-
-							if(varToAdd->getNodeType() == NT_Variable ||
-									(varToAdd->getNodeType() == NT_Literal && varToAdd->getType()->getNodeType() == NT_RefType)) {
-								if(ia::cba::mayAlias(expr, potentialAlias)) {
-									auto newly = toReplaceList.insert(varToAdd);
+						if(varToAdd->getNodeType() == NT_Variable ||
+								(varToAdd->getNodeType() == NT_Literal && varToAdd->getType()->getNodeType() == NT_RefType)) {
+							if(ia::cba::mayAlias(expr, potentialAlias)) {
+								auto newly = toReplaceList.insert(varToAdd);
 if(newly.second) {
-	std::cout << "Expr:  " << *expr;
-	std::cout << " alias: " << *potentialAlias << std::endl;
+std::cout << "Expr:  " << *expr;
+std::cout << " alias: " << *potentialAlias << std::endl;
 }
-								}
 							}
 						}
 					}
-				});
-			}
+				}
+			});
 		}
 	});
 
@@ -925,8 +924,8 @@ ExpressionMap AosToSoa::replaceAccesses(const ExpressionMap& varReplacements, co
 		const ExpressionPtr& oldVar = vr.first;
 		const ExpressionPtr& newVar = vr.second;
 
-		pattern::TreePattern structMemberAccess =  pattern::var("call", pirp::compositeRefElem(pirp::arrayRefElem1D(pirp::refDeref(pattern::atom(oldVar)),
-				var("index", pattern::any)), pattern::var("member", pattern::any)));
+		pattern::TreePattern structMemberAccess =  pattern::var("call", pirp::compositeRefElem(pirp::arrayRefElem1D(pirp::refDeref(
+				pattern::var("structAccess", pattern::aT(pattern::atom(oldVar)))), var("index", pattern::any)), pattern::var("member", pattern::any)));
 
 		pattern::TreePattern structAccess = pattern::var("call", pirp::refDeref(pirp::arrayRefElem1D(pirp::refDeref(pattern::atom(oldVar)),
 				var("index", pattern::any))));
@@ -964,14 +963,17 @@ ExpressionMap AosToSoa::replaceAccesses(const ExpressionMap& varReplacements, co
 			if(call) {
 				pattern::AddressMatchOpt match = structMemberAccess.matchAddress(node);
 //std::cout << "\nCall: ";
-// dumpPretty(node);
+//dumpPretty(node);
 				if(match) {
 //	assert(false);
-
-
 					StringValuePtr member = builder.stringValue(match.get()["member"].getValue().as<LiteralPtr>()->getStringValue());
 					ExpressionPtr index = match.get()["index"].getValue().as<ExpressionPtr>();
-					ExpressionPtr replacement = builder.arrayRefElem(builder.deref(builder.refMember(newVar, member)), index);
+
+					dumpPretty(match.get()["structAccess"].getValue());
+
+					ExpressionPtr newStructAccess = core::transform::fixTypes(mgr, match.get()["structAccess"].getValue().getAddressedNode(),
+							oldVar, newVar, false).as<ExpressionPtr>();
+					ExpressionPtr replacement = builder.arrayRefElem(builder.deref(builder.refMember(newStructAccess, member)), index);
 
 					replacements[node] = replacement;
 					return true;
@@ -1140,9 +1142,7 @@ std::cout << "ARG: " << arg << std::endl;
 				oldVar = varCheck->first;
 				newVar = varCheck->second;
 
-				ExpressionMap oldToNew;
-				oldToNew[oldVar] = newVar;
-				ExpressionPtr newArg = core::transform::fixTypesGen(mgr, oldVarArg, oldToNew, false);
+				ExpressionPtr newArg = core::transform::fixTypesGen(mgr, oldVarArg, oldVar, newVar, false);
 				indicesToNewArgs[idx] = newArg;
 			}
 		}
