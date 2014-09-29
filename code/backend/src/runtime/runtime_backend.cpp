1c663cde05a3c45ebe13b055317d36cd0e0e1ee7
Herbert <herbert@dps.uibk.ac.at>
2014-09-29 17:20:04 +0200

    Re-enabled long-long type interception.

--- code/backend/src/runtime/runtime_backend.cpp
+++ code/backend/src/runtime/runtime_backend.cpp
@@ -64,6 +64,7 @@
 #include "insieme/backend/addons/cpp_memb_ptr.h"
 #include "insieme/backend/addons/complex_type.h"
 #include "insieme/backend/addons/enum_type.h"
+#include "insieme/backend/addons/longlong_type.h"
 #include "insieme/backend/addons/simd_vector.h"
 #include "insieme/backend/addons/asm_stmt.h"
 #include "insieme/backend/addons/varargs.h"
@@ -100,6 +101,7 @@ namespace runtime {
 		res->addAddOn<addons::CppMembAddon>();
         res->addAddOn<addons::ComplexType>();
         res->addAddOn<addons::EnumTypes>();
+        res->addAddOn<addons::LongLongType>();
 		res->addAddOn<addons::SIMDVector>();
 		res->addAddOn<addons::AsmStmt>();
 		res->addAddOn<addons::VarArgs>();
