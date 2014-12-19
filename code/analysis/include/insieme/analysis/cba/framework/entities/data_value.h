29717cac4acf1ba6136e6f25213800ef5da1fbe7
Herbert <herbert@dps.uibk.ac.at>
2014-12-19 15:06:26 +0100

    Fixed a function name resolution problem encountered by the clang compiler.

--- code/analysis/include/insieme/analysis/cba/framework/entities/data_value.h
+++ code/analysis/include/insieme/analysis/cba/framework/entities/data_value.h
@@ -851,6 +851,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct projection_op {
+			projection_op() {}
 			template<typename IndexType>
 			const Data<BaseLattice>& operator()(const Data<BaseLattice>& value, const IndexType& index) const {
 				return value[index];
@@ -859,6 +860,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct mutation_op {
+			mutation_op() {}
 			Data<BaseLattice> operator()(DataManager<BaseLattice>& mgr, const Data<BaseLattice>& cur_state, const DataPath& path, const Data<BaseLattice>& new_value) const {
 				// forward request to data object
 				return cur_state.mutate(mgr, path, new_value);
@@ -867,6 +869,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct meet_assign_op {
+			meet_assign_op() {}
 			bool operator()(Data<BaseLattice>& a, const Data<BaseLattice>& b) const {
 				return a.meetAssign(b);
 			}
@@ -874,6 +877,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct less_op {
+			less_op() {}
 			template<typename E>
 			bool operator()(const E& e, const Data<BaseLattice>& a) const {
 				return a.contains(e);
@@ -1482,6 +1486,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct projection_op {
+			projection_op() {}
 			template<typename IndexType>
 			Data<BaseLattice> operator()(const Data<BaseLattice>& value, const IndexType& index) const {
 				return value[index];
@@ -1490,6 +1495,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct mutation_op {
+			mutation_op() {}
 			Data<BaseLattice> operator()(DataManager<BaseLattice>& mgr, const Data<BaseLattice>& cur_state, const DataPath& path, const Data<BaseLattice>& new_value) const {
 				return cur_state.mutate(mgr, path, new_value);
 			}
@@ -1497,6 +1503,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct meet_assign_op {
+			meet_assign_op() {}
 			bool operator()(Data<BaseLattice>& a, const Data<BaseLattice>& b) const {
 				return a.meetAssign(b);
 			}
@@ -1504,6 +1511,7 @@ namespace cba {
 
 		template<typename BaseLattice>
 		struct less_op {
+			less_op() {}
 			template<typename E>
 			bool operator()(const E& e, const Data<BaseLattice>& a) const {
 				return a.contains(e);
