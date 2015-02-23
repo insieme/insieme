bef77464fdf3af19756902a6aca4d4a50a392811
Klaus <klaus@dps.uibk.ac.at>
2015-02-23 17:28:18 +0100

    updated testcases

--- code/transform/test/datalayout/aos_to_soa_test.cc
+++ code/transform/test/datalayout/aos_to_soa_test.cc
@@ -136,10 +136,10 @@ TEST(DataLayout, AosToSoa) {
 //
 //	return;
 
-//	dumpPretty(code);
-
 	datalayout::AosToSoa ats(code, datalayout::findAllSuited);
 	ats.transform();
+
+//	dumpPretty(code);
 
 	auto semantic = checks::check(code);
 	auto warnings = semantic.getWarnings();
@@ -160,6 +160,72 @@ TEST(DataLayout, AosToSoa) {
 	EXPECT_EQ(10, countMarshalledAccesses(code));
 	EXPECT_EQ(4, countMarshalledAssigns(code));
 }
+/*
+let type000 = struct<
+	int:int<4>,
+	float:real<4>
+>;
+
+let type001 = struct<
+	int:ref<array<int<4>,1>>,
+	float:ref<array<real<4>,1>>
+>;
+
+{
+    decl ref<ref<array<type000,1>>> v0 =  var( new(array.create.1D(type<type000>, 100u)));
+    decl ref<type001> v90 =  var(undefined(type<type001>));
+    v90->int :=  new(array.create.1D(type<int<4>>, 100u));
+    v90->float :=  new(array.create.1D(type<real<4>>, 100u));
+    v0 :=  new(array.create.1D(type<type000>, 100u));
+    v90->int :=  new(array.create.1D(type<int<4>>, 100u));
+    v90->float :=  new(array.create.1D(type<real<4>>, 100u));
+    for(decl int<4> v1 = 0 .. 100 : 1) {
+        decl ref<type000> v2 =  var(undefined(type<type000>));
+         *v0&[v1] :=  *v2;
+    };
+    for(decl int<4> v97 = 0 .. 100 : 1) {
+         *v90->int&[v97] :=  * *v0&[v97]->int;
+         *v90->float&[v97] :=  * *v0&[v97]->float;
+    };
+    for(decl int<4> v3 = 0 .. 42 : 1) {
+        decl ref<type000> v4 =  var(struct{int:= * *v90->int&[v3], float:= * *v90->float&[v3]});
+        decl ref<ref<array<type000,1>>> v5 =  var( *v0);
+        decl ref<type001> v91 =  var(undefined(type<type001>));
+        v91->int :=  *v90->int;
+        v91->float :=  *v90->float;
+        v5 :=  *v0;
+        v91->int :=  *v90->int;
+        v91->float :=  *v90->float;
+        v0 :=  *v5;
+        v90->int :=  *v91->int;
+        v90->float :=  *v91->float;
+        decl ref<ref<array<type000,1>>> v6 =  var(undefined(type<ref<array<type000,1>>>));
+        decl ref<type001> v92 =  var(undefined(type<type001>));
+        v92->int := undefined(type<ref<array<int<4>,1>>>);
+        v92->float := undefined(type<ref<array<real<4>,1>>>);
+        v6 :=  *v0;
+        v92->int :=  *v90->int;
+        v92->float :=  *v90->float;
+        decl ref<ref<array<type000,1>>> v7 =  var(scalar.to.array( *v0&[v3]));
+        decl ref<type001> v93 =  var(undefined(type<type001>));
+        v93->int := scalar.to.array( *v90->int&[v3]);
+        v93->float := scalar.to.array( *v90->float&[v3]);
+         *v93.int&[v3] := v3;
+         *v90.int&[v3] := v3;
+    };
+    for(decl int<4> v98 = 0 .. 100 : 1) {
+         *v0&[v98]->int :=  * *v90->int&[v98];
+         *v0&[v98]->float :=  * *v90->float&[v98];
+    };
+    for(decl int<4> v8 = 0 .. 100 : 1) {
+        decl ref<type000> v9 =  var(undefined(type<type000>));
+        v9 :=  * *v0&[v8];
+    };
+     del( *v90->int);
+     del( *v90->float);
+     del( *v0);
+}
+*/
 
 TEST(DataLayout, AosToSoa2) {
 	NodeManager mgr;
@@ -226,10 +292,68 @@ TEST(DataLayout, AosToSoa2) {
 		std::cout << cur << std::endl;
 	});
 
-	EXPECT_EQ(64, numberOfCompoundStmts(code));
-	EXPECT_EQ(9, countMarshalledAccesses(code));
+	EXPECT_EQ(73, numberOfCompoundStmts(code));
+	EXPECT_EQ(11, countMarshalledAccesses(code));
 	EXPECT_EQ(7, countMarshalledAssigns(code));
 }
+/*
+let type000 = struct<
+	int:int<4>,
+	float:real<4>
+>;
+
+let type001 = struct<
+	int:ref<array<int<4>,1>>,
+	float:ref<array<real<4>,1>>
+>;
+
+let fun000 = fun(ref<array<type000,1>> v1, int<4> v2, type001 v154) -> unit {
+    for(decl int<4> v3 = 0 .. 42 : 1) {
+        v154.int&[v3] := v3;
+    };
+};
+
+{
+    decl ref<ref<array<type000,1>>> v0 =  var(undefined(type<ref<array<type000,1>>>));
+    decl ref<type001> v152 =  var(undefined(type<type001>));
+    v152->int := undefined(type<ref<array<int<4>,1>>>);
+    v152->float := undefined(type<ref<array<real<4>,1>>>);
+    v0 :=  new(array.create.1D(type<type000>, 100u));
+    v152->int :=  new(array.create.1D(type<int<4>>, 100u));
+    v152->float :=  new(array.create.1D(type<real<4>>, 100u));
+    loadData(v0);
+    for(decl uint<4> v155 = 0 .. 100u : 1) {
+         *v152->int&[v155] :=  * *v0&[v155]->int;
+         *v152->float&[v155] :=  * *v0&[v155]->float;
+    };
+    decl ref<ref<array<type000,1>>> v1 =  var( *v0);
+    decl ref<type001> v153 =  var(undefined(type<type001>));
+    v153->int :=  *v152->int;
+    v153->float :=  *v152->float;
+    for(decl uint<4> v159 = 0 .. 100u : 1) {
+         *v1&[v159]->int :=  * *v153->int&[v159];
+         *v1&[v159]->float :=  * *v153->float&[v159];
+    };
+    storeData( *v1);
+    for(decl uint<4> v157 = 0 .. 100u : 1) {
+         *v153->int&[v157] :=  * *v1&[v157]->int;
+         *v153->float&[v157] :=  * *v1&[v157]->float;
+    };
+    fun000( *v0, 0,  *v152);
+    for(decl uint<4> v158 = 0 .. 100u : 1) {
+         *v0&[v158]->int :=  * *v152->int&[v158];
+         *v0&[v158]->float :=  * *v152->float&[v158];
+    };
+    storeData( *v0);
+    for(decl uint<4> v156 = 0 .. 100u : 1) {
+         *v152->int&[v156] :=  * *v0&[v156]->int;
+         *v152->float&[v156] :=  * *v0&[v156]->float;
+    };
+     del( *v152->int);
+     del( *v152->float);
+     del( *v0);
+}
+*/
 
 TEST(DataLayout, Globals) {
 	NodeManager mgr;
@@ -288,6 +412,62 @@ TEST(DataLayout, Globals) {
 	EXPECT_EQ(13, countMarshalledAccesses(code));
 	EXPECT_EQ(7, countMarshalledAssigns(code));
 }
+/*
+let type000 = struct<
+	int:int<4>,
+	float:real<4>
+>;
+
+let type001 = struct<
+	int:ref<array<int<4>,1>>,
+	float:ref<array<real<4>,1>>
+>;
+
+let fun000 = fun(ref<ref<array<type000,1>>> v1, ref<type001> v154) -> unit {
+    for(decl int<4> v2 = 0 .. 42 : 1) {
+         *v154.int&[v2] := v2;
+    };
+};
+
+let fun001 = fun(int<4> v1) -> unit {
+    decl ref<type000> v2 =  var(struct{int:= * *a_soa->int&[v1], float:= * *a_soa->float&[v1]});
+    decl ref<ref<array<type000,1>>> v3 =  var( *a);
+    decl ref<type001> v155 =  var(undefined(type<type001>));
+    v155->int :=  *a_soa->int;
+    v155->float :=  *a_soa->float;
+    decl ref<ref<array<type000,1>>> v4 =  var(scalar.to.array( *a&[v1]));
+    decl ref<type001> v156 =  var(undefined(type<type001>));
+    v156->int := scalar.to.array( *a_soa->int&[v1]);
+    v156->float := scalar.to.array( *a_soa->float&[v1]);
+     *v156.int&[v1] := v1;
+     *a_soa.int&[v1] := v1;
+};
+
+{
+    a :=  new(array.create.1D(type<type000>, 100u));
+    a_soa->int :=  new(array.create.1D(type<int<4>>, 100u));
+    a_soa->float :=  new(array.create.1D(type<real<4>>, 100u));
+    loadData(a);
+    for(decl uint<4> v157 = 0 .. 100u : 1) {
+         *a_soa->int&[v157] :=  * *a&[v157]->int;
+         *a_soa->float&[v157] :=  * *a&[v157]->float;
+    };
+    fun000(a, a_soa);
+    fun001(7);
+    for(decl uint<4> v159 = 0 .. 100u : 1) {
+         *a&[v159]->int :=  * *a_soa->int&[v159];
+         *a&[v159]->float :=  * *a_soa->float&[v159];
+    };
+    storeData( *a);
+    for(decl uint<4> v158 = 0 .. 100u : 1) {
+         *a_soa->int&[v158] :=  * *a&[v158]->int;
+         *a_soa->float&[v158] :=  * *a&[v158]->float;
+    };
+     del( *a_soa->int);
+     del( *a_soa->float);
+     del( *a);
+}
+*/
 
 TEST(DataLayout, Tuple) {
 	NodeManager mgr;
@@ -347,6 +527,44 @@ TEST(DataLayout, Tuple) {
 	EXPECT_EQ(2, countMarshalledAccesses(code));
 	EXPECT_EQ(2, countMarshalledAssigns(code));
 }
+/*
+let type000 = struct<
+	int:int<4>,
+	float:real<4>
+>;
+
+let type001 = struct<
+	int:ref<array<int<4>,1>>,
+	float:ref<array<real<4>,1>>
+>;
+
+let fun000 = fun(ref<array<ref<array<type000,1>>,1>> v1, ref<array<type001,1>> v157) -> unit {
+    for(decl int<4> v2 = 0 .. 42 : 1) {
+         *v157&[0].int&[v2] := v2;
+    };
+};
+
+let fun001 = fun(ref<(ref<array<type001,1>>,ref<array<ref<array<real<4>,1>>,1>>,ref<array<uint<8>,1>>)> v160, ref<array<ref<array<type000,1>>,1>> v2, ref<array<type001,1>> v158) -> unit {
+     *v158&[0].int&[3] := 7;
+    tuple.ref.elem(v160, 0, type<ref<array<type001,1>>>) := v158;
+};
+
+{
+    decl ref<ref<array<type000,1>>> v0 =  var(undefined(type<ref<array<type000,1>>>));
+    decl ref<type001> v156 =  var(undefined(type<type001>));
+    v156->int := undefined(type<ref<array<int<4>,1>>>);
+    v156->float := undefined(type<ref<array<real<4>,1>>>);
+    decl ref<ref<array<real<4>,1>>> v1 =  var(undefined(type<ref<array<real<4>,1>>>));
+    decl ref<uint<8>> v2 =  var(undefined(type<uint<8>>));
+    decl ref<ref<(ref<array<type001,1>>,ref<array<ref<array<real<4>,1>>,1>>,ref<array<uint<8>,1>>)>> v159 =  var(undefined(type<ref<(ref<array<type001,1>>,ref<array<ref<array<real<4>,1>>,1>>,ref<array<uint<8>,1>>)>>));
+    v159 :=  new(undefined(type<(ref<array<type001,1>>,ref<array<ref<array<real<4>,1>>,1>>,ref<array<uint<8>,1>>)>));
+     *v0;
+    fun000(scalar.to.array(v0), scalar.to.array(v156));
+    tuple.ref.elem( *v159, 0, type<ref<array<type001,1>>>) := scalar.to.array(v156);
+    fun001( *v159, scalar.to.array(v0), scalar.to.array(v156));
+     del( *v159);
+}
+*/
 
 } // transform
 } // insieme
