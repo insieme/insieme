d3c8893752a3b2f0a139e0b36bf2a6cc36021ce5
Peter Thoman <petert@dps.uibk.ac.at>
2015-06-29 13:25:00 +0200

    New string constructor test

---
+++ test/cpp/new_string/new_string.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+int main(int argc, char** argv) {
+	std::string *s1 = new std::string;
+	std::string *s2 = new std::string();
+	std::string *s3 = new std::string("bla");
+	std::string x;
+	return 0;
+}
