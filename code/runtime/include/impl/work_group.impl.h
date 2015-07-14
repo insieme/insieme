e9d4052461edd62fd6bae9025e2a6f8a133c26f7
Peter Zangerl <peterz@dps.uibk.ac.at>
2015-07-14 10:33:59 +0200

    runtime: Fixed race condition in WG ending.

--- code/runtime/include/impl/work_group.impl.h
+++ code/runtime/include/impl/work_group.impl.h
@@ -98,7 +98,15 @@ void irt_wg_end(irt_work_group* wg) {
 
 static inline void _irt_wg_end_member(irt_work_group* wg) {
 	//IRT_INFO("_irt_wg_end_member: %u / %u\n", wg->ended_member_count, wg->local_member_count);
-	if(irt_atomic_add_and_fetch(&wg->ended_member_count, 1, uint32) == wg->local_member_count) {
+	/*
+	 * Note: We have to store the current value of wg->local_member_count in a local member here,
+	 * in order to avoid a race condition. If we do not store the value here and multiple threads
+	 * increment the ended_member_count concurrently, the last one might actually already be
+	 * done with cleaning up the work group until some other thread tries to read the
+	 * local_member_count field of a work group which no longer exists and thus cause a segfault.
+	 */
+	uint32 member_count = wg->local_member_count;
+	if(irt_atomic_add_and_fetch(&wg->ended_member_count, 1, uint32) == member_count) {
 		irt_wg_event_trigger(wg->id, IRT_WG_EV_COMPLETED);
 		irt_wg_end(wg);
 	}
