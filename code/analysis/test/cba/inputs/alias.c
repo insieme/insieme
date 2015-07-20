4ed763e1b7518eb3ea224f6fcf650d90fcf80aba
Peter Thoman <petert@dps.uibk.ac.at>
2015-07-20 14:47:28 +0200

    CBA tests: add missing returns

--- code/analysis/test/cba/inputs/alias.c
+++ code/analysis/test/cba/inputs/alias.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2002-2013 Distributed and Parallel Systems Group,
+ * Copyright (c) 2002-2015 Distributed and Parallel Systems Group,
  *                Institute of Computer Science,
  *               University of Innsbruck, Austria
  *
@@ -29,8 +29,8 @@
  *
  * All copyright notices must be kept intact.
  *
- * INSIEME depends on several third party software packages. Please 
- * refer to http://www.dps.uibk.ac.at/insieme/license.html for details 
+ * INSIEME depends on several third party software packages. Please
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details
  * regarding third party software licenses.
  */
 
@@ -54,4 +54,5 @@ int main() {
 	cba_expect_is_alias(*d,c);
 	cba_expect_is_alias(*d,c);
 
+	return 0;
 }
