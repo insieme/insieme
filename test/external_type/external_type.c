48a169a56485dfa13bea9a650146000865c87d16
Peter Zangerl <peterz@dps.uibk.ac.at>
2015-09-29 16:20:35 +0200

    integration_tests: Replaced external_types test' content with something which doesn't need X11.
    I already stumbled upon this somewhen in the past - today again it made the build fail on hudson even though it is green on the developer machines.

--- test/external_type/external_type.c
+++ test/external_type/external_type.c
@@ -1,15 +1,9 @@
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/extensions/XShm.h>
+#include <stdio.h>
+#include <sys/time.h>
 
 int main() {
-    XEvent xev;
-    Display *display = XOpenDisplay("");
+	struct timeval time;
 
-    XCheckMaskEvent(display, 0, &xev);
-
-    if (XShmQueryExtension(display))
-        return 0;
-    else
-        return 1;
+	int res = gettimeofday(&time, NULL);
+	printf("%d\n", res);
 }
