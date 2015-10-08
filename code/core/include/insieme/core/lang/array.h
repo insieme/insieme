c850a827362d3cf6559f59f8fecf77d727dcdafc
Peter Zangerl <peterz@dps.uibk.ac.at>
2015-10-08 15:19:10 +0200

    parser: Adapted basic core files and extensions to new parser syntax.

--- code/core/include/insieme/core/lang/array.h
+++ code/core/include/insieme/core/lang/array.h
@@ -98,7 +98,7 @@ namespace lang {
 		 */
 		LANG_EXT_DERIVED_WITH_NAME(ArrayReduce, "array_reduce",
 				    "                                                                                             "
-					"   (data : ref<array<'a,'s>,'v,'c,plain>, size : int<8>, op : ('b,'a)->'b, init : 'b)->'b {   "
+					"   lambda (data : ref<array<'a,'s>,'v,'c,plain>, size : int<8>, op : ('b,'a)->'b, init : 'b)->'b {   "
 					"   	var ref<'b,f,f,plain> res = ref_var(init);                                               "
 					"   	for(int<8> i = 0 .. size) {                                                           "
 					"   		res = op(*res, *(data[i]));                                                       "
@@ -113,7 +113,7 @@ namespace lang {
 		 */
 		LANG_EXT_DERIVED_WITH_NAME(ArrayFold, "array_fold",
 					"                                                                                       "
-					"   (data : array<'a,'s>, init : 'b, op : ('b,'a)->'b)->'b {                           "
+					"   lambda (data : array<'a,'s>, init : 'b, op : ('b,'a)->'b)->'b {                           "
 					"   	var ref<'b,f,f,plain> res = ref_var(init);                                         "
 					"   	for(int<8> i = 0 .. type_to_int(type_lit('s))) {                                     "
 					"   		res = op(*res, data[i]);                                                    "
