e5d4f3548f7a5fa00995f7b3705235c33ede43a0
Peter Zangerl <peterz@dps.uibk.ac.at>
2015-10-19 16:51:45 +0200

    parser: Partially fixed sequentialize_test.

--- code/core/test/transform/sequentialize_test.cc
+++ code/core/test/transform/sequentialize_test.cc
@@ -56,23 +56,23 @@ namespace transform {
 		NodeManager mgr;
 		IRBuilder builder(mgr);
 
-		StatementPtr code = analysis::normalize(builder.parseStmt("{"
-		                                                          "	let int = int<4>;"
-		                                                          "	decl ref<int> a = var(2);"
+		StatementPtr code = analysis::normalize(builder.parseStmt("alias int = int<4>;"
+		                                                          "{"
+		                                                          "	var ref<int> a = ref_var(2);"
 		                                                          "	atomic_fetch_and_add(a, 10);"
 		                                                          "}")
 		                                            .as<StatementPtr>());
 
 		ASSERT_TRUE(code);
 
-		EXPECT_EQ("{decl ref<int<4>,f,f,plain> v0 =  var(2);atomic_fetch_and_add(v0, 10);}",
+		EXPECT_EQ("{var ref<int<4>,f,f,plain> v0 =  ref_var(2);atomic_fetch_and_add(v0, 10);}",
 		          toString(printer::PrettyPrinter(code, printer::PrettyPrinter::PRINT_SINGLE_LINE)));
 		EXPECT_TRUE(check(code, checks::getFullCheck()).empty()) << check(code, checks::getFullCheck());
 
 
 		auto res = analysis::normalize(transform::trySequentialize(mgr, code));
 		//		std::cout << core::printer::PrettyPrinter(res) << "\n";
-		EXPECT_EQ("{decl ref<int<4>,f,f,plain> v0 =  var(2);function(ref<ref<'a,f,'v,plain>,f,f,plain> v1, ref<'a,f,f,plain> v2) -> 'a {return function(ref<ref<'a,f,'v,plain>,f,f,plain> v1, ref<'a,f,f,plain> v2) -> 'a {decl 'a v3 = v1;v1 = gen_add(v1, v2);return v3;}(v1, v2);}(v0, 10);}",
+		EXPECT_EQ("{var ref<int<4>,f,f,plain> v0 =  ref_var(2);function(v1 : ref<ref<'a,f,'v,plain>,f,f,plain>, v2 : ref<'a,f,f,plain>) -> 'a {return (v1 : ref<'a,f,'v,plain>,f,f,plain, v2 : 'a) -> 'a {var 'a v3 = v1;v1 = gen_add(v1, v2);return v3;}(v1, v2);}(v0, 10);}",
 		          toString(printer::PrettyPrinter(res, printer::PrettyPrinter::PRINT_SINGLE_LINE)));
 		EXPECT_TRUE(check(res, checks::getFullCheck()).empty()) << check(res, checks::getFullCheck());
 	}
