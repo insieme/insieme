b26947887647ae5fcca7204234a2a6fd5e4b0fcf
stefanm <stefan.moosbrugger@student.uibk.ac.at>
2015-10-20 14:41:44 +0200

    fixed varargs_extension_test

--- code/frontend/test/inputs/conversion/c_variable_argument_lists.c
+++ code/frontend/test/inputs/conversion/c_variable_argument_lists.c
@@ -10,19 +10,19 @@ int main() {
 	#define BLA "lit(\"bla\":(int<4>,int<4>,var_list)->unit)"
 	#define FOO "lit(\"foo\":(int<4>,var_list)->unit)"
 
-	#pragma test expect_ir("{",PREAMBLE,BLA,"(1, 2, varlist_pack(())); }")
+	#pragma test expect_ir(PREAMBLE "{",BLA,"(1, 2, varlist_pack(())); }")
 	{ bla(1,2); }
 	
-	#pragma test expect_ir("{",PREAMBLE,BLA,"(2, 3, varlist_pack((17))); }")
+	#pragma test expect_ir(PREAMBLE "{",BLA,"(2, 3, varlist_pack((17))); }")
 	{ bla(2,3, 17); }
 	
-	#pragma test expect_ir("{",PREAMBLE,BLA,"(5, 6, varlist_pack((5u, 17))); }")
+	#pragma test expect_ir(PREAMBLE "{",BLA,"(5, 6, varlist_pack((5u, 17))); }")
 	{ bla(5,6, 5u, 17); }
 	
-	#pragma test expect_ir("{",PREAMBLE,FOO,"(1, varlist_pack(())); }")
+	#pragma test expect_ir(PREAMBLE "{",FOO,"(1, varlist_pack(())); }")
 	{ foo(1); }
 	
-	#pragma test expect_ir("{",PREAMBLE,FOO,"(1, varlist_pack((lambda () -> int<4> { return 0;}()))); }")
+	#pragma test expect_ir(PREAMBLE "{",FOO,"(1, varlist_pack((() -> int<4> { return 0;}()))); }")
 	{ foo(1, bar()); }
 
 	return 0;
