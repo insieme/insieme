ff4937052d9be1068df069a948f5bd361ba799bd
Markus Walzthoeni <markus.walzthoeni@student.uibk.ac.at>
2015-11-17 09:32:43 +0100

    printer: snippets test fixed. minor code style changes

--- code/backend/test/code_snippets_test.cc
+++ code/backend/test/code_snippets_test.cc
@@ -608,7 +608,10 @@ namespace backend {
 			EXPECT_PRED2(notContainsSubString, code, "<?>");
 			EXPECT_PRED2(notContainsSubString, code, "<a>");
 			EXPECT_PRED2(notContainsSubString, code, "UNSUPPORTED");
-			EXPECT_PRED2(containsSubString, code, "{\n    v0 = function() -> int<4> {\n        return g(f());\n    };\n}");
+			EXPECT_PRED2(containsSubString, code, "{v0 = () -> int<4> {\n"
+													"        return g(f());\n"
+													"    };\n"
+													"}");
 
 			// try compiling the code fragment
 			utils::compiler::Compiler compiler = utils::compiler::Compiler::getDefaultCppCompiler();
