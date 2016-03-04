da46e8721c36e97a475aa1510dde0fed31102f61
Peter Thoman <petert@dps.uibk.ac.at>
2016-03-04 15:38:52 +0100

    Core: add printer option to print full literal syntax

--- code/core/include/insieme/core/printer/pretty_printer.h
+++ code/core/include/insieme/core/printer/pretty_printer.h
@@ -110,7 +110,8 @@ namespace printer {
 			PRINT_LITERAL_TYPES = 1 << 13,
 			USE_COLOR = 1 << 14,
 			PRINT_DERIVED_IMPL = 1 << 15,
-			JUST_LOCAL_CONTEXT = 1 << 16
+			JUST_LOCAL_CONTEXT = 1 << 16,
+			FULL_LITERAL_SYNTAX = 1 << 17
 		};
 
 		/**
--- code/core/src/printer/pretty_printer.cpp
+++ code/core/src/printer/pretty_printer.cpp
@@ -1132,6 +1132,14 @@ namespace printer {
 					return;
 				}
  				const string& str = node->getStringValue();
+				// decide whether to really do the full print
+				bool doFullSyntax = printer.hasOption(PrettyPrinter::FULL_LITERAL_SYNTAX);
+				if(doFullSyntax) {
+					if(core::lang::isBuiltIn(node) || core::lang::isBuiltIn(node->getType())) doFullSyntax = false;
+				}
+				if(doFullSyntax) {
+					out << "lit(\"";
+				}
 				if(printer.hasOption(PrettyPrinter::NAME_CONTRACTION) && str.size() > 9) {
 					out << str.substr(0, 3) << "..." << str.substr(str.size() - 3, str.size());
 				} else {
@@ -1151,11 +1159,14 @@ namespace printer {
 
 					if (basic.isInt8(type))     out << "l";
 					if (basic.isInt16(type))    out << "ll";
-
 				}
 
-				// add type if requested
-				if(printer.hasOption(PrettyPrinter::PRINT_LITERAL_TYPES)) {
+				if(doFullSyntax) {
+					out << "\" : ";
+					VISIT(node->getType());
+					out << ")";
+				}
+				else if(printer.hasOption(PrettyPrinter::PRINT_LITERAL_TYPES)) {
 					out << ":";
 					VISIT(node->getType());
 				}
