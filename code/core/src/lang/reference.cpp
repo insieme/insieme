ea272959c9f9a880a18532832e689af922186381
Peter Thoman <petert@dps.uibk.ac.at>
2016-04-14 16:22:56 +0200

    Core: omit reference cast on type matching

--- code/core/src/lang/reference.cpp
+++ code/core/src/lang/reference.cpp
@@ -348,7 +348,7 @@ namespace lang {
 	ExpressionPtr buildRefReinterpret(const ExpressionPtr& refExpr, const TypePtr& targetTy) {
 		assert_pred1(isReference, refExpr) << "Trying to build a ref reinterpret cast from non-ref.";
 		auto rT = ReferenceType(refExpr);
-		if(rT.getElementType() == targetTy) return refExpr;
+		if(rT.getElementType() == targetTy || core::types::isMatchable(rT.getElementType(), targetTy)) return refExpr;
 		IRBuilder builder(refExpr->getNodeManager());
 		auto& rExt = refExpr->getNodeManager().getLangExtension<ReferenceExtension>();
 		return builder.callExpr(rExt.getRefReinterpret(), refExpr, builder.getTypeLiteral(targetTy));
