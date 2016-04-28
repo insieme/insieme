d9a361549b8ac1f02ac359392ebc52870c85ad0e
Laurent Seiler <laurent.seiler@student.uibk.ac.at>
2016-04-28 13:16:45 +0200

    Added Soufflé install script

---
+++ scripts/souffle_install.sh
@@ -0,0 +1,62 @@
+# setup environment variables
+. ./environment.setup
+set -e
+
+VERSION=20160420
+
+########################################################################
+##                       SOUFFLÉ DATALOG ENGINE
+########################################################################
+
+strip_java_from_souffle()
+{
+	rm configure Makefile.in
+	ed configure.ac <<-'EOF'
+		/JAVA
+		.,.+2d
+		/prof
+		d
+		/prof
+		d
+		w
+		q
+	EOF
+	ed Makefile.am <<-EOF
+		/prof
+		s/ prof//
+		w
+		q
+	EOF
+}
+
+if [ -d $PREFIX/souffle-$VERSION ]; then
+	echo "Soufflé version $VERSION already installed"
+	exit 0
+fi
+
+rm -rf $PREFIX/souffle-$VERSION
+echo "#### Downloading Soufflé ####"
+wget -nc http://www.dps.uibk.ac.at/~csaf7445/ext_libs/souffle-$VERSION.zip
+
+unzip -d souffle-$VERSION souffle-$VERSION.zip
+cd souffle-$VERSION/souffle-master
+
+export LD_LIBRARY_PATH=$PREFIX/gcc-latest/lib64:$LD_LIBRARY_PATH
+
+echo "#### Building Soufflé ####"
+strip_java_from_souffle
+sh bootstrap
+./configure --prefix=$PREFIX/souffle-$VERSION
+make -j $SLOTS
+
+echo "#### Installing Soufflé ####"
+make install
+
+rm -f $PREFIX/souffle-latest
+ln -s $PREFIX/souffle-$VERSION $PREFIX/souffle-latest
+
+echo "#### Cleaning up environment ####"
+cd ../..
+rm -rf souffle-$VERSION*
+
+exit 0
