c9111267a9f1aa14ecee374912520488f6f6de06
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2016-05-27 13:23:14 +0200

    HAT: Fix unnecessary building of libHSinsieme-hat

--- code/analysis/src/haskell/insieme-hat/utils/copyLib.hs
+++ code/analysis/src/haskell/insieme-hat/utils/copyLib.hs
@@ -1,24 +1,43 @@
+import Control.Monad
+import Data.Time.Clock
 import System.Directory
 import System.Exit
 import System.Environment
 import System.FilePath
 import System.FilePath.Glob
 
+outOfDate :: UTCTime -> FilePath -> IO Bool
+outOfDate ts dst = do
+    exists <- doesFileExist dst
+    if not exists
+       then return True
+       else do
+           mtime <- getModificationTime dst
+           return $ mtime > ts
+
 main :: IO ()
 main = do
 
-    args <- getArgs
+    args  <- getArgs
     if null args then exitFailure else return ()
 
     -- find shared libraries
-    libs <- head . fst <$> globDir [(compile "install/**/libHSinsieme-hat-*.so")] ".stack-work"
+    libs  <- head . fst <$> globDir [(compile "install/**/libHSinsieme-hat-*.so")] ".stack-work"
 
     -- attach timestamps
-    ts   <- mapM getModificationTime libs
+    ts    <- mapM getModificationTime libs
 
     -- find latest lib
-    let latestlib = snd $ maximum $ zip ts libs
+    let (latestts, latestlib) = maximum $ zip ts libs
+
+    -- check for out-of-date
+    isOod <- outOfDate latestts (args !! 0 </> "libHSinsieme-hat.so")
 
     -- copy original + simple name
-    copyFile latestlib ((args !! 0) </> "libHSinsieme-hat.so")
-    copyFile latestlib ((args !! 0) </> takeFileName latestlib)
+    if isOod
+        then do
+            copyFile latestlib  (args !! 0 </> "libHSinsieme-hat.so")
+            setModificationTime (args !! 0 </> "libHSinsieme-hat.so") latestts
+            copyFile latestlib  (args !! 0 </> takeFileName latestlib)
+            setModificationTime (args !! 0 </> takeFileName latestlib) latestts
+        else return ()
