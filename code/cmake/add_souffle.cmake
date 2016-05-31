aa586da5a28462fa091474e00c5594446dd4573a
Laurent Seiler <laurent.seiler@student.uibk.ac.at>
2016-05-31 17:47:19 +0200

    Trying to integrate a soufflé preprocessor into CMake

--- code/cmake/add_souffle.cmake
+++ code/cmake/add_souffle.cmake
@@ -2,17 +2,23 @@
 macro(configure_souffle)
 
 	# Settings
+	set(souffle_tmp_dir       ${CMAKE_CURRENT_BINARY_DIR}/souffle_tmp)
+	set(souffle_dough_done    ${souffle_tmp_dir}/preprocessor_done.txt)
 	set(souffle_output_base   ${CMAKE_CURRENT_BINARY_DIR}/souffle_gen)
 	set(souffle_output_path   ${souffle_output_base}/souffle/gen)
 
 	# Define preprocessor macro for embedded soufflé
 	add_definitions(-D__EMBEDDED_SOUFFLE__)
 
-	# Create output directory
+	# Create tmp and output directory
+	file(MAKE_DIRECTORY ${souffle_tmp_dir})
 	file(MAKE_DIRECTORY ${souffle_output_path})
 
 	# Generated files will also be included as header files
 	include_directories(SYSTEM ${souffle_output_base})
+
+	# Find the Dough script, a preprocessor script
+	set(souffle_dough ${CMAKE_SOURCE_DIR}/code/analysis/src/souffle_dough.sh)
 
 	# Find Soufflé installation directory
 	set(souffle_home $ENV{INSIEME_LIBS_HOME}/souffle-latest CACHE PATH "Souffle Home Directory")	# TODO: use a FindSouffle to locate it
@@ -25,6 +31,26 @@ macro(configure_souffle)
 	set(souffle_binary ${souffle_home}/bin/souffle)
 
 endmacro(configure_souffle)
+
+
+
+macro(souffle_run_dough input_dir output_dir)
+
+	# Glob the dl-files to add them as a dependency
+	file(GLOB souffle_original_dls         ${input_dir}/*.dl)
+	file(GLOB souffle_original_dl_includes ${input_dir}/include/*.dl)
+
+	# Execute on given input dir
+	add_custom_command(
+		COMMAND ${souffle_dough}
+		ARGS ${input_dir} ${output_dir}
+		COMMENT "todo"
+		DEPENDS ${souffle_original_dls} ${souffle_original_dl_includes}
+		WORKING_DIRECTORY ${input_dir}
+		OUTPUT ${souffle_dough_done}
+	)
+
+endmacro(souffle_run_dough)
 
 
 
@@ -55,7 +81,7 @@ macro(souffle_generate_cpp souffle_input_path souffle_dl_target souffle_include_
 		COMMAND ${souffle_binary}
 		ARGS -g ${souffle_output_file}.h ${souffle_input_file} ${include_argument}
 		COMMENT "Generating compiled soufflé datalog: ${souffle_dl_target}.h"
-		DEPENDS ${souffle_input_file} ${include_dl_dependencies}
+		DEPENDS ${souffle_dough_done} ${souffle_input_file} ${include_dl_dependencies}
 		IMPLICIT_DEPENDS C ${souffle_input_file}
 		WORKING_DIRECTORY ${souffle_output_path}
 		OUTPUT ${souffle_output_file}.h
