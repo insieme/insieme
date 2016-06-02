5e0f63a2f1c97451c58c935ec2f69caa43980f55
Laurent Seiler <laurent.seiler@student.uibk.ac.at>
2016-06-02 14:51:39 +0200

    Working on dough

--- code/analysis/src/datalog/souffle_dough.sh
+++ code/analysis/src/datalog/souffle_dough.sh
@@ -6,6 +6,9 @@ srcdir="${1}"
 outdir="${2}"
 incdir="include"
 
+mangletoken="D474L06_"
+decltoken="decl"
+
 # Check if in and out dirs are given and exist
 if [ "x$srcdir" == "x" ]; then
 	echo "Please provide a source dir as first argument!"
@@ -26,3 +29,23 @@ fi
 
 cp -r "$srcdir/"*".dl" "$srcdir/$incdir" "$outdir"
 
+# Step 1: Name-mangle declarations in header files
+cd "$outdir/$incdir"
+for header in *.dl
+do
+	# Skip ir.dl
+	test "$header" == "ir.dl" && continue;
+
+	prefix="$(echo $mangletoken$header | sed 's/\./_/g')__"
+	decls="$(cat "$header" | sed -n 's/^[[:space:]]*\.'$decltoken'[[:space:]]\+\([a-zA-Z0-9_]\+\)[[:space:]]*(.*)\?.*/\1/p')"
+	new_header="$(cat "$header")"
+
+	for decl in $decls
+	do
+		new_decl="$prefix$decl"
+		new_header="$(echo "$new_header" | sed 's/\([^a-zA-Z0-9_]\+\|^\)'$decl'\([^a-zA-Z0-9_]\)/\1'$new_decl'\2/g')"
+	done
+
+	echo "$new_header" > $header
+done
+cd -
