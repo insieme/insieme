38d4be7edf3d52ac579570165e53d21931cc934c
Laurent Seiler <laurent.seiler@student.uibk.ac.at>
2016-06-16 11:03:30 +0200

    Datalog: Added integer analysis test. Currently just checks a string-to-num function

---
+++ code/analysis/test/integer_analysis_test.cc
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2002-2016 Distributed and Parallel Systems Group,
+ *                Institute of Computer Science,
+ *               University of Innsbruck, Austria
+ *
+ * This file is part of the INSIEME Compiler and Runtime System.
+ *
+ * We provide the software of this file (below described as "INSIEME")
+ * under GPL Version 3.0 on an AS IS basis, and do not warrant its
+ * validity or performance.  We reserve the right to update, modify,
+ * or discontinue this software at any time.  We shall have no
+ * obligation to supply such updates or modifications or any other
+ * form of support to you.
+ *
+ * If you require different license terms for your intended use of the
+ * software, e.g. for proprietary commercial or industrial use, please
+ * contact us at:
+ *                   insieme@dps.uibk.ac.at
+ *
+ * We kindly ask you to acknowledge the use of this software in any
+ * publication or other disclosure of results by referring to the
+ * following citation:
+ *
+ * H. Jordan, P. Thoman, J. Durillo, S. Pellegrini, P. Gschwandtner,
+ * T. Fahringer, H. Moritsch. A Multi-Objective Auto-Tuning Framework
+ * for Parallel Codes, in Proc. of the Intl. Conference for High
+ * Performance Computing, Networking, Storage and Analysis (SC 2012),
+ * IEEE Computer Society Press, Nov. 2012, Salt Lake City, USA.
+ *
+ * All copyright notices must be kept intact.
+ *
+ * INSIEME depends on several third party software packages. Please
+ * refer to http://www.dps.uibk.ac.at/insieme/license.html for details
+ * regarding third party software licenses.
+ */
+
+#include <gtest/gtest.h>
+
+#include "insieme/analysis/datalog/integer_analysis.h"
+#include "insieme/core/ir_builder.h"
+
+namespace insieme {
+namespace analysis {
+namespace datalog {
+
+	using namespace core;
+
+	using SymbolTable = std::map<std::string,core::NodePtr>;
+
+	IRBuilder& getBuilder() {
+		static NodeManager mgr;
+		static IRBuilder builder(mgr);
+		return builder;
+	}
+
+	template <typename T>
+	bool stringRepresentsNumber(const std::string &str) {
+		try {
+			boost::lexical_cast<T>(str);
+		} catch (boost::bad_lexical_cast &) {
+			return false;
+		}
+		return true;
+	}
+
+	template <typename T>
+	T convertStringToNumber(const std::string &str) {
+		return boost::lexical_cast<T>(str);
+	}
+
+	bool isInt32(const std::string &str) {
+		return stringRepresentsNumber<int32_t>(str);
+	}
+
+	TEST(IntegerAnalysis, Int32Check) {
+		EXPECT_TRUE(isInt32("-10"));
+		EXPECT_TRUE(isInt32("-1"));
+		EXPECT_TRUE(isInt32("0"));
+		EXPECT_TRUE(isInt32("1"));
+		EXPECT_TRUE(isInt32("1234"));
+		EXPECT_TRUE(isInt32("-2147483648")); /* int32 min */
+		EXPECT_TRUE(isInt32("2147483647"));  /* int32 max */
+
+		const std::string str = "267100722";
+		printf("Resulting number from string '%s': %d\n", str.c_str(),
+				convertStringToNumber<uint32_t>(str));
+
+		EXPECT_FALSE(isInt32(""));            /* empty */
+		EXPECT_FALSE(isInt32(" "));
+		EXPECT_FALSE(isInt32("hello world")); /* string */
+		EXPECT_FALSE(isInt32("a"));
+		EXPECT_FALSE(isInt32("1a"));          /* num is part of string */
+		EXPECT_FALSE(isInt32("a1"));
+		EXPECT_FALSE(isInt32(" 1"));
+		EXPECT_FALSE(isInt32("1 "));
+		EXPECT_FALSE(isInt32(" 1 "));
+		EXPECT_FALSE(isInt32("1."));         /* floating points */
+		EXPECT_FALSE(isInt32("1.0"));
+		EXPECT_FALSE(isInt32("1.337"));
+		EXPECT_FALSE(isInt32("-2147483649")); /* invalid value range - int32 min - 1*/
+		EXPECT_FALSE(isInt32("2147483648"));  /* int32 max + 1 */
+		EXPECT_FALSE(isInt32("-4294967296")); /* int32 min << 2 */
+		EXPECT_FALSE(isInt32("4294967294"));  /* int32 max << 2 */
+	}
+
+} // end namespace datalog
+} // end namespace analysis
+} // end namespace insieme
+
