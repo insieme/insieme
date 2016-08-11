1ea8f1d30afbd9e3b23ffead8b038cd60a6b13a9
Herbert <herbert@dps.uibk.ac.at>
2016-08-11 14:00:07 +0200

    Generalized alias analysis test.

--- code/analysis/test/datalog/alias_analysis_test.cc
+++ code/analysis/test/datalog/alias_analysis_test.cc
@@ -34,94 +34,22 @@
  * regarding third party software licenses.
  */
 
+
 #include <gtest/gtest.h>
 
-#include "insieme/analysis/datalog/alias_analysis.h"
-#include "insieme/core/ir_builder.h"
+#include "insieme/analysis/datalog_interface.h"
+
+#include "../common/alias_analysis_test.inc"
 
 namespace insieme {
 namespace analysis {
-namespace datalog {
 
-	using namespace core;
+	/**
+	 * Run the alias analysis tests using the datalog backend.
+	 */
+	INSTANTIATE_TYPED_TEST_CASE_P(Datalog, AliasAnalysis, DatalogEngine);
 
-	using SymbolTable = std::map<std::string,core::NodePtr>;
 
-
-	TEST(AliasAnalysis, Basic) {
-		NodeManager mgr;
-		IRBuilder builder(mgr);
-
-		auto addresses = builder.parseAddressesStatement(
-				"{"
-				"	var ref<bool> x = ref_new(type_lit(bool));"
-				"	$x$;"
-				"	$x$;"
-				"}"
-		);
-
-		EXPECT_EQ(2,addresses.size());
-
-		auto x1 = addresses[0].getRootAddress().as<CompoundStmtAddress>()[0].as<DeclarationStmtAddress>()->getVariable();
-		auto x2 = addresses[0].as<VariableAddress>();
-		auto x3 = addresses[1].as<VariableAddress>();
-
-		EXPECT_TRUE(areAlias(x1,x1));
-		EXPECT_TRUE(mayAlias(x1,x1));
-
-		EXPECT_TRUE(areAlias(x2,x2));
-		EXPECT_TRUE(mayAlias(x2,x2));
-
-		EXPECT_TRUE(areAlias(x3,x3));
-		EXPECT_TRUE(mayAlias(x3,x3));
-
-		EXPECT_TRUE(areAlias(x1,x2));
-		EXPECT_TRUE(mayAlias(x1,x2));
-
-		EXPECT_TRUE(areAlias(x1,x3));
-		EXPECT_TRUE(mayAlias(x1,x3));
-
-		EXPECT_TRUE(areAlias(x2,x3));
-		EXPECT_TRUE(mayAlias(x2,x3));
-	}
-
-	TEST(AliasAnalysis, MultipleReferences) {
-		NodeManager mgr;
-		IRBuilder builder(mgr);
-
-		auto addresses = builder.parseAddressesStatement(
-				"{"
-				"	var ref<bool> x = ref_new(type_lit(bool));"
-				"	var ref<bool> y = ref_new(type_lit(bool));"
-				"	var ref<bool> z = x;"
-				"	$x$;"
-				"	$y$;"
-				"	$z$;"
-				"}"
-		);
-
-		EXPECT_EQ(3,addresses.size());
-
-		auto x = addresses[0].as<VariableAddress>();;
-		auto y = addresses[1].as<VariableAddress>();
-		auto z = addresses[2].as<VariableAddress>();
-
-		EXPECT_FALSE(areAlias(x,y));
-		EXPECT_FALSE(mayAlias(x,y));
-
-		EXPECT_TRUE(areAlias(x,z));
-		EXPECT_TRUE(mayAlias(x,z));
-
-		EXPECT_FALSE(areAlias(y,z));
-		EXPECT_FALSE(mayAlias(y,z));
-
-	}
-
-	// TODO: test global variables
-
-	// TODO: test free variables
-
-} // end namespace datalog
 } // end namespace analysis
 } // end namespace insieme
 
