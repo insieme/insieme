abe9b253824f4d1ee8cab6ce5ff0f3192849e2ee
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2016-08-18 16:33:23 +0200

    HAT: Binary runs reference analysis on matrix_mul_static

--- code/analysis/src/haskell/insieme-hat/src/Main.hs
+++ code/analysis/src/haskell/insieme-hat/src/Main.hs
@@ -6,11 +6,13 @@ import Control.Monad
 import Data.List
 import Data.Tree (Tree(Node))
 import Debug.Trace
+import Insieme.Analysis.Entities.FieldIndex (SimpleFieldIndex)
 import qualified Data.ByteString as BS
 import qualified Insieme.Analysis.Alias as Alias
 import qualified Insieme.Analysis.Arithmetic as Arith
 import qualified Insieme.Analysis.Boolean as AnBoolean
 import qualified Insieme.Analysis.Framework.PropertySpace.ComposedValue as ComposedValue
+import qualified Insieme.Analysis.Reference as Ref
 import qualified Insieme.Analysis.Solver as Solver
 import qualified Insieme.Inspire as IR
 import qualified Insieme.Inspire.BinaryParser as BinPar
@@ -18,6 +20,7 @@ import qualified Insieme.Inspire.NodeAddress as Addr
 import qualified Insieme.Inspire.Utils as Utils
 import qualified Insieme.Utils.Arithmetic as Ar
 import qualified Insieme.Utils.BoundSet as BSet
+import qualified Insieme.Utils.UnboundSet as USet
 
 
 
@@ -29,4 +32,22 @@ main = do
     -- run parser
     let Right ir = BinPar.parseBinaryDump dump
 
-    print . length . IR.getTree $ ir
+    let res = Utils.foldTree go ir
+
+    print $ length $ filter (=="error") res
+    print $ length $ filter (=="ok") res
+
+ where
+    go :: Addr.NodeAddress -> [String] -> [String]
+    go addr xs = case Addr.getNode addr of
+
+        Node IR.CallExpr _ | Addr.isBuiltin (Addr.goDown 1 addr) "ref_deref" ->
+            if USet.null res
+               then "error" : xs
+               else "ok" : xs
+
+        _ -> xs
+
+      where
+        res :: USet.UnboundSet (Ref.Reference SimpleFieldIndex)
+        res = ComposedValue.toValue $ Solver.resolve (Ref.referenceValue $ Addr.goDown 2 addr)
