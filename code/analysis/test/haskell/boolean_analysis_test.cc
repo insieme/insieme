66d901ea7f2460355eb991ca98ffa562be3ff5d8
Herbert <herbert@dps.uibk.ac.at>
2016-08-19 16:55:31 +0200

    Added support for function pointer calls.

--- code/analysis/test/haskell/boolean_analysis_test.cc
+++ code/analysis/test/haskell/boolean_analysis_test.cc
@@ -48,6 +48,56 @@ namespace analysis {
 	 */
 	INSTANTIATE_TYPED_TEST_CASE_P(Haskell, BooleanValue, HaskellEngine);
 
+
+	bool isTrue(const StatementAddress& stmt) {
+		return isTrue<HaskellEngine>(stmt.as<ExpressionAddress>());
+	}
+
+	bool isFalse(const StatementAddress& stmt) {
+		return isFalse<HaskellEngine>(stmt.as<ExpressionAddress>());
+	}
+
+	TEST(AdvancedBooleanAnalysis, FunctionReferences) {
+		NodeManager mgr;
+		IRBuilder builder(mgr);
+
+		auto stmt = builder.parseStmt(
+				"def always = ()->bool {"
+				"	return true;"
+				"};"
+				""
+				"def never = ()->bool {"
+				"	return false;"
+				"};"
+				""
+				"{"
+				"	always();"
+				"	never();"
+				"	"
+				"	var ref<()->bool> f = always;"
+				"	(*f)();"
+				"	f = never;"
+				"	(*f)();"
+				"	"
+				"	var ref<()->bool> g = ref_of_function(always);"
+				"	(*g)();"
+				""
+				"}"
+		).as<CompoundStmtPtr>();
+
+		auto comp = CompoundStmtAddress(stmt);
+
+		EXPECT_TRUE(isTrue(comp[0]));
+		EXPECT_TRUE(isFalse(comp[1]));
+
+		EXPECT_TRUE(isTrue(comp[3]));
+		EXPECT_TRUE(isFalse(comp[5]));
+
+		EXPECT_TRUE(isTrue(comp[7]));
+
+
+	}
+
 } // end namespace analysis
 } // end namespace insieme
 
