b239ab222a63cf84d810b668bff35dc5f49bc314
Herbert <herbert@dps.uibk.ac.at>
2016-09-22 17:26:57 +0200

    Call-Site analysis utilizes now context-free lambda reference discovery.

--- code/analysis/src/haskell/insieme-hat/src/Insieme/Analysis/CallSite.hs
+++ code/analysis/src/haskell/insieme-hat/src/Insieme/Analysis/CallSite.hs
@@ -45,6 +45,7 @@ import Insieme.Inspire.NodeAddress
 import Insieme.Inspire.Utils
 import qualified Data.Set as Set
 import qualified Insieme.Analysis.Callable as Callable
+import qualified Insieme.Analysis.RecursiveLambdaReferences as RecLambdaRefs
 import qualified Insieme.Analysis.Solver as Solver
 import qualified Insieme.Inspire as IR
 import qualified Insieme.Utils.UnboundSet as USet
@@ -117,67 +118,66 @@ callSites addr = case getNodeType addr of
     numParams = numChildren $ goDown 1 addr                     -- TODO: this is wrong in case of variadic parameters!!
 
 
-    -- distinguish binding case --
-    var =
-        if isRoot addr then noCallSitesVar
-        else if isStaticallyBound then staticCallSiteVar
-        else allCallSitesVar
-
-    noCallSitesVar = Solver.mkVariable id [] Solver.bot
-
-    recReferences = case getNodeType addr of
-        IR.BindExpr -> []          -- bind can not be recursive
-        IR.Lambda   -> if isRoot p then [] else res
-            where
-                ref = getNodePair $ goDown 0 $ fromJust $ getParent addr
-                def = fromJust $ getParent $ fromJust $ getParent addr
-                res = foldAddressPrune agg filter def
-
-                agg n l = case getNodeType n of
-                    IR.LambdaReference | isUse -> n : l --- TODO: filter out refs in bindings
-                        where
-                            isUse = getNodePair n == ref && (getNodeType . fromJust . getParent) n /= IR.LambdaBinding
-                    _                                            -> l
-
-                filter n = isType (getNodeType n) || (IR.LambdaExpr == (getNodeType n))      -- TODO: support for lambda references exceeding local scope
+    -- create the variable (by binding at least the id) --
+    var = Solver.mkVariable id cons init
+    
+    cons = if isRoot addr then [] else [con]
+    init = if isRoot addr then Solver.bot else directCall
 
 
-    isStaticallyBound = i == 1 && isCall p && all check recReferences
-        where
-            check a = getIndex a == 1 && (isCall $ fromJust $ getParent a)
+    -- assemble the constraint computing all call sites --
+    
+    con = Solver.createConstraint dep val var
+    dep a = (Solver.toVar <$> recReferencesDep) ++ (Solver.toVar <$> callSiteVars a)
+    val a = indirectCalls a 
+    
 
-    staticCallSiteVar = Solver.mkVariable id [] (res)
-        where
-            res = Set.fromList $ CallSite p : recCalls
-            recCalls = (CallSite . fromJust . getParent) <$> recReferences
+    
+    isLambda = getNodeType addr == IR.Lambda
+    
+    recReferencesVar = RecLambdaRefs.recursiveCalls addr
+    recReferencesVal a = if isLambda then Solver.get a recReferencesVar else Set.empty
+    recReferencesDep   = if isLambda then [recReferencesVar] else []  
 
-    allCallSitesVar = Solver.mkVariable id [con] Solver.bot
-        where
-            con = Solver.createConstraint dep val var
 
-            allCalls = collectAll callable (getRootAddress addr)
-              where
-                callable node = case IR.getNodeType node of
-                    IR.CallExpr | IR.numChildren node == numParams + 2 ->
-                        case IR.getNodeType $ IR.getChildren node !! 1 of
-                            IR.Lambda          -> False
-                            IR.BindExpr        -> False
-                            IR.Literal         -> False
-                            IR.LambdaReference -> False
-                            _                  -> True
-                    _ -> False
+    -- determines whether all calls are statically bound (if not, we have to search) --
+    isStaticallyBound a = i == 1 && isCall p && all check (recReferencesVal a)
+      where
+        check a = getIndex a == 1 && (isCall $ fromJust $ getParent a)
 
-            allTrgVars = map (\c -> (c , Callable.callableValue $ goDown 1 c ) ) allCalls
+    
+    -- the list of call site variables to be checked for potential calls to this lambda
+    callSiteVars a = 
+        if isStaticallyBound a then [] 
+        else Callable.callableValue . (goDown 1) <$> allCalls  
 
-            callable = case getNodeType addr of
-                IR.Lambda   -> Callable.Lambda addr
-                IR.BindExpr -> Callable.Closure addr
-                _           -> error "unexpected NodeType"
 
-            dep a = map Solver.toVar (map snd allTrgVars)
-            val a = foldr go Set.empty allTrgVars
-                where
-                    go = \(call,var) set ->
-                        if (USet.member callable (ComposedValue.toValue $ Solver.get a var))
-                        then Set.insert (CallSite call) set
-                        else set
+    -- computes a list of addresses of all potentiall call sites 
+    allCalls = collectAll callable (getRootAddress addr)
+      where
+        callable node = case IR.getNodeType node of
+            IR.CallExpr | IR.numChildren node == numParams + 2 ->
+                case IR.getNodeType $ IR.getChildren node !! 1 of
+                    IR.Lambda          -> False
+                    IR.BindExpr        -> False
+                    IR.Literal         -> False
+                    IR.LambdaReference -> False
+                    _                  -> True
+            _ -> False
+
+
+    directCall = if i == 1 && isCall p then Set.singleton $ CallSite p else Set.empty
+    
+    indirectCalls a = 
+        Set.fromList $ if isStaticallyBound a 
+        then CallSite . fromJust . getParent <$> Set.toList (recReferencesVal a)
+        else CallSite <$> reachingCalls
+      where
+        reachingCalls = filter f $ allCalls
+        f c = USet.member callable $ ComposedValue.toValue $ Solver.get a $ Callable.callableValue $ goDown 1 c
+
+        callable = case getNodeType addr of
+            IR.Lambda   -> Callable.Lambda addr
+            IR.BindExpr -> Callable.Closure addr
+            _           -> error "unexpected NodeType"
+
