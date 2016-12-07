274f5344793ef21c48b26d2ba8d26f167ce7f118
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2016-12-07 15:06:44 +0100

    Tpl: Mirror python source on sunray

--- scripts/dependencies/package_python.sh
+++ scripts/dependencies/package_python.sh
@@ -3,7 +3,7 @@ VERSION="2.7.12"
 PACKAGE="$NAME-$VERSION"
 
 FILE="Python-$VERSION.tgz"
-URL="https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz"
+URL="http://www.insieme-compiler.org/ext_libs/$FILE"
 SHA256SUM="3cb522d17463dfa69a155ab18cffa399b358c966c0363d6c8b5b3bf1384da4b6"
 
 pkg_extract() {
