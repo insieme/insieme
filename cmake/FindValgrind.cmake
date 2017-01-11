1955d742bf9e2ea7c0ed43eafd6b5bd3ebd9fc9d
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-01-11 15:10:36 +0100

    Tpl: Fix cases of VALGRIND_ROOT directive
    ref #20

--- cmake/FindValgrind.cmake
+++ cmake/FindValgrind.cmake
@@ -6,7 +6,7 @@
 # Variables used by this module, they can change the default behaviour and need
 # to be set before calling find_package:
 #
-# Valgrind_ROOT Set this variable to the root installation of valgrind if the
+# VALGRIND_ROOT Set this variable to the root installation of valgrind if the
 # module has problems finding the proper installation path.
 #
 # Variables defined by this module:
@@ -15,15 +15,15 @@
 # 	Valgrind_EXECUTABLE the valgrind executable.
 
 # Get hint from environment variable (if any)
-if(NOT Valgrind_ROOT AND DEFINED ENV{Valgrind_ROOT})
-	set(Valgrind_ROOT "$ENV{Valgrind_ROOT}" CACHE PATH "Valgrind base directory location (optional, used for nonstandard installation paths)")
-	mark_as_advanced(Valgrind_ROOT)
+if(NOT VALGRIND_ROOT AND DEFINED ENV{VALGRIND_ROOT})
+	set(VALGRIND_ROOT "$ENV{VALGRIND_ROOT}" CACHE PATH "Valgrind base directory location (optional, used for nonstandard installation paths)")
+	mark_as_advanced(VALGRIND_ROOT)
 endif()
 
 # Search path for nonstandard locations
-if(Valgrind_ROOT)
-	set(Valgrind_INCLUDE_PATH PATHS "${Valgrind_ROOT}/include" NO_DEFAULT_PATH)
-	set(Valgrind_BINARY_PATH PATHS "${Valgrind_ROOT}/bin" NO_DEFAULT_PATH)
+if(VALGRIND_ROOT)
+	set(Valgrind_INCLUDE_PATH PATHS "${VALGRIND_ROOT}/include" NO_DEFAULT_PATH)
+	set(Valgrind_BINARY_PATH PATHS "${VALGRIND_ROOT}/bin" NO_DEFAULT_PATH)
 endif()
 
 find_path(Valgrind_INCLUDE_DIR valgrind HINTS ${Valgrind_INCLUDE_PATH})
