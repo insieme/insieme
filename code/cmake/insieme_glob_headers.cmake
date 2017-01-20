a0af94d99458a84f066cc49a36d9bdd322f6bd02
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-01-20 12:46:26 +0100

    cmake: Remove old cmake setup

--- code/cmake/insieme_glob_headers.cmake
+++
@@ -1,9 +0,0 @@
-#####
-#
-#	insieme function to find all headers and header-like files in the given directory
-#      and its sub-directories. Includes .h, .def, .inc etc.
-#
-#####
-macro(insieme_glob_headers output_var input_path)
-	file(GLOB_RECURSE ${output_var} ${input_path}/*.h  ${input_path}/*.inc  ${input_path}/*.def)
-endmacro()
