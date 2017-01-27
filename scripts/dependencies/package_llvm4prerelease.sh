0d8796e40b2fe0a06e37910e33703d4a1c9b82b5
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-01-27 10:21:42 +0100

    scripts: Fix llvm4prerelease script

--- scripts/dependencies/package_llvm4prerelease.sh
+++ scripts/dependencies/package_llvm4prerelease.sh
@@ -2,7 +2,7 @@ NAME="llvm4prerelease"
 VERSION="master"
 PACKAGE="$NAME-$VERSION"
 
-DEPENDS="gcc cmake"
+DEPENDS="gcc cmake python"
 
 export CC="$PREFIX/gcc-latest/bin/gcc"
 export CXX="$PREFIX/gcc-latest/bin/g++"
@@ -11,11 +11,11 @@ export LD_LIBRARY_PATH="$PREFIX/gcc-latest/lib64"
 
 pkg_download() {
 	git clone "http://llvm.org/git/llvm.git" "$PACKAGE"
-	git clone "http://llvm.org/git/clang.git" "$PACKAGE/llvm/tools/clang"
-	git clone "http://llvm.org/git/compiler-rt.git" "$PACKAGE/llvm/projects/compiler-rt"
-	git clone "http://llvm.org/git/openmp.git" "$PACKAGE/llvm/projects/openmp"
-	git clone "http://llvm.org/git/libcxx.git" "$PACKAGE/llvm/projects/libcxx"
-	git clone "http://llvm.org/git/libcxxabi.git" "$PACKAGE/llvm/projects/libcxxabi"
+	git clone "http://llvm.org/git/clang.git" "$PACKAGE/tools/clang"
+	git clone "http://llvm.org/git/compiler-rt.git" "$PACKAGE/projects/compiler-rt"
+	git clone "http://llvm.org/git/openmp.git" "$PACKAGE/projects/openmp"
+	git clone "http://llvm.org/git/libcxx.git" "$PACKAGE/projects/libcxx"
+	git clone "http://llvm.org/git/libcxxabi.git" "$PACKAGE/projects/libcxxabi"
 }
 
 pkg_extract() {
@@ -25,7 +25,25 @@ pkg_extract() {
 pkg_configure() {
 	mkdir build
 	cd build
-	$PREFIX/cmake-latest/bin/cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX/$PACKAGE" -DCMAKE_BUILD_TYPE=Release
+	$PREFIX/cmake-latest/bin/cmake \
+		-DCMAKE_INSTALL_PREFIX="$PREFIX/$PACKAGE" \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DPYTHON_EXECUTABLE="$PREFIX/python-latest/bin/python" \
+		..
+}
+
+pkg_build() {
+	make -j "$SLOTS"
+	make -j "$SLOTS" cxx
+}
+
+pkg_install() {
+	make install
+	make install-cxx install-cxxabi
+
+	rm -f "$PREFIX/$NAME-latest"
+	ln -s "$PREFIX/$PACKAGE" "$PREFIX/$NAME-latest"
+	touch "$PREFIX/$PACKAGE/.installed"
 }
 
 pkg_cleanup() {
