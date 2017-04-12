bd1f7fc4f91b17ed39d9801c79372945afd21fe6
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-04-12 13:10:48 +0200

    Tpl: No longer inherit CFLAGS and CXXFLAGS

--- cmake/build_settings.cmake
+++ cmake/build_settings.cmake
@@ -15,22 +15,18 @@ if(BUILD_TESTS)
 endif()
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR ${CMAKE_CXX_COMPILER_ID} STREQUAL "AppleClang")
-	# base C flags
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -std=c99")
-	set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -g3 -ggdb")
-	set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2")
+	# C flags
+	set(CMAKE_C_FLAGS "-Wall -Wextra -std=c99")
+	set(CMAKE_C_FLAGS_DEBUG "-O0 -g3 -ggdb")
+	set(CMAKE_C_FLAGS_RELEASE "-O2")
 
-	# base C++ flags
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -std=c++14")
-	set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g3 -ggdb")
-	set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2")
+	# C++ flags
+	set(CMAKE_CXX_FLAGS "-Wall -Wextra -std=c++14")
+	set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g3 -ggdb")
+	set(CMAKE_CXX_FLAGS_RELEASE "-O2")
 
 	# Yo Dawg, I heard you like templates!
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftemplate-depth-900")
-
-	# remove NDEBUG from release flags
-	string(REPLACE "-DNDEBUG" "" CMAKE_C_FLAGS_RELEASE ${CMAKE_C_FLAGS_RELEASE})
-	string(REPLACE "-DNDEBUG" "" CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE})
 
 	# \[T]/ Praise the sun. There be dragons ahead.
 	if(NOT USE_ASSERT)
