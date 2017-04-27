89c96c77193de256c4e8274e7b96f48a68bfb7b1
Peter Zangerl <peterz@dps.uibk.ac.at>
2017-04-27 15:46:56 +0200

    backend: Fixed certain uses of std::initializer_lists.

--- test/seq/cpp/cpp11/initializer_list_interception/initializer_list_interception.cpp
+++ test/seq/cpp/cpp11/initializer_list_interception/initializer_list_interception.cpp
@@ -1,15 +1,22 @@
 
 #include <vector>
 #include <initializer_list>
+#include "header.h"
 
 template<typename T>
 struct A {
 	std::vector<T> mem;
 	A(int a, int b) : mem({a, b}) {}
 	A(std::initializer_list<T> l) {}
+	A(const std::initializer_list<T>& l, int i) {}
 };
 
 int main() {
 	A<int> a(1, 2);
 	A<double> b({3.0, 4.0});
+
+	std::initializer_list<int> list{5, 6};
+	A<int>(list, 7);
+
+	Intercepted({8, 9});
 }
