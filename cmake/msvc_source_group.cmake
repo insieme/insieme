bccaa039116ddf4b8ae7da2e9ed38e33544d6e45
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-05-16 14:39:31 +0200

    CMake: Fix path to builtins

--- cmake/msvc_source_group.cmake
+++ cmake/msvc_source_group.cmake
@@ -1,13 +1,18 @@
 macro(msvc_source_group label files)
 	if(MSVC)
-		set(one_value_args STRIP)
+		set(one_value_args STRIP STRIP_ABS)
 		cmake_parse_arguments(ARG "" "${one_value_args}" "" ${ARGN})
 
 		foreach(file ${files})
+			message("== ${file} | ${ARG_STRIP}")
 			get_filename_component(file_dir ${file} DIRECTORY)
 
 			# strip
-			string(LENGTH ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_STRIP} strip_dir_length)
+			if(STRIP_ABS)
+				string(LENGTH ${ARG_STRIP_ABS} strip_dir_length)
+			else()
+				string(LENGTH ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_STRIP} strip_dir_length)
+			endif()
 			string(SUBSTRING ${file_dir} ${strip_dir_length} -1 file_dir)
 
 			string(REPLACE "/" "\\" file_dir "${file_dir}")
