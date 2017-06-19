dce997f6d85d8c2cb1845734eaf5c7d4430226f2
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-06-19 13:27:52 +0200

    CBA: Only require GMP when Haskell Analysis is enabled

--- cmake/dependencies/gmp.cmake
+++ cmake/dependencies/gmp.cmake
@@ -1,2 +1,4 @@
-set(GMP_VERSION 6.0.0 CACHE STRING "GMP Version")
-find_package(GMP ${GMP_VERSION})
+if(ANALYSIS_HASKELL)
+	set(GMP_VERSION 6.0.0 CACHE STRING "GMP Version")
+	find_package(GMP ${GMP_VERSION})
+endif()
