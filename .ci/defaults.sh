e87e147392bec35bae98ac1fe2d185e70ae239df
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-06-19 17:31:27 +0200

    CI: Better workspace assumption if not set

--- .ci/defaults.sh
+++ .ci/defaults.sh
@@ -11,7 +11,8 @@ export IRT_NUM_WORKERS="3"
 export THIRD_PARTY_LIBS="${THIRD_PARTY_LIBS:-$HOME/third_party_libs}"
 
 # Assume Workspace if not set.
-export WORKSPACE="${WORKSPACE:-..}"
+CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export WORKSPACE="${WORKSPACE:-"$(realpath "$CI_DIR/..")"}"
 
 # Assume Build Directory if not set.
 export BUILD_DIR="${BUILD_DIR:-$WORKSPACE/build}"
