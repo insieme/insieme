df9d685d944bb3b2ab64b73725f6ec216f9d43b6
Herbert Jordan <herbert@dps.uibk.ac.at>
2017-07-13 11:21:13 +0200

    Added cartesion product operator for lists of bound sets.

--- code/analysis/src/cba/haskell/insieme-hat/src/Insieme/Utils/BoundSet.hs
+++ code/analysis/src/cba/haskell/insieme-hat/src/Insieme/Utils/BoundSet.hs
@@ -89,6 +89,7 @@ module Insieme.Utils.BoundSet (
     union,
     intersection,
     cartProduct,
+    cartProductL,
 
     -- * Unbound Set
     UnboundSet,
@@ -214,6 +215,15 @@ cartProduct _            Universe     = Universe
 cartProduct (BoundSet x) (BoundSet y) = fromList prod
   where
      prod = [(u, v) | u <- Set.toList x, v <- Set.toList y]
+     
+cartProductL :: (IsBound bb, Ord a) => [BoundSet bb a] -> BoundSet bb [a]
+cartProductL       []        = singleton []
+cartProductL (Universe : bs) = Universe
+cartProductL (       h : bs) = if isUniverse base then Universe else fromList prod
+  where
+    base = cartProductL bs
+    prod = [ u : vs | u <- toList h, vs <- toList base ] 
+
 
 -- | Special bound which does not turn a 'BoundSet' automatically into
 -- 'Universe'.
