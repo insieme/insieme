d7c85d399ad8c358182ceaf260495d3b39532bc1
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-11-30 11:13:45 +0100

    CMake: Fix FindLuaJIT not taking LUAJIT_ROOT into account

--- cmake/FindLuaJIT.cmake
+++ cmake/FindLuaJIT.cmake
@@ -1,8 +1,24 @@
-# - Try to find luajit
+# Try to find luajit
+#
+# LUAJIT_ROOT Set this variable to the root installation of
+# libluajit if the module has problems finding the
+# proper installation path.
+#
 # Once done this will define
 #  LUAJIT_FOUND - System has luajit
 #  LUAJIT_INCLUDE_DIRS - The luajit include directories
 #  LUAJIT_LIBRARIES - The libraries needed to use luajit
+
+if(NOT LUAJIT_ROOT AND DEFINED ENV{LUAJIT_ROOT})
+  set(LUAJIT_ROOT "$ENV{LUAJIT_ROOT}" CACHE PATH "LUAJIT base directory location (optional, used for nonstandard installation paths)")
+  mark_as_advanced(LUAJIT_ROOT)
+endif()
+
+if(LUAJIT_ROOT)
+  set(LUAJIT_INCLUDE_PATH PATHS "${LUAJIT_ROOT}/include" NO_DEFAULT_PATH)
+  # for MSVC, the luajit library is located in the root directory, not root/lib
+  set(LUAJIT_LIBRARY_PATH PATHS "${LUAJIT_ROOT}/lib" "${LUAJIT_ROOT}" NO_DEFAULT_PATH)
+endif()
 
 if(NOT USE_BUNDLED_LUAJIT)
   find_package(PkgConfig)
@@ -20,6 +36,7 @@ endif()
 set(LUAJIT_DEFINITIONS ${PC_LUAJIT_CFLAGS_OTHER})
 
 find_path(LUAJIT_INCLUDE_DIR luajit.h
+          HINTS ${LUAJIT_INCLUDE_PATH}
           PATHS ${PC_LUAJIT_INCLUDEDIR} ${PC_LUAJIT_INCLUDE_DIRS}
           PATH_SUFFIXES luajit-2.0
           ${LIMIT_SEARCH})
@@ -40,6 +57,7 @@ else()
 endif()
 
 find_library(LUAJIT_LIBRARY NAMES ${LUAJIT_NAMES}
+             HINTS ${LUAJIT_LIBRARY_PATH}
              PATHS ${PC_LUAJIT_LIBDIR} ${PC_LUAJIT_LIBRARY_DIRS}
              ${LIMIT_SEARCH})
 
@@ -52,4 +70,8 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LuaJit DEFAULT_MSG
                                   LUAJIT_LIBRARY LUAJIT_INCLUDE_DIR)
 
-mark_as_advanced(LUAJIT_INCLUDE_DIR LUAJIT_LIBRARY)
+if(NOT LUAJIT_ROOT)
+  get_filename_component(LUAJIT_ROOT ${LUAJIT_INCLUDE_DIR} DIRECTORY)
+endif()
+
+mark_as_advanced(LUAJIT_ROOT LUAJIT_INCLUDE_DIR LUAJIT_LIBRARY)
