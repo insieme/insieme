092b780d76d732b3935c885fa647ff8da039a5dd
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-12-01 09:54:46 +0100

    scripts: Add jemalloc install script

---
+++ scripts/dependencies/package_jemalloc.sh
@@ -0,0 +1,7 @@
+NAME="jemalloc"
+VERSION="5.0.1"
+PACKAGE="$NAME-$VERSION"
+
+FILE="$PACKAGE.tar.bz2"
+URL="http://www.insieme-compiler.org/ext_libs/$FILE"
+SHA256SUM="4814781d395b0ef093b21a08e8e6e0bd3dab8762f9935bbfb71679b0dea7c3e9"
