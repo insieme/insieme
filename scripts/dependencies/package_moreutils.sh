cf58eff1e0234a306be1f77656f54a8f8c247030
Daniel Gröber <daniel@dps.uibk.ac.at>
2018-04-17 14:27:39 +0200

    dependencies: Add moreutils package

---
+++ scripts/dependencies/package_moreutils.sh
@@ -0,0 +1,21 @@
+NAME="moreutils"
+VERSION="0.60"
+PACKAGE="$NAME-$VERSION"
+
+FILE="${NAME}_${VERSION}.orig.tar.xz"
+URL="http://http.debian.net/debian/pool/main/m/moreutils/moreutils_${VERSION}.orig.tar.xz"
+SHA256SUM="e42d18bacbd2d003779a55fb3542befa5d1d217ee37c1874e8c497581ebc17c5"
+
+DEPENDS=""
+
+pkg_configure() {
+    : # pacakge doesn't have a configure script, just a makefile
+}
+
+pkg_build() {
+    DOCBOOK2XMAN='sh -c "touch "' make -e -j"$SLOTS"
+}
+
+pkg_install() {
+    make -e install PREFIX="$PREFIX/$PACKAGE"
+}
